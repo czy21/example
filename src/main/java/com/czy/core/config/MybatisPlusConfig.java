e5b7cc642275436545ed00d154819a4828863b8a
Fighter <805899926@qq.com>
2018-10-13 10:40:30 +0800

    重新配置MybatisPlus

---
+++ src/main/java/com/czy/core/config/MybatisPlusConfig.java
@@ -0,0 +1,12 @@
+package com.czy.core.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootConfiguration
+@MapperScan(value = "com.czy.dao*")
+public class MybatisPlusConfig {
+
+}
