e45a5e344b668e0e79acb31d265d88799dc0a39a
Fighter <805899926@qq.com>
2018-10-18 23:45:41 +0800

    配置mysql并完成部分备份数据库脚本

--- script/test.py
+++ script/test.py
@@ -1,2 +1,17 @@
+# !/usr/bin/env python
 import os
-print(os.getcwd());
+import configparser
+
+scriptPath = os.path.abspath('.')
+backupPath = scriptPath + '\\backup'
+configPath = scriptPath + '\\config'
+os.chdir(configPath)
+cf = configparser.ConfigParser();
+cf.read("mysql.conf")
+db_host = cf.get("db", "db_host")
+db_port = cf.getint("db", "db_port")
+db_user = cf.get("db", "db_user")
+db_pass = cf.get("db", "db_pass")
+db_name = cf.get("db", "db_name")
+dumpcmd = "mysqldump"" --default-character-set=UTF8 --skip-add-locks --quick -t " + db_name + " -u" + db_user + " -p" + db_pass + " > " + backupPath + "\\" + db_name + "_bak" ".sql"
+os.system(dumpcmd)
