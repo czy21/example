05e9d37b7d5ba52a72b0f6c6014805fc943eb904
Fighter <805899926@qq.com>
2018-10-18 23:45:41 +0800

    配置mysql并完成部分备份数据库脚本

--- script/local_backup_oa_to_oa_bak.py
+++ script/local_backup_oa_to_oa_bak.py
@@ -1,56 +1,18 @@
-#!/usr/bin/python
+# !/usr/bin/env python
 import os
-import time
-import datetime
+import configparser
 
-DB_HOST = 'localhost'
-DB_USER = 'root'
-DB_USER_PASSWORD = 'sasa'
-DB_NAME = 'czy_oa'
-BACKUP_PATH = os.getcwd()
-
-
-dumpcmd = "mysqldump -u " + DB_USER + " -p" + DB_USER_PASSWORD + \
-            " " + db + " > " + TODAYBACKUPPATH + "/" + db + ".sql"
-        os.system(dumpcmd)
-# Checking if backup folder already exists or not. If not exists will create it.
-# print("creating backup folder")
-# if not os.path.exists(TODAYBACKUPPATH):
-#     os.makedirs(TODAYBACKUPPATH)
-
-# # Code for checking if you want to take single database backup or assinged multiple backups in DB_NAME.
-# print("checking for databases names file.")
-# if os.path.exists(DB_NAME):
-#     file1 = open(DB_NAME)
-#     multi = 1
-#     print("Databases file found...")
-#     print("Starting backup of all dbs listed in file " + DB_NAME)
-# else:
-#     print("Databases file not found...")
-#     print("Starting backup of database " + DB_NAME)
-#     multi = 0
-
-# # Starting actual database backup process.
-# if multi:
-#     in_file = open(DB_NAME, "r")
-#     flength = len(in_file.readlines())
-#     in_file.close()
-#     p = 1
-#     dbfile = open(DB_NAME, "r")
-
-#     while p <= flength:
-#         db = dbfile.readline()   # reading database name from file
-#         db = db[:-1]         # deletes extra line
-#         dumpcmd = "mysqldump -u " + DB_USER + " -p" + DB_USER_PASSWORD + \
-#             " " + db + " > " + TODAYBACKUPPATH + "/" + db + ".sql"
-#         os.system(dumpcmd)
-#         p = p + 1
-#     dbfile.close()
-# else:
-#     db = DB_NAME
-#     dumpcmd = "mysqldump -u " + DB_USER + " -p" + DB_USER_PASSWORD + \
-#         " " + db + " > " + TODAYBACKUPPATH + "/" + db + ".sql"
-#     os.system(dumpcmd)
-
-# print("Backup script completed")
-# print("Your backups has been created in '" + TODAYBACKUPPATH + "' directory")
+scriptPath = os.path.abspath('.')
+backupPath = scriptPath + '\\backup'
+configPath = scriptPath + '\\config'
+os.chdir(configPath)
+cf = configparser.ConfigParser();
+cf.read("mysql.conf")
+db_host = cf.get("db", "db_host")
+db_port = cf.getint("db", "db_port")
+db_user = cf.get("db", "db_user")
+db_pass = cf.get("db", "db_pass")
+db_name = cf.get("db", "db_name")
+dumpcmd = "mysqldump -u " + db_user + " -p" + db_pass + " " + db_name + " > " + backupPath + "\\" + db_name + "_bak" ".sql"
+print(dumpcmd)
+# os.system(dumpcmd)
