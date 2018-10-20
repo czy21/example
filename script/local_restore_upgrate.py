7f061f9f90528dd8be546e9dd268fbc47fa8efc2
Fighter <805899926@qq.com>
2018-10-20 15:51:18 +0800

    ff

--- script/local_restore_upgrate.py
+++ script/local_restore_upgrate.py
@@ -1,13 +1,16 @@
 # !/usr/bin/env python
 import os
 import configparser
+import tempfile
 
 root_path = os.path.abspath('..')
-db_path = root_path+'\\db'
-version_path = db_path+'\\create\\200_ByVersion'
-scriptPath = os.path.abspath('.')
-configPath = scriptPath + '\\config'
-os.chdir(configPath)
+temp_path = root_path + "\\_temp"
+db_temp_path = temp_path + "\\db"
+db_path = root_path + '\\db'
+script_path = root_path + "\\script"
+config_path = script_path + '\\config'
+version_path = db_path + '\\create\\200_ByVersion'
+os.chdir(config_path)
 cf = configparser.ConfigParser()
 cf.read("mysql.conf")
 db_host = cf.get("db", "db_host")
@@ -16,21 +19,25 @@ db_user = cf.get("db", "db_user")
 db_pass = cf.get("db", "db_pass")
 db_name = cf.get("db", "db_name")
 db_bak_name = cf.get("db", "db_bak_name")
-# dumpcmd = "mysqldump " + db_bak_name + " -u " + db_user + " -p" + db_pass + " --add-drop-table "+  "| "+ "mysql "+ db_name+" -u " + db_user + " -p" + db_pass
-# os.system(dumpcmd)
-
-
-sourcecmd = ""
-# os.system(upgradecmd)
+dumpcmd = "mysqldump " + db_bak_name + " -u " + db_user + " -p" + db_pass + " --add-drop-table " + "| " + "mysql " + db_name + " -u " + db_user + " -p" + db_pass
+os.system(dumpcmd)
+temp_sql = tempfile.NamedTemporaryFile('w+', suffix=".sql", dir=db_temp_path, delete=False, encoding="utf-8")
 for r, s, fs in os.walk(version_path):
     if r[-3:] == "(s)":
         for r, s, fs in os.walk(r):
             for f in fs:
                 os.chdir(r)
-                sourcecmd += f+","
-
-                # os.system(sourcecmd)
-                # os.system(upgradecmd)
-upgradecmd = "mysql " + db_name+" -u " + db_user + " -p" + db_pass +" < " + sourcecmd
+                struc_sql = open(f, 'r', encoding="utf-8")
+                temp_sql.write(struc_sql.read())
+                struc_sql.close()
+    elif r[-3:] == "(i)":
+        for r, s, fs in os.walk(r):
+            for f in fs:
+                os.chdir(r)
+                initdata_sql = open(f, 'r', encoding="utf-8")
+                temp_sql.write(initdata_sql.read())
+                initdata_sql.close()
+temp_sql.seek(0)
+temp_sql.close()
+upgradecmd = "mysql " + db_name + " -u " + db_user + " -p" + db_pass + " < " + temp_sql.name
 os.system(upgradecmd)
-print(sourcecmd)
