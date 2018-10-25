2f51cd1852ce1c26cac48b1a31ec76228bfe0470
Fighter <805899926@qq.com>
2018-10-26 01:13:29 +0800

    temp

---
+++ script/representative.py
@@ -0,0 +1,15 @@
+[
+  {
+    "deviceId": "0fd5f0b40204",
+    "representativeName": "DS小雯"
+  }
+]
+
+//{
+//  "devices": [
+//    {
+//      "deviceId": "0fd5f0b40204",
+//      "representativeName": "DS小雯"
+//    }
+//  ]
+//}
