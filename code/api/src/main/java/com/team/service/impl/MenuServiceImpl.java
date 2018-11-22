497e3d41d8231ed2c2c0ba997670b99f16f52f8d
Fighter <805899926@qq.com>
2018-11-22 21:23:41 +0800

    java获取子节点未完成

--- code/api/src/main/java/com/team/service/impl/MenuServiceImpl.java
+++ code/api/src/main/java/com/team/service/impl/MenuServiceImpl.java
@@ -1,9 +1,15 @@
 package com.team.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.github.pagehelper.PageHelper;
+import com.team.core.extension.entity.MenuExtensions;
+import com.team.core.universal.PageModel;
 import com.team.entity.po.Menu;
 import com.team.service.MenuService;
 import com.team.core.universal.BaseServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @Description Menu 服务实现类
@@ -13,4 +19,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class MenuServiceImpl extends BaseServiceImpl<Menu> implements MenuService {
 
+
+    @Override
+    public List<Menu> getPermissionPageList(Integer pageIndex, Integer pageSize, String menuId) {
+//        PageHelper.startPage(pageIndex, pageSize);
+        List<Menu> temp = super.SelectListBy(new QueryWrapper<Menu>().eq("IsMenu", true));
+        return MenuExtensions.GetSons(temp, menuId);
+    }
 }
