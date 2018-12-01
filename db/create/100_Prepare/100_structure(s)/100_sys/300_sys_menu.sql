0e09d9c207b570a6e743c004990ea374527a637e
Fighter <805899926@qq.com>
2018-12-01 16:35:54 +0800

    修复数据库脚本

--- db/create/100_Prepare/100_structure(s)/100_sys/300_sys_menu.sql
+++ db/create/100_Prepare/100_structure(s)/100_sys/300_sys_menu.sql
@@ -10,6 +10,6 @@ CREATE TABLE `sys_menu`  (
   `AddedTime` datetime(0) DEFAULT NULL,
   `ModifiedTime` datetime(0) DEFAULT NULL,
   `AddedUser` varchar(36) DEFAULT NULL,
-  `ModifiedUser` varchar(36) DEFAULT NULL
+  `ModifiedUser` varchar(36) DEFAULT NULL,
   PRIMARY KEY (`MenuId`) 
 ) COMMENT='菜单权限表';
