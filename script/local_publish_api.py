a7b8519c37620a3fbf08d508a537ab7b19dfb8f7
Fighter <805899926@qq.com>
2018-12-03 21:39:39 +0800

    发布脚本优化

--- script/local_publish_api.py
+++ script/local_publish_api.py
@@ -2,8 +2,7 @@
 import os
 from common import temp_api_path, erp_home
 
-# os.system("python ./local_build_api.py")
-# api_publish = "scp -r " + temp_api_path + " erp:" + erp_home
-# os.system(api_publish)
-api_restart='ssh erp "cd api && ./test.sh --apiName erp"'
-os.system(api_restart)
+os.system("python ./local_build_api.py")
+os.system('ssh erp "cd api && ./restart.sh -d"')
+os.system("scp -r " + temp_api_path + " erp:" + erp_home)
+os.system('ssh erp "cd api && ./restart.sh -u"')
