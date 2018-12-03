039df141b9d76ce5de7f17f3cf3d5d90205ca09f
Fighter <805899926@qq.com>
2018-12-03 19:00:57 +0800

    测试脚本

--- script/local_publish_api.py
+++ script/local_publish_api.py
@@ -2,8 +2,8 @@
 import os
 from common import temp_api_path, erp_home
 
-os.system("python ./local_build_api.py")
-api_publish = "scp -r " + temp_api_path + " erp:" + erp_home
-os.system(api_publish)
-api_restart='ssh erp "cd api && ./restart.sh"'
+# os.system("python ./local_build_api.py")
+# api_publish = "scp -r " + temp_api_path + " erp:" + erp_home
+# os.system(api_publish)
+api_restart='ssh erp "cd api && ./test.sh --apiName erp"'
 os.system(api_restart)
