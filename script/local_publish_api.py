d661356a6b1e862846260459f581d58ff633eee7
Fighter <805899926@qq.com>
2018-12-03 18:41:05 +0800

    服务器api重启脚本完成

--- script/local_publish_api.py
+++ script/local_publish_api.py
@@ -5,5 +5,5 @@ from common import temp_api_path, erp_home
 os.system("python ./local_build_api.py")
 api_publish = "scp -r " + temp_api_path + " erp:" + erp_home
 os.system(api_publish)
-# api_restart='ssh erp "cd api && ./restart.sh"'
-# os.system(api_restart)
+api_restart='ssh erp "cd api && ./restart.sh"'
+os.system(api_restart)
