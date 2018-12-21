0e3ed78113da3700ae6d6dee466800d5746cebf8
Fighter <805899926@qq.com>
2018-12-21 11:57:06 +0800

    修复aop

--- code/api/erp-operation/src/main/java/com/team/aop/AspectLog.java
+++ code/api/erp-operation/src/main/java/com/team/aop/AspectLog.java
@@ -6,8 +6,10 @@ import com.team.util.DateTimeUtil;
 import com.team.util.HttpClientUtil;
 import com.team.util.JwtUtil;
 import com.team.entity.system.Log;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import net.sf.jsqlparser.statement.select.Join;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
@@ -26,26 +28,32 @@ public class AspectLog {
     ThreadLocal<Long> startTime = new ThreadLocal<>();
 
     @Pointcut("execution(* com.team.controller..*.*(..))")
-    public void methodCachePointcut() {
+    public void excudeController() {
 
     }
 
-    @Before("methodCachePointcut()")
+    @Pointcut("@annotation(io.swagger.annotations.ApiOperation)")
+    public void apiAnnotation() {
+
+    }
+
+    @Before("excudeController()&&apiAnnotation()")
     public void doBefore() {
         startTime.set(System.currentTimeMillis());
     }
 
-    @AfterReturning("methodCachePointcut()")
-    public void doAfterReturning(JoinPoint point) {
-        if (!isNoLog(point)) {
-            Log sysLog = getSystemLogInit(point);
+    @AfterReturning("excudeController()&&apiAnnotation()")
+    public void doAfterReturning(JoinPoint joinPoint) {
+        Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
+        if (!isNoLog(method) && isApiOperation(method)) {
+            Log sysLog = getSystemLogInit(joinPoint);
             sysLog.setLogType(Log.logInfo);
             sysLog.setSpendTime((int) (System.currentTimeMillis() - startTime.get()));
             logQueue.add(sysLog);
         }
     }
 
-    @AfterThrowing(pointcut = "methodCachePointcut()", throwing = "e")
+    @AfterThrowing(pointcut = "excudeController()&&apiAnnotation()", throwing = "e")
     public void doAfterThrowing(JoinPoint p, Throwable e) {
         if (!(e instanceof ServiceException)) {
             try {
@@ -55,38 +63,38 @@ public class AspectLog {
                 sysLog.setExceptionDetail(e.getMessage());
                 logQueue.add(sysLog);
             } catch (Exception ex) {
-                log.error("==异常通知异常==");
                 log.error("异常信息:{}", ex.getMessage());
             }
         }
     }
 
-    public Boolean isNoLog(JoinPoint joinPoint) {
-        Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
-        NoLog logAttr = method.getAnnotation(NoLog.class);
-        return logAttr != null;
+    private Boolean isNoLog(Method method) {
+        return method.isAnnotationPresent(NoLog.class);
     }
 
-    private Log getSystemLogInit(JoinPoint p) {
+    private Boolean isApiOperation(Method method) {
+        return method.isAnnotationPresent(ApiOperation.class);
+    }
+
+    private String getMethodRemark(JoinPoint joinPoint) {
+        Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
+        ApiOperation methodCache = method.getAnnotation(ApiOperation.class);
+        return methodCache.value();
+    }
+
+    private Log getSystemLogInit(JoinPoint joinPoint) {
         Log sysLog = new Log();
         try {
-            String targetClass = p.getTarget().getClass().getSimpleName();
-            String tartgetMethod = p.getSignature().getName();
-            sysLog.setDescription(getMethodRemark(p));
+            String targetClass = joinPoint.getTarget().getClass().getSimpleName();
+            String tartgetMethod = joinPoint.getSignature().getName();
+            sysLog.setDescription(getMethodRemark(joinPoint));
             sysLog.setMethod(targetClass + "->" + tartgetMethod);
             sysLog.setRequestIp(HttpClientUtil.getClientIp());
             sysLog.setUserId(JwtUtil.getCurrentUser().getUserId());
             sysLog.setAddedTime(DateTimeUtil.getCurrentDateTime());
         } catch (Exception ex) {
-            log.error("==异常通知异常==");
             log.error("异常信息:{}", ex.getMessage());
         }
         return sysLog;
-    }
-
-    private static String getMethodRemark(JoinPoint joinPoint) {
-        Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
-        ApiOperation methodCache = method.getAnnotation(ApiOperation.class);
-        return methodCache.value();
     }
 }
