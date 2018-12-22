f4f4299f39335cb3a0985c78593f4334c08c9308
Fighter <805899926@qq.com>
2018-12-22 22:37:52 +0800

    优化前端发布脚本

--- script/local_publish_op.py
+++ script/local_publish_op.py
@@ -3,5 +3,6 @@ import os
 from common import temp_operation_path, erp_home
 
 os.system("python ./local_build_op.py")
+os.system('ssh erp "cd web/operation && rm -rf *"')
 op_publish = "scp -r " + temp_operation_path + " erp:" + erp_home + "/web"
 os.system(op_publish)
