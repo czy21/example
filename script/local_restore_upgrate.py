a132d2d47536db70505a43379113568ed9edada7
Fighter <805899926@qq.com>
2018-12-24 19:03:12 +0800

    temp

--- script/local_restore_upgrate.py
+++ script/local_restore_upgrate.py
@@ -4,7 +4,7 @@ from common import version_path, temp_db_path, db_name, db_user, db_pass, db_bak
 
 dumpcmd = "mysqldump " + db_bak_name + " -u " + db_user + " -p" + db_pass + " --add-drop-table " + "| " + "mysql " + db_name + " -u " + db_user + " -p" + db_pass
 os.system(dumpcmd)
-all_sql = open(temp_db_path + "\\prepare.sql", "w+", encoding="utf8")
+all_sql = open(temp_db_path + "\\all_in_one.sql", "w+", encoding="utf8")
 for r, s, fs in os.walk(version_path):
     if r[-3:] == "(s)":
         for r, s, fs in os.walk(r):
@@ -19,5 +19,6 @@ for r, s, fs in os.walk(version_path):
                 all_sql.write(init_data_sql.read())
                 init_data_sql.close()
 all_sql.close()
-upgradecmd = "mysql " + db_name + " -u " + db_user + " -p" + db_pass + " < " + all_sql.name
-os.system(upgradecmd)
+print(all_sql)
+# upgradecmd = "mysql " + db_name + " -u " + db_user + " -p" + db_pass + " < " + all_sql.name
+# os.system(upgradecmd)
