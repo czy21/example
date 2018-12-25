62e4ef5f774146928bb85ab6673ac75bd532c516
Fighter <805899926@qq.com>
2018-12-25 23:40:43 +0800

    优化脚本

--- script/local/local_restore_upgrate.py
+++ script/local/local_restore_upgrate.py
@@ -1,31 +1,44 @@
 #!/usr/bin/env python3
 import os
-from default.path_default import temp_db_path, version_path
+import re
+from subprocess import Popen, PIPE
 from default.local_default import local
+from default.path_default import temp_db_path, version_path
+from default.basic_config \
+    import update_release_config_sql, \
+    select_release_config_sql, \
+    user_param, \
+    migrate_db_sql, \
+    mysql_cmd
 
-local_user = " -u" + local.db_user + " -p" + local.db_pass
-local_port = " -P" + local.db_port + " "
-dumpcmd = "mysqldump" + local_port + local.db_bak_name + local_user + " --add-drop-table " + "| " + "mysql" + local_port + local.db_name + local_user
-#
-# "mysqldump" + " -P" + prod.db_port + " " + prod.db_bak_name + " -u" + prod.db_user + " -p" + prod.db_pass + " --add-drop-table " + "| " + "mysql" + " -P" + local.db_port + " " + local.db_bak_name + " -u" + local.db_user + " -p" + local.db_pass
-print(dumpcmd)
-print(local_user)
-# os.system(dumpcmd)
-# all_sql = open(temp_db_path + "\\all_in_one.sql", "w+", encoding="utf8")
-# for r, s, fs in os.walk(version_path):
-#     if r[-3:] == "(s)":
-#         for r, s, fs in os.walk(r):
-#             for f in fs:
-#                 structure_sql = open(r + "\\" + f, 'r', encoding="utf-8")
-#                 all_sql.write(structure_sql.read() + "\n")
-#                 structure_sql.close()
-#     elif r[-3:] == "(i)":
-#         for r, s, fs in os.walk(r):
-#             for f in fs:
-#                 init_data_sql = open(r + "\\" + f, 'r', encoding="utf-8")
-#                 all_sql.write(init_data_sql.read())
-#                 init_data_sql.close()
-# all_sql.close()
-# print(all_sql)
-# upgradecmd = "mysql " + db_name + " -u " + db_user + " -p" + db_pass + " < " + all_sql.name
-# os.system(upgradecmd)
+local_user = user_param(local.db_port, local.db_user, local.db_pass)
+
+dump_cmd = migrate_db_sql(local.db_bak_name, local_user, local.db_name, local_user)
+os.system(dump_cmd)
+release_config = select_release_config_sql(local_user)
+db_version_value = re.findall("\d+", str(Popen(release_config, stdout=PIPE, stderr=PIPE).stdout.readline()))[0]
+phy_max_db_path = max(os.listdir(version_path))
+
+if phy_max_db_path.endswith("v"):
+    if phy_max_db_path[: -1] > db_version_value:
+        max_version_dir_name = phy_max_db_path[:-1]
+        all_sql = open(temp_db_path + "\\all_in_one.sql", "w+", encoding="utf8")
+        for r, s, fs in os.walk(version_path + "\\" + phy_max_db_path):
+            if r[-3:] == "(s)":
+                for r, s, fs in os.walk(r):
+                    for f in fs:
+                        structure_sql = open(r + "\\" + f, 'r', encoding="utf-8")
+                        all_sql.write(structure_sql.read() + "\n")
+                        structure_sql.close()
+            if r[-3:] == "(i)":
+                for r, s, fs in os.walk(r):
+                    for f in fs:
+                        init_data_sql = open(r + "\\" + f, 'r', encoding="utf-8")
+                        all_sql.write(init_data_sql.read())
+                        init_data_sql.close()
+        all_sql.close()
+        upgrade_cmd = mysql_cmd(local.db_name, local_user) \
+                      + " < " + all_sql.name + " >" \
+                      + os.path.basename(__file__) + ".log"
+        update_release_config = update_release_config_sql(local_user, phy_max_db_path[:-1])
+        os.system(upgrade_cmd + " && " + update_release_config)
