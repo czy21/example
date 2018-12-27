0bb2dab169c276f5d3e08a8f646772dd146e346a
Fighter <805899926@qq.com>
2018-12-27 19:15:44 +0800

    temp

--- code/api/erp-application/src/main/java/com/team/entity/page/PageParams.java
+++ code/api/erp-application/src/main/java/com/team/entity/page/PageParams.java
@@ -5,6 +5,6 @@ import lombok.Data;
 @Data
 public class PageParams {
     private Integer pageIndex = 1;
-    private Integer pageSize = 15;
+    private Integer pageSize = 20;
     private Integer total;
 }
