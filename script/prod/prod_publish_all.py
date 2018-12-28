b3fc4a2b7982147701ffc04b1eb5ea102617e0a3
Fighter <805899926@qq.com>
2018-12-28 20:34:12 +0800

    finished db script

--- script/prod/prod_publish_all.py
+++ script/prod/prod_publish_all.py
@@ -3,3 +3,4 @@ import os
 
 os.system("python ../local/local_publish_api.py")
 os.system("python ../local/local_publish_op.py")
+os.system("python ./prod_publish_db.py")
--- script/prod/prod_publish_db.py
+++ script/prod/prod_publish_db.py
@@ -11,10 +11,12 @@ from local.local_build_version_sql import built_version_sql_path
 db_version = input("please input db dir version: ")
 
 if db_version.endswith("v"):
+    update_release_config = update_release_config_sql(prod_user, db_version[:-1])
     if os.path.exists(version_path + "\\" + db_version):
         os.system("python ./prod_backup_db_to_bak.py")
-        os.system("python ./prod_restore_upgrate.py")
+        os.system("python ./prod_restore_db.py")
         upgrade_cmd = mysql_cmd(prod.db_name, prod_user) \
                       + import_sql_file(built_version_sql_path(db_version), os.path.basename(__file__))
-        update_release_config = update_release_config_sql(prod_user, db_version[:-1])
         os.system(update_release_config + "&&" + update_release_config)
+    else:
+        os.system(update_release_config)
