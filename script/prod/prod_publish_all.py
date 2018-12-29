ea2fe1778403fbb889e0123fd853670f9bc0630a
Fighter <805899926@qq.com>
2018-12-29 10:25:26 +0800

    fix script

--- script/prod/prod_publish_all.py
+++ script/prod/prod_publish_all.py
@@ -1,6 +1,5 @@
 # !/usr/bin/env python
 import os
 
-os.system("python ../local/local_publish_api.py")
-os.system("python ../local/local_publish_op.py")
-os.system("python ./prod_publish_db.py")
+os.system("python ./prod_publish_api.py")
+os.system("python ./prod_publish_op.py")
