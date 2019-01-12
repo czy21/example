5cc6a5b9d6d4ad6627323c18c1ee20c1917a00d3
Fighter <805899926@qq.com>
2019-01-12 21:36:34 +0800

    temp

--- code/api/erp-application/src/main/java/com/team/core/universal/MongoBaseService.java
+++ code/api/erp-application/src/main/java/com/team/core/universal/MongoBaseService.java
@@ -3,4 +3,5 @@ package com.team.core.universal;
 import com.team.entity.BaseEntity;
 
 public interface MongoBaseService<TEntity extends BaseEntity> {
+
 }
--- code/api/erp-application/src/main/java/com/team/core/universal/MongoBaseServiceImpl.java
+++ code/api/erp-application/src/main/java/com/team/core/universal/MongoBaseServiceImpl.java
@@ -1,6 +1,16 @@
 package com.team.core.universal;
 
 import com.team.entity.BaseEntity;
+import com.team.entity.page.PageModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public class MongoBaseServiceImpl<TEntity extends BaseEntity> implements MongoBaseService<TEntity> {
+
+    @Autowired
+    private MongoRepository mongoRepository;
 }
