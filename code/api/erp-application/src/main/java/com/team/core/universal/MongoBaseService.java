af3b12c3d88b2635b4c3f685fc409b2dfee59810
Fighter <805899926@qq.com>
2019-01-20 23:15:12 +0800

    temp

--- code/api/erp-application/src/main/java/com/team/core/universal/MongoBaseService.java
+++ code/api/erp-application/src/main/java/com/team/core/universal/MongoBaseService.java
@@ -1,7 +1,11 @@
 package com.team.core.universal;
 
 import com.team.entity.BaseEntity;
+import com.team.entity.page.PageModel;
+import org.springframework.data.domain.Example;
 
 public interface MongoBaseService<TEntity extends BaseEntity> {
+
+    PageModel<TEntity> SelectPageListBy(Integer pageIndex, Integer pageSize, Example<TEntity> query);
 
 }
--- code/api/erp-application/src/main/java/com/team/service/impl/LogServiceImpl.java
+++ code/api/erp-application/src/main/java/com/team/service/impl/LogServiceImpl.java
@@ -5,15 +5,11 @@ import com.team.entity.dto.LogDto;
 import com.team.entity.dto.PageDto;
 import com.team.entity.map.LogMap;
 import com.team.entity.mongo.Log;
-import com.team.entity.page.PageModel;
 import com.team.model.SeachLogModel;
 import com.team.service.LogService;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @Service
 public class LogServiceImpl extends MongoBaseServiceImpl<Log> implements LogService {
@@ -22,9 +18,7 @@ public class LogServiceImpl extends MongoBaseServiceImpl<Log> implements LogServ
     private LogMap logMap;
 
     @Override
-    public Page getLogPageListBy(SeachLogModel search) {
-        Page page= mongoRepository.findAll(PageRequest.of(search.getPageIndex() - 1, search.getPageSize()));
-        return page;
-//        return logMap.toPageDto(new PageModel<>(search.getPageIndex(), search.getPageSize(), list));
+    public PageDto<LogDto> getLogPageListBy(SeachLogModel search) {
+        return logMap.mapToPageDto(super.SelectPageListBy(search.getPageIndex(), search.getPageSize(), null));
     }
 }
