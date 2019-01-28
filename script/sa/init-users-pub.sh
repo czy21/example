1b9ed07735e5b3708e38120bb47c677ad75d5d11
Fighter <805899926@qq.com>
2019-01-28 15:35:57 +0800

    temp

--- script/sa/init-users-pub.sh
+++ script/sa/init-users-pub.sh
@@ -5,4 +5,6 @@ for file in $(ls ./pubs/*.pub);
     do 
     rsa_pub="$rsa_pub"command=\"read\",no-X11-forwarding,no-agent-forwarding,no-pty,no-user-rc"`cat $file`\n"
 done
-./user-tunnel.sh cent_a -create < echo -e $rsa_pub
+echo -e $rsa_pub > temp
+./user-tunnel.sh cent_a -create < temp
+rm -rf temp
