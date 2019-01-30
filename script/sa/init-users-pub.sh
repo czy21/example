0508c59c8b6ee3567e920e6b2ac1a4ceff3ddc54
Fighter <805899926@qq.com>
2019-01-30 16:26:02 +0800

    fix

--- script/sa/init-users-pub.sh
+++ script/sa/init-users-pub.sh
@@ -4,7 +4,7 @@ set -e
 
 for file in $(ls ./pubs/*.pub);
     do 
-    tunnel_pubs="$tunnel_pubs"command=\"read\",no-X11-forwarding,no-agent-forwarding,no-pty,no-user-rc"`cat $file`\n"
+    tunnel_pubs="$tunnel_pubs"command=\"read\",no-X11-forwarding,no-agent-forwarding,no-pty,no-user-rc" `cat $file`\n"
     erp_pubs="$erp_pubs`cat $file`\n"
 done
 echo -e $tunnel_pubs > tunnel_temp
