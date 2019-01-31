c374c5ff08441dfdfd4ce2de3d8d8a0b17cce9e7
Fighter <805899926@qq.com>
2019-01-31 17:18:48 +0800

    improve ssh script

--- script/prod/prod_publish_op.py
+++ script/prod/prod_publish_op.py
@@ -3,13 +3,16 @@ import os
 import sys
 
 sys.path.append("..")
+from default.basic_config import ssh_config
 from default.temp_path import temp_operation_path
+
+remote = ssh_config("ssh")
+ssh_user = remote.user_name + "@" + remote.host_name
 
 
 def run():
-    os.system('ssh erp@prod_erp "rm -rf ./web/operation/*"')
-    os.system("scp -r " + temp_operation_path + " erp@prod_erp:web")
-
+    print('ssh ' + ssh_user + ' "rm -rf ./web/operation/*"')
+    print("scp -r " + temp_operation_path + " " + ssh_user + ":web")
 
 if __name__ == '__main__':
-    run()
+        run()
