dbee7b98587239b885086bb00c4b8cb9eefaf2ee
Fighter <805899926@qq.com>
2019-02-01 23:38:46 +0800

    fix script

--- script/prod/prod_publish_api.py
+++ script/prod/prod_publish_api.py
@@ -5,6 +5,7 @@ import sys
 sys.path.append("..")
 from default.basic_config import ssh_config
 from default.temp_path import temp_api_path
+from local import local_build_api
 
 remote = ssh_config("ssh")
 ssh_user = remote.user_name + "@" + remote.host_name
@@ -17,5 +18,6 @@ def run():
 
 
 if __name__ == '__main__':
+    local_build_api.build_to_temp()
     run()
     os.system("pause")
--- script/prod/prod_publish_op.py
+++ script/prod/prod_publish_op.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append("..")
 from default.basic_config import ssh_config
 from default.temp_path import temp_operation_path
-
+from local import local_build_op
 remote = ssh_config("ssh")
 ssh_user = remote.user_name + "@" + remote.host_name
 
@@ -16,5 +16,6 @@ def run():
 
 
 if __name__ == '__main__':
+    local_build_op.build_to_temp()
     run()
     os.system("pause")
