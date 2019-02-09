6b2fc80335d82476e7c51169dc937035e5cd6d78
Fighter <805899926@qq.com>
2019-02-09 19:09:33 +0800

    add baidu uuidgenerator

--- code/api/erp-application/src/main/java/com/team/core/configure/MybatisPlusConfigure.java
+++ code/api/erp-application/src/main/java/com/team/core/configure/MybatisPlusConfigure.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan(basePackages = "com.team.repository.mybatis")
+@MapperScan({"com.team.repository.mybatis","com.baidu.fsg.uid.worker.dao"})
 public class MybatisPlusConfigure {
 
 }
