4668c7dffd03f3dccdaac888c3cfcf9df25b0575
Fighter <805899926@qq.com>
2019-02-17 21:53:23 +0800

    improve script

--- script/prod/prod_backup_db_bak_to_local.py
+++ script/prod/prod_backup_db_bak_to_local.py
@@ -2,9 +2,11 @@
 import os, sys
 
 sys.path.append("..")
-from default.basic_config import mysql_migrate_db
+from default.basic_config import mysql_migrate_db, mysql_create_db
 from default.local_default import local_mysql, local_mysql_user
 from default.prod_default import prod_mysql, prod_mysql_user
 
+create_db_cmd = mysql_create_db(local_mysql_user, local_mysql.db_bak_name)
 dump_cmd = mysql_migrate_db(prod_mysql.db_bak_name, prod_mysql_user, local_mysql.db_bak_name, local_mysql_user)
+os.system(create_db_cmd)
 os.system(dump_cmd)
