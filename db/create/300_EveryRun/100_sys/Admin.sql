90f2fc4910073c9ff1d986045ebbdf25680680c6
Fighter <805899926@qq.com>
2019-03-05 18:03:47 +0800

    improve

--- db/create/300_EveryRun/100_sys/Admin.sql
+++ db/create/300_EveryRun/100_sys/Admin.sql
@@ -1,8 +1,5 @@
-set @recent_seq_value = (select wn.sequence_value from worker_node wn order by wn.modified_time desc limit 1);
 
-set @max_role_function_id = (select max(rf.role_function_id) from sys_role_function rf);
-
-set @role_function_id = (select ifnull(@max_role_function_id,@recent_seq_value));
+set @role_function_id = max_target_table_id{{role_function_id,sys_role_function}};
 
 INSERT INTO sys_role_function(`role_function_id`,`role_id`, `function_id`, `added_time`, `modified_time`, `added_user`, `modified_user`)
 select (@role_function_id:=@role_function_id + 1) AS role_function_id,r.role_id,f.function_id,now(),now(),null,null from sys_role r
