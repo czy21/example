1b9163fc84e926e8667f3bf787c39ecf4ff9b90f
Fighter <805899926@qq.com>
2019-03-06 17:51:40 +0800

    temp

--- script/local/local_restore_upgrate.py
+++ script/local/local_restore_upgrate.py
@@ -11,14 +11,16 @@ from local.local_build_db import built_version_sql
 
 create_db_cmd = mysql_create_db(local_mysql_user, local_mysql.db_name)
 dump_cmd = mysql_migrate_db(local_mysql.db_bak_name, local_mysql_user, local_mysql.db_name, local_mysql_user)
-os.system(create_db_cmd)
-os.system(dump_cmd);
 
 release_config = mysql_select_release_config(local_mysql.db_bak_name, local_mysql_user)
 db_version_value = re.findall("\d+", str(Popen(release_config, stdout=PIPE, stderr=PIPE).stdout.readline()))[0]
 phy_max_db_dir = max(os.listdir(db_version_path))
-if phy_max_db_dir.endswith("v"):
+
+if __name__ == '__main__':
+    all_cmd = create_db_cmd + "&&\n" + dump_cmd
     if phy_max_db_dir[: -1] > db_version_value:
         db_dir_val = built_version_sql(phy_max_db_dir)
         upgrade_cmd = mysql_cmd(local_mysql.db_name, local_mysql_user) + mysql_import_sql_file(db_dir_val, os.path.basename(__file__))
-        os.system(upgrade_cmd)
+        all_cmd += "&&\n" + upgrade_cmd
+    print(all_cmd)
+    os.system(all_cmd.replace("\n", ""))
