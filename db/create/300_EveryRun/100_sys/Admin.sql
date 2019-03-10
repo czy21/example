85e714925068c02f601f06d3b89a97817372e820
Fighter <805899926@qq.com>
2019-03-10 15:07:52 +0800

    improve script

--- db/create/300_EveryRun/100_sys/Admin.sql
+++ db/create/300_EveryRun/100_sys/Admin.sql
@@ -6,3 +6,11 @@ select (@max_role_function_id:=@max_role_function_id + 1) AS role_function_id,r.
 cross join sys_function f 
 left join sys_role_function rf on rf.role_id = r.role_id and rf.function_id = f.function_id
 where r.role_name='管理员' and rf.role_function_id is null;
+
+set @max_role_menu_id = max_target_table_id{{role_menu_id,sys_role_menu}};
+
+INSERT INTO sys_role_menu(`role_menu_id`,`role_id`, `menu_id`, `added_time`, `modified_time`, `added_user`, `modified_user`)
+select (@max_role_menu_id:=@max_role_menu_id + 1) AS role_menu_id,r.role_id,m.menu_id,now(),now(),null,null from sys_role r
+cross join sys_menu m 
+left join sys_role_menu rm on rm.role_id = r.role_id and rm.menu_id = m.menu_id
+where r.role_name='管理员' and rm.role_menu_id is null;
--- script/local/local_build_db.py
+++ script/local/local_build_db.py
@@ -1,5 +1,7 @@
 # !/usr/bin/env python
-import os, sys, re
+import os
+import re
+import sys
 
 sys.path.append("..")
 from default.path_default import db_version_path, db_everyRun_path
@@ -8,13 +10,17 @@ from default.temp_path import temp_db_path
 
 # max_target_table_id{{table_id,table_name}};
 def parse_max_target_table_id(source_sql_str):
-    match_result = re.findall(r"max_target_table_id{{(.+?)}}", source_sql_str, re.M)
-    if match_result.__len__() == 0:
-        return source_sql_str
-    for target in match_result:
-        max_sql = "(select ifnull((select max(" + target.split(",")[0] + ")from " + target.split(",")[1] + ")" \
-                  + ",(select wn.sequence_value from worker_node wn order by wn.modified_time desc limit 1)))"
-        return re.sub(r"max_target_table_id{{" + target.split(",")[0] + "," + target.split(",")[1] + "}}", max_sql, source_sql_str)
+    all_lines = []
+    for cur_line in source_sql_str:
+        match_result = ''.join(re.findall(r"max_target_table_id{{(.+?)}}", cur_line, re.M))
+        if match_result != '':
+            max_sql = "(select ifnull((select max(" + match_result.split(",")[0] + ")from " + match_result.split(",")[1] + ")" \
+                      + ",(select wn.sequence_value from worker_node wn order by wn.modified_time desc limit 1)))"
+            all_lines.append(re.sub(r"max_target_table_id{{" + match_result.split(",")[0] + "," + match_result.split(",")[1] + "}}", max_sql, cur_line))
+        else:
+            all_lines.append(cur_line)
+
+    return ''.join(all_lines)
 
 
 def built_version_sql(version_dir_val):
@@ -24,24 +30,24 @@ def built_version_sql(version_dir_val):
             for tr, tds, tfs in os.walk(r):
                 for f in tfs:
                     structure_sql = open(tr + "\\" + f, 'r', encoding="utf-8")
-                    all_sql.write(parse_max_target_table_id(structure_sql.read()) + "\n")
+                    all_sql.write(parse_max_target_table_id(structure_sql.readlines()) + "\n")
                     structure_sql.close()
         if r[-3:] == "(i)":
             for tr, tds, tfs in os.walk(r):
                 for f in tfs:
                     init_data_sql = open(tr + "\\" + f, 'r', encoding="utf-8")
-                    all_sql.write(parse_max_target_table_id(init_data_sql.read()) + "\n")
+                    all_sql.write(parse_max_target_table_id(init_data_sql.readlines()) + "\n")
                     init_data_sql.close()
     for r, ds, fs in os.walk(db_everyRun_path):
         for f in fs:
             increment_sql = open(r + "\\" + f, 'r', encoding="utf-8")
-            all_sql.write(parse_max_target_table_id(increment_sql.read()) + "\n")
+            all_sql.write(parse_max_target_table_id(increment_sql.readlines()) + "\n")
             increment_sql.close()
     all_sql.close()
     return all_sql.name
 
 
 if __name__ == '__main__':
-    db_version = input("please input db version: ")
-    built_version_sql(db_version + 'v')
+    db_version = input("please input db version: ") + 'v'
+    built_version_sql(db_version)
     os.system("pause")
