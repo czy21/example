e14cb4a8722d80995c709adcc47ed5295df121db
Fighter <805899926@qq.com>
2019-03-13 22:51:04 +0800

    temp

--- config/mysql/run.cmd
+++ config/mysql/run.cmd
@@ -1,1 +1,4 @@
-docker run --name mysql-master -p 30579:3306 -v /d/mysql/data:/var/lib/mysql -v /d/mysql/mysql.cnf:/etc/mysql/conf.d/mysql.cnf -v /d/mysql/mysqld.cnf:/etc/mysql/mysql.conf.d/mysqld.cnf -e MYSQL_ROOT_PASSWORD=sasa -d mysql:5.7
+@echo off
+docker run --name mysql-master -p 30579:3306 -v /d/Database/MySQL/Docker/Master/Data:/var/lib/mysql -v /d/Database/MySQL/Docker/Master/mysql.cnf:/etc/mysql/conf.d/mysql.cnf -v /d/Database/MySQL/Docker/Master/mysqld.cnf:/etc/mysql/mysql.conf.d/mysqld.cnf -e MYSQL_ROOT_PASSWORD=sasa -d mysql:5.7
+
+pause
