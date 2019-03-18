98417aee5e26c95f7a75d70ab4c84893bc56385e
Fighter <805899926@qq.com>
2019-03-19 00:34:14 +0800

    improve unfinished

--- script/default/local_default.py
+++ script/default/local_default.py
@@ -1,5 +1,5 @@
 # !/usr/bin/env python
-from default.basic_config import mysql_db_config, mysql_user_param, mongo_db_config
+from default.basic_config import mysql_db_config, mysql_obj
 
 local_mysql = mysql_db_config("local_mysql")
-local_mysql_user = mysql_user_param(local_mysql.db_port, local_mysql.db_user, local_mysql.db_pass)
+local_mysql_user = mysql_obj(t_port=local_mysql.db_port, t_user=local_mysql.db_user, t_pwd=local_mysql.db_pass)
