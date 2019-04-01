ffa6eacac6df89fd70e81fc96a36e5f8589af261
Bruce <805899926@qq.com>
2019-04-01 13:18:24 +0800

    improve harbor script

--- script/sa/docker/docker-install.sh
+++ script/sa/docker/docker-install.sh
@@ -2,13 +2,14 @@
 
 set -e
 
+local_ip=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d '/')
 sudo mkdir -p /etc/docker
-sudo tee /etc/docker/daemon.json <<-'EOF'
+sudo bash -c 'echo -e "
 {
-  "registry-mirrors": ["https://idyylogn.mirror.aliyuncs.com"],
-  "insecure-registries":["192.168.36.140"]
-}
-EOF
+  \"registry-mirrors\": [\"https://idyylogn.mirror.aliyuncs.com\"],
+  \"insecure-registries\":[\"'$local_ip'\"]
+}" > /etc/docker/daemon.json'
+
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 sudo yum makecache fast
@@ -16,7 +17,7 @@ sudo yum -y install docker-ce
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 
-# docker compose
+# # docker compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
