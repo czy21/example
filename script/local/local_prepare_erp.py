2ace00ff6d6145bf7e6c72f99aa9b281560d2690
Bruce CHEN <805899926@qq.com>
2019-04-27 19:36:35 +0800

    improve script

--- script/local/local_prepare_erp.py
+++ script/local/local_prepare_erp.py
@@ -1,11 +1,12 @@
 # !/usr/bin/env python
-import os, sys
+import os
+import sys
 
 sys.path.append("..")
 from default.path_default import db_prepare_path
 from default.temp_path import temp_db_path
 from default.local_default import local_mysql, local_mysql_user_param
-from default.basic_config import mysql
+from default.basic_config import mysql_handler
 
 prepare_sql = open(temp_db_path + "\\prepare.sql", "w+", encoding="utf8")
 for r, s, fs in os.walk(db_prepare_path):
@@ -22,6 +23,6 @@ for r, s, fs in os.walk(db_prepare_path):
                 prepare_sql.writelines(init_data_sql.read())
                 init_data_sql.close()
 prepare_sql.close()
-upgrade_cmd = mysql(t_db=local_mysql.db_name, t_user_param=local_mysql_user_param).obtain_exec_cmd() + " < " + prepare_sql.name
+upgrade_cmd = mysql_handler(t_db=local_mysql.db_name, t_user_param=local_mysql_user_param).obtain_exec_cmd() + " < " + prepare_sql.name
 print(upgrade_cmd)
 # os.system(upgrade_cmd)
--- script/test/input.py
+++
@@ -1,5 +0,0 @@
-# !/usr/bin/env python
-import os
-
-str = input("请输入: ")
-print("你输入的是什么?", str)
