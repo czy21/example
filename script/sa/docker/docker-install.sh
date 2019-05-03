5099585bcc49239b85c49f78786e4c88c71bc8d2
Bruce CHEN <805899926@qq.com>
2019-05-03 20:40:01 +0800

    improve script
    # Conflicts:
    #	code/api/erp-operation/src/main/resources/application.yml

--- script/sa/docker/docker-install.sh
+++ script/sa/docker/docker-install.sh
@@ -18,7 +18,7 @@ sudo systemctl daemon-reload
 sudo systemctl restart docker
 
 # # docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
 # docker machine
