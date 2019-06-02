2f74ed28c4d46d4a457d5e023dc1642abc3227e2
Bruce CHEN <805899926@qq.com>
2019-06-02 12:23:47 +0800

    temp

--- script/openwrt/proxy/init.sh
+++ script/openwrt/proxy/init.sh
@@ -4,8 +4,16 @@ set -e
 # $1: user@host
 # $2: ~/.ssh/[private_key]
 # example: ./init.sh user@host ~/.ssh/[private_key]
-# scp $2 $1:
-scp ./polipo/config $1:/etc/polipo/
+
+# ssh tunnel config
+scp $2 $1:
 scp -r ./init.d/* $1:/etc/init.d/
-ssh $1 "bash -c" \''chmod 600 ~/softium-bruce'\'
-ssh $1 "bash -c" \''/etc/init.d/ssh-socks enable;/etc/init.d/ssh-http enable;reboot;'\'
+ssh $1 "ash -c" \''chmod 600 /root/softium-bruce'\'
+ssh $1 "ash -c" \''/etc/init.d/ssh-socks5 enable;'\'
+
+# redsocks config
+scp ./redsocks/redsocks.conf $1:/etc/
+ssh $1 "ash -c" \''/etc/init.d/redsocks enable;'\'
+
+# reboot
+ssh $1 "ash -c" \''reboot;'\'
