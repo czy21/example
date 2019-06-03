13cf87442972c86234deeec5565ae7848bb52701
Bruce CHEN <805899926@qq.com>
2019-06-03 22:51:15 +0800

    #temp

--- script/sa/install-tool.sh
+++ script/sa/install-tool.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # jdk
 sudo wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn-pub/java/jdk/12.0.1+12/69cfe15208a647278a19ef0990eea691/jdk-12.0.1_linux-x64_bin.rpm
