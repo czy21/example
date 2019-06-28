5204d7b7a9612f55c7e17b94c861fc9241e839a6
Bruce CHEN <805899926@qq.com>
2019-06-28 23:11:30 +0800

    rm orignal api version bug no fix

--- code/api/erp-application/src/main/java/com/team/core/configure/RepositoryConfigure.java
+++ code/api/erp-application/src/main/java/com/team/core/configure/RepositoryConfigure.java
@@ -1,12 +1,14 @@
 package com.team.core.configure;
 
+import com.team.repository.mongo.MongoBaseRepository;
 import com.team.repository.mongo.MongoBaseRepositoryImpl;
+import com.team.repository.mybatis.MybatisBaseRepository;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
-@EnableMongoRepositories(basePackages = "com.team.repository.mongo", repositoryBaseClass = MongoBaseRepositoryImpl.class)
-@MapperScan({"com.team.repository.mybatis"})
+@EnableMongoRepositories(basePackageClasses = MongoBaseRepository.class, repositoryBaseClass = MongoBaseRepositoryImpl.class)
+@MapperScan(basePackageClasses = MybatisBaseRepository.class)
 public class RepositoryConfigure {
 }
