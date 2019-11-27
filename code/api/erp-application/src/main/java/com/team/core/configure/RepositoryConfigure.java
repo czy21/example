6def5e94f5a1b5027c56c5fa6da7b3911c0735d7
Bruce CHEN <805899926@qq.com>
2019-11-27 21:07:39 +0800

    #Temp

--- code/api/erp-application/src/main/java/com/team/core/configure/RepositoryConfigure.java
+++ code/api/erp-application/src/main/java/com/team/core/configure/RepositoryConfigure.java
@@ -1,14 +1,14 @@
 package com.team.core.configure;
 
-import com.team.infrastructure.MongoBaseRepository;
 import com.team.infrastructure.MongoBaseRepositoryImpl;
 import com.team.infrastructure.MybatisBaseMapper;
+import com.team.repository.LogRepository;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
-@EnableMongoRepositories(basePackageClasses = MongoBaseRepository.class, repositoryBaseClass = MongoBaseRepositoryImpl.class)
-@MapperScan(basePackageClasses = MybatisBaseMapper.class)
+@EnableMongoRepositories(basePackageClasses = LogRepository.class, repositoryBaseClass = MongoBaseRepositoryImpl.class)
+@MapperScan(markerInterface = MybatisBaseMapper.class)
 public class RepositoryConfigure {
 }
