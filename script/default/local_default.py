94dbd53073ffd7041b21b2e0bdd64bfb3ea5f0cc
Bruce CHEN <805899926@qq.com>
2019-11-27 19:08:07 +0800

    remove

--- script/default/local_default.py
+++
@@ -1,5 +0,0 @@
-# !/usr/bin/env python
-from default.basic_config import mysql_config, mysql_handler
-
-local_mysql = mysql_config("local_mysql")
-local_mysql_user_param = mysql_handler(t_host=local_mysql.db_host, t_port=local_mysql.db_port, t_user=local_mysql.db_user, t_pwd=local_mysql.db_pass).t_user_param
--- script/default/prod_default.py
+++
@@ -1,5 +0,0 @@
-# !/usr/bin/env python
-from default.basic_config import mysql_config, mysql_handler
-
-prod_mysql = mysql_config("prod_mysql")
-prod_mysql_user_param = mysql_handler(t_host=prod_mysql.db_host, t_port=prod_mysql.db_port, t_user=prod_mysql.db_user, t_pwd=prod_mysql.db_pass).t_user_param
