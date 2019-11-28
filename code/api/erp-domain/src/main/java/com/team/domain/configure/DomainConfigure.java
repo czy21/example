ac0fc5dfae526e479a9e2793faa3d5193e0531ed
Bruce CHEN <805899926@qq.com>
2019-11-29 00:07:20 +0800

    improve

--- code/api/erp-domain/src/main/java/com/team/domain/configure/DomainConfigure.java
+++ code/api/erp-domain/src/main/java/com/team/domain/configure/DomainConfigure.java
@@ -1,7 +1,7 @@
 package com.team.domain.configure;
 
-import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import com.team.domain.infrastructure.base.MongoBaseRepositoryImpl;
+import com.team.domain.infrastructure.metadata.EntityMetadataHandler;
 import com.team.domain.mapper.UserMapper;
 import com.team.domain.repository.LogRepository;
 import org.mybatis.spring.annotation.MapperScan;
@@ -9,10 +9,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import java.util.Properties;
-
 @Configuration
 @EnableMongoRepositories(basePackageClasses = LogRepository.class, repositoryBaseClass = MongoBaseRepositoryImpl.class)
 @MapperScan(basePackageClasses = {UserMapper.class})
 public class DomainConfigure {
+    @Bean
+    public EntityMetadataHandler entityMetadataHandler() {
+        return new EntityMetadataHandler();
+    }
 }
--- code/api/erp-domain/src/main/java/com/team/domain/infrastructure/metadata/EntityMetadataHandler.java
+++ code/api/erp-domain/src/main/java/com/team/domain/infrastructure/metadata/EntityMetadataHandler.java
@@ -2,12 +2,11 @@ package com.team.domain.infrastructure.metadata;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
-import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
-@Component
-public class DomainMetadata implements MetaObjectHandler {
+
+public class EntityMetadataHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         this.setInsertFieldValByName("createdDate", LocalDateTime.now(), metaObject);
