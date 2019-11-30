a9d12fc70ea25763e1507b67556809f1e6231e2d
Bruce CHEN <805899926@qq.com>
2019-11-30 15:48:19 +0800

    add cooperated module

---
+++ code/api/cooperated/src/main/java/com/team/cooperated/CooperatedConfigure.java
@@ -0,0 +1,77 @@
+package com.team.cooperated;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.team.cooperated.json.LocalDateTimeDeserializer;
+import com.team.cooperated.json.LocalDateTimeSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+public class CooperatedConfigure implements WebMvcConfigurer {
+
+    private final ObjectMapper objectMapper;
+
+    public CooperatedConfigure(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setMaxPayloadLength(64000);
+        return loggingFilter;
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.removeIf(s -> s instanceof StringHttpMessageConverter);
+        converters.removeIf(s -> s instanceof MappingJackson2HttpMessageConverter);
+        converters.add(mappingJackson2HttpMessageConverter());
+    }
+
+    /**
+     * 基于默认Jackson2ObjectMapperBuilder之上添加自定义序列化反序列化的配置
+     *
+     * @author bruce
+     */
+    private MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        Jackson2ObjectMapperBuilder.json().configure(objectMapper);
+        SimpleModule customModule = new SimpleModule();
+        customModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer());
+        customModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer());
+        objectMapper.registerModule(customModule);
+        return new MappingJackson2HttpMessageConverter(objectMapper);
+    }
+
+    /**
+     * 配置忽略路径大小写
+     *
+     * @author bruce
+     */
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        AntPathMatcher matcher = new AntPathMatcher();
+        matcher.setCaseSensitive(false);
+        configurer.setPathMatcher(matcher);
+    }
+}
