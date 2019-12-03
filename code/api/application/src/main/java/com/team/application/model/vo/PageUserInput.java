d7789cd661455df4364d14f1bc4f532ab547b42e
Bruce CHEN <805899926@qq.com>
2019-12-03 22:34:59 +0800

    recursionObject finished

--- code/api/application/src/main/java/com/team/application/model/vo/PageUserInput.java
+++ code/api/application/src/main/java/com/team/application/model/vo/PageUserInput.java
@@ -3,8 +3,10 @@ package com.team.application.model.vo;
 import com.team.application.model.page.PageInput;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class PageUserInput {
     private PageInput page;
-    private UserVO filter;
+    private List<UserVO> filter;
 }
