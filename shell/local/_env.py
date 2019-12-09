58f84e6cc569d016c162f3e9b5aba1f9943e0fd0
Bruce CHEN <805899926@qq.com>
2019-12-09 11:01:15 +0800

    build api shell finished

--- shell/local/_env.py
+++ shell/local/_env.py
@@ -3,6 +3,9 @@
 from shell import env_common
 
 env_common.default_common.param_main_db_host = "127.0.0.1"
-env_common.default_common.param_main_db_port = "3306"
-env_common.default_common.param_main_db_name = "erp_java"
-env_common.default_common.param_main_db_bak_name = "erp_java_bak"
+
+# mysql
+env_common.default_common.param_main_db_mysql_port = "3306"
+
+# mongo
+env_common.default_common.param_main_db_mongo_port = "27017"
