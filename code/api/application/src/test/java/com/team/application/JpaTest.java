54f26a6172bd7b696f2e3c48ea459f75243f58e9
Bruce CHEN <805899926@qq.com>
2020-03-05 20:45:49 +0800

    #Bruce Tmp

--- code/api/application/src/test/java/com/team/application/JpaTest.java
+++ code/api/application/src/test/java/com/team/application/JpaTest.java
@@ -1,5 +1,6 @@
 package com.team.application;
 
+import com.team.application.util.DateTimeUtil;
 import com.team.domain.model.CompanyEntity;
 import com.team.domain.model.DepartmentEntity;
 import com.team.domain.repository.jpa.CompanyRepository;
@@ -8,8 +9,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -23,6 +31,9 @@ public class JpaTest {
     @Autowired
     DepartmentRepository departmentRepository;
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Test
     public void findAll1() {
         List<DepartmentEntity> entities = departmentRepository.findAll();
@@ -31,19 +42,37 @@ public class JpaTest {
 
     @Test
     public void add1() {
-        DepartmentEntity entity = new DepartmentEntity();
-        entity.setId(UUID.randomUUID().toString());
-        entity.setName("ceshi");
-        CompanyEntity companyEntity = new CompanyEntity();
-        companyEntity.setId("03aec595-24c4-11ea-9a7c-0242ac150002");
+        List<DepartmentEntity> entities = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            DepartmentEntity entity = new DepartmentEntity();
+            entity.setId(UUID.randomUUID().toString());
+            entity.setName("ceshi");
+            CompanyEntity companyEntity = new CompanyEntity();
+            companyEntity.setId("03aec595-24c4-11ea-9a7c-0242ac150002");
+            entities.add(entity);
+            entity.setCompany(companyEntity);
+        }
+        System.out.println(DateTimeUtil.toTimeStamp(LocalDateTime.now()));
+        entities.forEach(s-> departmentRepository.save(s));
+        List<DepartmentEntity> saveEntity = departmentRepository.saveAll(entities);
 
-        entity.setCompany(companyEntity);
+//        jdbcTemplate.batchUpdate("insert into ent_sys_department(id,name,company_id) values (?,?,?)", new BatchPreparedStatementSetter() {
+//            @Override
+//            public void setValues(PreparedStatement ps, int i) throws SQLException {
+//                ps.setString(1,entities.get(i).getId());
+//                ps.setString(2,entities.get(i).getName());
+//                ps.setString(3,entities.get(i).getCompany().getId());
+//            }
+//
+//            @Override
+//            public int getBatchSize() {
+//                return entities.size();
+//            }
+//        });
 
-        DepartmentEntity saveEntity = departmentRepository.saveAndFlush(entity);
 
+        System.out.println(DateTimeUtil.toTimeStamp(LocalDateTime.now()));
         System.out.println("aaa");
-
-
     }
 
 }
