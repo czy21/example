44cb47efd8d1bff40e2d666a917de1c78f8ed958
Bruce CHEN <805899926@qq.com>
2020-04-22 18:23:08 +0800

    #Bruce user statemachine scope is request mode

--- code/api/application/src/test/java/com/team/application/service/impl/RinseServiceImpl.java
+++ code/api/application/src/test/java/com/team/application/service/impl/RinseServiceImpl.java
@@ -4,29 +4,28 @@ import com.team.application.kind.RinseEvent;
 import com.team.application.kind.RinseNode;
 import com.team.application.service.RinseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.persist.StateMachinePersister;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 //@Scope(value = "prototype",proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class RinseServiceImpl implements RinseService {
 
     @Autowired
-    @Qualifier("stateMachineTarget")
     private StateMachine<RinseNode, RinseEvent> stateMachine;
 
     @Autowired
     private StateMachinePersister<RinseNode, RinseEvent, String> stateMachinePersister;
 
     @Override
-    public void sendEvent(String processId) throws Exception {
-        resetStateMachineFromStore(processId);
+    public Map<String,Object> sendEvent(String processId) {
+//        resetStateMachineFromStore(processId);
         stateMachine.sendEvent(RinseEvent.NORMAL);
-        stateMachinePersister.persist(stateMachine, "testprefix:" + processId);
+//        stateMachinePersister.persist(stateMachine, "testprefix:" + processId);
+        return Map.of("uuid", stateMachine.getUuid().toString(), "state", stateMachine.getState().getId());
     }
 
     @Override
