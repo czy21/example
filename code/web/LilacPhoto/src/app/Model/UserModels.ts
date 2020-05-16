639b75e9e6854d974c30948910163d43f1197ea7
Bruce CHEN <805899926@qq.com>
2020-05-16 10:14:37 +0800

    #Bruce Improve

--- code/web/LilacPhoto/src/app/Model/UserModels.ts
+++
@@ -1,8 +0,0 @@
-export class UserModel { 
-    constructor(
-        public nickname?: string,
-        public headimgurl?: string,
-        public UserId?: number,
-        public refresh_token?:string
-    ) { }
-}
