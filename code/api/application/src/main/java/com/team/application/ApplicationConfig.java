25850e8a151499288cd81d8be296cb953b1dcd6d
Bruce CHEN <805899926@qq.com>
2020-07-09 23:48:34 +0800

    #Bruce Improve

--- code/api/application/src/main/java/com/team/application/ApplicationConfig.java
+++ code/api/application/src/main/java/com/team/application/ApplicationConfig.java
@@ -1,8 +1,12 @@
 package com.team.application;
 
+import com.team.external.ali.model.AliConfig;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
 
+    public ApplicationConfig(AliConfig aliConfig) {
+
+    }
 }
