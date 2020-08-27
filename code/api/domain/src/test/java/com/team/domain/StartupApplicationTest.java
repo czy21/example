85bd2dd4d5c029677bd57c4c2bc814ac3a097fa0
Bruce CHEN <805899926@qq.com>
2020-08-27 17:38:56 +0800

    #Bruce Improve

---
+++ code/api/domain/src/test/java/com/team/domain/StartupApplicationTest.java
@@ -0,0 +1,11 @@
+package com.team.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartupApplicationTest {
+    public static void main(String[] args) {
+        SpringApplication.run(StartupApplicationTest.class, args);
+    }
+}
