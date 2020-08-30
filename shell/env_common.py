f1c49cbf150e641387271dc8a65a299840f3adf6
Bruce CHEN <805899926@qq.com>
2020-08-30 23:04:41 +0800

    #Bruce Improve

--- shell/env_common.py
+++ shell/env_common.py
@@ -1,13 +1,13 @@
 # !/usr/bin/env python
-from pathlib import Path
-
 from script.domain import default_common
+from script.utility import path as path_util
 
 default_common.param_main_db_name = "erp_java"
 default_common.param_main_db_bak_name = default_common.param_main_db_name + "_bak"
 
 default_common.param_main_db_mysql_user = default_common.param_main_db_mongo_user = "admin"
 default_common.param_main_db_mysql_pass = default_common.param_main_db_mongo_pass = "***REMOVED***"
-default_common.param_api_extra_config_template_name = Path(__file__).parent.resolve().joinpath("template/build.extra.gradle").as_posix()
-default_common.param_api_yml_override_template_name = Path(__file__).parent.resolve().joinpath("template/application-override.yml").as_posix()
-default_common.param_api_root_project_path = Path(default_common.path_default.project_code_api).joinpath("portal").as_posix()
+default_common.param_api_extra_config_template_name = path_util.pure_path_join(__file__, "template/build.extra.gradle")
+default_common.param_api_yml_override_template_name = path_util.pure_path_join(__file__, "template/application-override.yml")
+default_common.param_api_root_project_path = path_util.pure_path_join(default_common.path_default.project_code_api, "portal")
+default_common.param_main_db_mysql_file_path = path_util.pure_path_join(default_common.param_main_db_mysql_file_path, "1_version")
--- shell/local/test.py
+++
@@ -1,9 +0,0 @@
-# !/usr/bin/env python
-
-import sys;
-
-sys.path.append("../../");
-from shell.local import _env;
-from script.domain import api_source;
-
-api_source.build_api()
