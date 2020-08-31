7c6612c24416575441f050c4deaccaec6a223844
Bruce CHEN <805899926@qq.com>
2020-08-31 14:52:57 +0800

    #Bruce Improve

---
+++ shell/test/_env.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python
+
+from shell import env_common
+
+env_common.default_common.param_main_db_host = "127.0.0.1"
+
+# mysql
+env_common.default_common.param_main_db_mysql_port = "3306"
+
+# mongo
+env_common.default_common.param_main_db_mongo_port = "27017"
