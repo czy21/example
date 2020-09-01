74696d1156e74f3fa644362818fca7f79bf86441
Bruce CHEN <805899926@qq.com>
2020-09-01 22:17:46 +0800

    #Bruce Improve

--- shell/local/rebuild_gdb.sh
+++ shell/local/rebuild_gdb.sh
@@ -3,5 +3,5 @@
 import="import sys;sys.path.append(\"../../\");from shell.local import _env;"
 rebuild_db="from script.domain.source import neo4j as neo4j_source;neo4j_source.rebuild_neo4j()"
 
-python3 -c "$import${rebuild_db}"
+py -c "$import${rebuild_db}"
 read -n 1
--- shell/local/rebuild_rdb.sh
+++ shell/local/rebuild_rdb.sh
@@ -3,5 +3,5 @@
 import="import sys;sys.path.append(\"../../\");from shell.local import _env;"
 rebuild_db="from script.domain.source import mysql as mysql_source;mysql_source.rebuild_mysql()"
 
-python3 -c "$import${rebuild_db}"
+py -c "$import${rebuild_db}"
 read -n 1
