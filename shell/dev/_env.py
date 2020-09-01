417b073653a23a2eee4567c35b9371d90a5d9ea3
Bruce CHEN <805899926@qq.com>
2020-09-02 01:41:41 +0800

    #Bruce Improve

--- shell/dev/_env.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-script.domain.default.default_common.param_main_db_host = "127.0.0.1"
-
-# mysql
-script.domain.default.default_common.param_main_db_mysql_port = "5306"
-
-# mongo
-script.domain.default.default_common.param_main_db_mongo_port = "5017"
