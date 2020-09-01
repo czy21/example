cb352e60c12d1a58b0fbede9c53de9174f26c6e3
Bruce CHEN <805899926@qq.com>
2020-09-01 22:17:46 +0800

    #Bruce Improve

--- shell/local/backup_rdb.sh
+++ shell/local/backup_rdb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 import="import sys;sys.path.append(\"../../\");from shell.local import _env;"
-backup_db="from script.domain import source;db_source.backup_mysql()"
+backup_db="from script.domain.source import mysql as mysql_source;mysql_source.backup_mysql()"
 
-sh python3 -c "$import${backup_db}"
+py -c "$import${backup_db}"
 read -n 1
--- shell/py/build_api.py
+++ shell/py/build_api.py
@@ -3,7 +3,7 @@
 import sys;
 
 sys.path.append("../../");
-from shell.local import _env;
-from script.domain.source import java as java_source;
+from shell.local import _env
+from script.domain.source import java as java_source
 
 java_source.build_api()
