bc24e562fafeb60aefeab252c21412603125fc9b
Bruce CHEN <805899926@qq.com>
2020-09-02 01:41:41 +0800

    #Bruce Improve

--- shell/dev/rebuild_db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-import="import sys;sys.path.append(\"../../\");from shell.dev import _env;"
-rebuild_db="from script.domain import db_source;db_source.rebuild_mysql()"
-
-python3 -c "$import${rebuild_db}"
-read -n 1
