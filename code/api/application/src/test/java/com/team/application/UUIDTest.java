338ac25685d825799a462410497b316d3a5c7ad0
Bruce CHEN <805899926@qq.com>
2020-09-17 12:56:43 +0800

    #Bruce Improve

--- code/api/application/src/test/java/com/team/application/UUIDTest.java
+++ code/api/application/src/test/java/com/team/application/UUIDTest.java
@@ -1,13 +1,22 @@
 package com.team.application;
 
+import org.junit.Test;
+
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public class UUIDTest {
 
     public static void main(String[] args) {
-        UUID uuid=UUID.randomUUID();
+        UUID uuid = UUID.randomUUID();
 
         System.out.print(uuid);
+    }
+
+    @Test
+    public void bigDecimalTest() {
+        BigDecimal ret1 = new BigDecimal("0.5");
+        System.out.println("s");
     }
 
 }
--- code/api/portal/src/main/java/com/team/portal/configure/PortalConfiguration.java
+++ code/api/portal/src/main/java/com/team/portal/configure/PortalConfiguration.java
@@ -1,9 +1,17 @@
 package com.team.portal.configure;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @ComponentScan(value = "com.team")
 @Configuration
 public class PortalConfiguration {
+
+    @Bean
+    public Queue hello() {
+        return new Queue("hello");
+    }
+
 }
