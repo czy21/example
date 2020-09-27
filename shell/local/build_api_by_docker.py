3ffe5a3f4628430edefefb0284a3fbe5fab6af7b
Bruce CHEN <805899926@qq.com>
2020-09-27 16:12:44 +0800

    #Bruce Improve

---
+++ shell/local/build_api_by_docker.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import sys
+
+sys.path.append("../../")
+from shell.local import _env
+from script.domain.source import java as java_source
+
+_env.env_common.default_common.param_api_docker_gradle_command = "sudo docker run --rm -u gradle -v \"$PWD\":/home/bruce/ -v /home/bruce/.gradle/init.gradle:/home/gradle/.gradle/init.gradle gradle:jdk11"
+java_source.build_api()
