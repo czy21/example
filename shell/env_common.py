109e70db9ef4560768bb3178e8622547d77f96cf
Bruce CHEN <805899926@qq.com>
2020-09-28 00:14:09 +0800

    #Bruce Improve

--- shell/env_common.py
+++ shell/env_common.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+from pathlib import Path
+
 from script.domain.default import common as default_common, path as default_path
 from script.utility import list as list_util
 from script.utility import path as path_util
 
+shell_path = Path.cwd().parent
 default_common.param_main_db_name = "erp_java"
 default_common.param_main_db_bak_name = default_common.param_main_db_name + "_bak"
 
@@ -13,9 +16,9 @@ default_common.param_main_db_mysql_pass = default_common.param_main_db_mongo_pas
 default_common.param_main_db_neo4j_user = "neo4j"
 
 # api
-default_common.param_api_gradle_init_script_file_path = path_util.pure_path_join(default_common.default_path.root_path, "shell/template/init.gradle")
-default_common.param_api_extra_config_template_name = path_util.pure_path_join(default_common.default_path.root_path, "shell/template/build.extra.gradle")
-default_common.param_api_yml_override_template_name = path_util.pure_path_join(default_common.default_path.root_path, "shell/template/application-override.yml")
+default_common.param_api_gradle_init_script_file_path = path_util.pure_path_join(shell_path, "template", "init.gradle")
+default_common.param_api_extra_config_template_name = path_util.pure_path_join(shell_path, "template", "build.extra.gradle")
+default_common.param_api_yml_override_template_name = path_util.pure_path_join(shell_path, "template", "application-override.yml")
 default_common.param_api_root_project_path = path_util.pure_path_join(default_path.project_code_api, "portal")
 default_common.param_api_docker_gradle_command = list_util.arr_param_to_str(["sudo docker run --rm -u gradle",
                                                                              "-v \"$HOME\":\"$HOME\"",
