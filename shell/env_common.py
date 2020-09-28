fc474ba99bb0363f4c41238e0604489ce774a34d
Bruce CHEN <805899926@qq.com>
2020-09-28 17:54:18 +0800

    #Bruce Improve

--- shell/env_common.py
+++ shell/env_common.py
@@ -19,6 +19,7 @@ default_common.param_main_db_neo4j_user = "neo4j"
 default_common.param_api_gradle_init_script_file_path = path_util.pure_path_join(shell_path, "template", "init.gradle")
 default_common.param_api_extra_config_template_name = path_util.pure_path_join(shell_path, "template", "build.extra.gradle")
 default_common.param_api_yml_override_template_name = path_util.pure_path_join(shell_path, "template", "application-override.yml")
+default_common.param_api_dockerfile_template_name = path_util.pure_path_join(shell_path, "template", "Dockerfile")
 default_common.param_api_root_project_path = path_util.pure_path_join(default_path.project_code_api, "portal")
 default_common.param_api_docker_gradle_command = list_util.arr_param_to_str(
     [
