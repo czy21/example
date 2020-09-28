48be2b00cf223124e1fecb011ff95c4e4f5515b7
Bruce CHEN <805899926@qq.com>
2020-09-28 21:54:56 +0800

    #Bruce Improve

--- shell/env_common.py
+++ shell/env_common.py
@@ -9,16 +9,8 @@ shell_path = Path.cwd().parent
 default_common.param_main_db_name = "erp_java"
 default_common.param_main_db_bak_name = default_common.param_main_db_name + "_bak"
 
-# db user password
-default_common.param_main_db_mysql_user = default_common.param_main_db_mongo_user = "admin"
-default_common.param_main_db_mysql_pass = default_common.param_main_db_mongo_pass = default_common.param_main_db_neo4j_pass = "***REMOVED***"
-
-default_common.param_main_db_neo4j_user = "neo4j"
-
 # api
 default_common.param_api_archive_file_name = "api.jar"
-default_common.param_api_output_resource_path = default_path.output_api_resource
-default_common.param_api_output_path = default_path.output_api
 default_common.param_api_plugin_file_path = path_util.pure_path_join(default_path.project_plugin, "build.gradle")
 default_common.param_api_gradle_init_script_file_path = path_util.pure_path_join(shell_path, "template", "init.gradle")
 default_common.param_api_extra_config_template_name = path_util.pure_path_join(shell_path, "template", "build.extra.gradle")
@@ -35,5 +27,8 @@ default_common.param_api_docker_gradle_command = list_util.arr_param_to_str(
     ])
 
 # db
-default_common.param_main_db_mysql_file_path = path_util.pure_path_join(default_common.param_main_db_mysql_file_path, "1_version")
-default_common.param_main_db_neo4j_db_name = "neo4j"
+# db user password
+default_common.param_main_db_mysql_user = default_common.param_main_db_mongo_user = default_common.param_main_db_neo4j_user = "admin"
+default_common.param_main_db_mysql_pass = default_common.param_main_db_mongo_pass = default_common.param_main_db_neo4j_pass = "***REMOVED***"
+
+default_common.param_main_db_mysql_file_path = path_util.pure_path_join(default_path.project_db, "mysql", "1_version")
