6ab7f4d5fcc6b925e17c85af54f4e4e057027909
Bruce CHEN <805899926@qq.com>
2020-10-01 19:31:09 +0800

    #Bruce Improve

--- shell/run.py
+++ shell/run.py
@@ -4,7 +4,7 @@ import json
 import sys
 from pathlib import Path
 
-from script.utility import log as log_util
+from script.utility import log as log_util, basic as basic_util
 
 logger = log_util.Logger(__name__)
 
@@ -16,7 +16,7 @@ def exec_file(source_dict: {}):
     source_mod_files = [{"module": importlib.import_module(m), "func": f} for m, f in source_dict.items()]
     default_common_mod = importlib.import_module("script.domain.default.common")
     common_param = getattr(default_common_mod, "get_params")()
-    logger.info(json.dumps(common_param))
+    logger.info(basic_util.action_formatter("params", json.dumps(common_param)))
     default_path_module = importlib.import_module("script.domain.default.path")
     getattr(default_path_module, "re_mkdir")()
     [getattr(i["module"], j)() for i in source_mod_files for j in i["func"]]
