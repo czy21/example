c64a6b73a7f55744adea4dd0b41125bc0988435a
Bruce CHEN <805899926@qq.com>
2020-10-01 15:05:34 +0800

    #Bruce IMprove

--- shell/run.py
+++ shell/run.py
@@ -15,4 +15,6 @@ def exec_file(source_dict: {}):
     default_common_mod = importlib.import_module("script.domain.default.common")
     common_param = getattr(default_common_mod, "get_params")()
     logger.info(common_param)
+    default_path_module = importlib.import_module("script.domain.default.path")
+    getattr(default_path_module, "mkdir")()
     [getattr(i["module"], j)() for i in source_mod_files for j in i["func"]]
