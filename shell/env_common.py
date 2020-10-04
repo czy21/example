c931f6120ee950f5e0139e4b62977218cfcae813
Bruce CHEN <805899926@qq.com>
2020-10-04 14:28:05 +0800

    #Bruce use argparse

--- shell/env_common.py
+++ shell/env_common.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import argparse
 from pathlib import Path
 
 from script.domain.default import common as default_common, path as default_path
@@ -41,6 +42,10 @@ default_common.param_main_db_neo4j_file_path = path_util.pure_path_join(default_
 
 
 def inject():
+    parser = argparse.ArgumentParser(description="Demo of argparse")
+    parser.add_argument('--param', nargs="+", default=[])
+    args = parser.parse_args()
+    args_param_dict = dict({t.split("=")[0]: t.split("=")[1] for t in args.param})
     default_common.param_main_db_name = "_".join([param_project_name, "java", param_env_suffix])
     default_common.param_main_db_bak_name = "_".join([default_common.param_main_db_name, "bak"])
-    default_common.param_injected = dict({k: v for k, v in globals().items() if k.startswith("param")})
+    default_common.param_injected = dict(dict({k: v for k, v in globals().items() if k.startswith("param")}).items(), **args_param_dict)
