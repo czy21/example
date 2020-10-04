a04371ad11c1380fa4c32f116aec8568c96ab953
Bruce CHEN <805899926@qq.com>
2020-10-05 00:49:23 +0800

    #Bruce IMprove

--- shell/play/start_api.py
+++ shell/play/start_api.py
@@ -4,4 +4,4 @@ import sys
 sys.path.append("../../")
 from shell import run
 
-run.exec_file({"script.domain.source.java": ["build_plugin", "build_api", "build_api_image"]})
+run.exec_file({"script.domain.source.java": ["start_api_compose"]})
