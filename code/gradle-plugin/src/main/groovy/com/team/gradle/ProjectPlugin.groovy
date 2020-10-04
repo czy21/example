80e9f38df6827f1ace200bd550515ecba32b102e
Bruce CHEN <805899926@qq.com>
2020-10-04 16:26:12 +0800

    #Bruce Improve

--- code/gradle-plugin/src/main/groovy/com/team/gradle/ProjectPlugin.groovy
+++ code/gradle-plugin/src/main/groovy/com/team/gradle/ProjectPlugin.groovy
@@ -8,22 +8,35 @@ import org.gradle.testfixtures.ProjectBuilder
 class ProjectPlugin implements Plugin<Project> {
 
     void apply(Project p) {
-        p.apply plugin: 'java-library'
-        p.apply plugin: 'idea'
-        p.targetCompatibility = "11"
-        p.sourceCompatibility = "11"
-        p.tasks.withType(JavaCompile) {
-            options.encoding = 'UTF-8'
+        p.subprojects {
+            group = 'com.team'
+            apply plugin: 'idea'
+            apply plugin: 'java'
+            apply plugin: 'java-library'
+            apply plugin: 'org.springframework.boot'
+            apply plugin: 'io.spring.dependency-management'
+            bootJar {
+                mainClassName = "com.team.portal.StartupApplication"
+            }
+            compileJava {
+                sourceCompatibility = 11
+                targetCompatibility = 11
+            }
+            tasks.withType(JavaCompile) {
+                options.encoding = "UTF-8"
+            }
+            dependencies {
+                "compileOnly" "org.mapstruct:mapstruct:1.3.1.Final"
+                "annotationProcessor" "org.mapstruct:mapstruct-processor:1.3.1.Final"
+                "compileOnly" "org.projectlombok:lombok:1.18.10"
+                "annotationProcessor" "org.projectlombok:lombok:1.18.10"
+                "testCompileOnly" "org.mapstruct:mapstruct:1.3.1.Final"
+                "testAnnotationProcessor" "org.mapstruct:mapstruct-processor:1.3.1.Final"
+                "testCompileOnly" "org.projectlombok:lombok:1.18.10"
+                "testAnnotationProcessor" "org.projectlombok:lombok:1.18.10"
+                "testImplementation" "org.springframework.boot:spring-boot-starter-test:2.3.3.RELEASE"
+            }
         }
-        p.dependencies.add("compileOnly", "org.mapstruct:mapstruct:1.3.1.Final")
-        p.dependencies.add("annotationProcessor", "org.mapstruct:mapstruct-processor:1.3.1.Final")
-        p.dependencies.add("compileOnly", "org.projectlombok:lombok:1.18.10")
-        p.dependencies.add("annotationProcessor", "org.projectlombok:lombok:1.18.10")
-        p.dependencies.add("testCompileOnly", "org.mapstruct:mapstruct:1.3.1.Final")
-        p.dependencies.add("testAnnotationProcessor", "org.mapstruct:mapstruct-processor:1.3.1.Final")
-        p.dependencies.add("testCompileOnly", "org.projectlombok:lombok:1.18.10")
-        p.dependencies.add("testAnnotationProcessor", "org.projectlombok:lombok:1.18.10")
-        p.dependencies.add("testImplementation", "org.springframework.boot:spring-boot-starter-test:2.3.3.RELEASE")
     }
 
     static void main(String[] args) {
