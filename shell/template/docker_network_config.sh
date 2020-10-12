487e146568f32aa61320e60be4cc2484c7b607ae
Bruce CHEN <805899926@qq.com>
2020-10-12 14:08:54 +0800

    #Bruce Improve

--- shell/template/docker_network_config.sh
+++
@@ -1,5 +0,0 @@
-
-sudo docker network create --driver bridge erp_play_default
-sudo docker network connect erp_play_default mysql
-sudo docker network connect erp_play_default mongo
-sudo docker network connect erp_play_default neo4j
