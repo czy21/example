0476242a4c71d975d5ea967d4a9367721fd25a15
Bruce CHEN <805899926@qq.com>
2020-10-12 13:15:17 +0800

    #Bruce IMprove

--- shell/local/_env.py
+++ shell/local/_env.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 from shell import env_common
 
-env_common.param_env_suffix = "local"
-env_common.default_common.param_main_db_host = "127.0.0.1"
-
 # mysql
+main_db_host = "127.0.0.1"
+
+env_common.default_common.param_main_db_mysql_host = main_db_host
 env_common.default_common.param_main_db_mysql_port = "3306"
 
 # mongo
+env_common.default_common.param_main_db_mongo_host = main_db_host
 env_common.default_common.param_main_db_mongo_port = "27017"
 
 # neo4j
+env_common.default_common.param_main_db_neo4j_host = main_db_host
 env_common.default_common.param_main_db_neo4j_port = "7687"
-
 env_common.default_common.param_api_docker_gradle_command = ""
--- shell/play/_env.py
+++ shell/play/_env.py
@@ -3,8 +3,6 @@ from shell import env_common
 
 env_common.param_env_suffix = "play"
 
-env_common.default_common.param_main_db_host = "127.0.0.1"
-
 # mysql
 env_common.default_common.param_main_db_mysql_port = "3306"
 
