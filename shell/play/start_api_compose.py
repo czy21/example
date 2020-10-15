6d52921c2387e8d885ea14b619913d0d8cf91898
Bruce CHEN <805899926@qq.com>
2020-10-15 20:16:55 +0800

    #Bruce Improve

--- shell/play/start_api_compose.py
+++ shell/play/start_api_compose.py
@@ -4,4 +4,4 @@ import sys
 sys.path.append("../../")
 from shell import run
 
-run.exec_file({"script.domain.source.java": ["start_api_compose"]})
+run.exec_file({"script.domain.source.java": ["ensure_network", "start_api_compose"]})
