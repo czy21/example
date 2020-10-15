cdc7f8d1cf24c48b8752c016738861fd273782a2
Bruce CHEN <805899926@qq.com>
2020-10-16 00:09:29 +0800

    #Bruce Init vue

---
+++ code/web/vue/old/operation/src/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+<style lang="scss">
+  @import "../static/css/app";
+  @import "../static/css/custom";
+</style>
