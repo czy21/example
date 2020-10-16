74af33217e5f03f95355a2cfc5fa9e88ad1cb649
Bruce CHEN <805899926@qq.com>
2020-10-16 17:44:59 +0800

    #ss

--- shell/local/build_api.sh
+++ shell/local/build_api.sh
@@ -2,11 +2,8 @@
 
 env="local"
 
-
-python3 ../../shell/run.py \
+cmd="'sss'"
+echo 'python3 ../../shell/run.py \
 --env ../../shell/${env}/_env.py \
 --log-file build_plugin.log \
---cmd '
-from script.domain.source import java as java_source
-java_source.build_api()
-'
+--cmd '${cmd}''
