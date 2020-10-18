f84a1512bdf16d5159b721f700c274a6e9246b80
Bruce CHEN <805899926@qq.com>
2020-10-18 12:31:40 +0800

    #

--- shell/local/rebuild_rdb.sh
+++ shell/local/rebuild_rdb.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+cd $(cd "$(dirname "$0")"; pwd)
+
 env="local"
 
 python3 ../../shell/run.py \
 --env ../../shell/${env}/_env.py \
---log-file build_plugin.log \
+--log-file rebuild_rdb.log \
 --cmd '
 from script.domain.source import mysql as mysql_source
 mysql_source.assemble()
