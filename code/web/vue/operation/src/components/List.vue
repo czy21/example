789d87c6fbc5e66a08f674275b5a207d5905dbf9
Bruce CHEN <805899926@qq.com>
2020-10-19 15:42:48 +0800

    #

---
+++ code/web/vue/operation/src/components/List.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <el-table :data="data">
+      <template v-for="t in columns">
+        <el-table-column :label="t.label" :width="t.width">
+          <template slot-scope="scope">
+            {{ scope.row[t.prop] }}
+          </template>
+        </el-table-column>
+      </template>
+    </el-table>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Prop, Vue} from 'vue-property-decorator';
+
+export interface Column {
+  prop: string
+  label: string
+  width?: string
+}
+
+@Component
+export default class List extends Vue {
+  @Prop({default: () => []}) private data?: Array<any>;
+  @Prop({default: () => []}) private columns!: Array<Column>
+}
+
+
+</script>
