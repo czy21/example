287e5d07b196628bf22617ec7f25f81cdc14100c
Bruce CHEN <805899926@qq.com>
2020-10-19 22:03:32 +0800

    #

--- code/web/vue/operation/vue.config.js
+++ code/web/vue/operation/vue.config.js
@@ -1,9 +1,22 @@
 let webpackConfig = require('./webpack.config')
 
-let _ = require('lodash')
-
 module.exports = {
     configureWebpack: config => {
         Object.assign(config.resolve.alias, webpackConfig.resolve.alias)
-    }
+    },
+    devServer: {
+        host: "localhost",
+        port: 3000,
+        proxy: {
+            "/api": {
+                target: 'http://localhost:8075',
+                changeOrigin: true,
+                ws: true,
+                pathRewrite: {
+                    '^/api': '',
+                }
+            }
+        },
+    },
+
 };
