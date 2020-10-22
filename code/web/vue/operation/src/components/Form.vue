0dc690d19da447029ed3b96b84eebc6c78f51dfb
Bruce CHEN <805899926@qq.com>
2020-10-22 14:55:22 +0800

    #

--- code/web/vue/operation/src/components/Form.vue
+++ code/web/vue/operation/src/components/Form.vue
@@ -1,30 +1,38 @@
-<template>
-  <el-form v-bind="getForm">
-    <template v-for="t in getFormItems">
-      <el-form-item v-bind="t">
-        <template v-if="!t.type">
-          <el-input v-model="getForm.model[t]" v-on="t.attrs"/>
-        </template>
-      </el-form-item>
-    </template>
-    <slot/>
-  </el-form>
-</template>
-
 <script lang="tsx">
-import {Component, Prop, Vue} from 'vue-property-decorator';
+import {Component, Prop, Provide, Vue} from 'vue-property-decorator';
 
 @Component
 export default class Form extends Vue {
   @Prop({default: {}}) private form!: Object
   @Prop({default: []}) private formItems!: Object[];
 
-  get getForm() {
-    return Object.assign({"label-width": "50px"}, this.form)
+  @Provide() formDefault: Object = {
+    "label-width": "50px"
+  }
+  @Provide() formItemDefault: Object = {
+    style: "width:360px",
+    component: (form: any, item: any) => this.input(form, item)
   }
 
-  get getFormItems() {
-    return this.formItems.map(s => Object.assign({style: "width:360px"}, s))
+  input(form: any, item: any) {
+    return (<el-input {...{attrs: {model: form.model[item.prop]}}}/>)
+  }
+
+  render(h: any) {
+    return (
+        <el-form {...{attrs: Object.assign(this.formDefault, this.form)}}>
+          {
+            this.formItems.map(s => Object.assign(this.formItemDefault, s))
+                ?.map((s: any) => {
+                  return (
+                      <el-form-item {...{attrs: s}}>
+                        {s.component(this.form, s)}
+                      </el-form-item>
+                  )
+                })
+          }
+        </el-form>
+    )
   }
 }
 
