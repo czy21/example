418261bf7ea95692957ad0e71edd70ab16a40a26
Bruce CHEN <805899926@qq.com>
2020-10-27 14:57:59 +0800

    #Bruce Improve Web React

--- code/web/react/operation/src/App.tsx
+++ code/web/react/operation/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter} from "react-router-dom";
 import {createStore} from "redux";
 import {Provider} from "react-redux";
-import rootReducer from "@/redux/reducer";
+import rootReducer from "@/redux";
 import Home from "@/layout/Home";
 
 const store = createStore(rootReducer);
--- code/web/react/operation/src/redux/reducer/Home.ts
+++ code/web/react/operation/src/redux/reducer/Home.ts
@@ -1,11 +1,17 @@
+const Action = {
+    Collapse: () => {
+        return {type: "Collapse"}
+    }
+}
+
 const Home = (state: any = {collapsed: false}, action: any) => {
     switch (action.type) {
         case 'Collapse':
-            return Object.assign({}, state, {
-                collapsed: !state.collapsed
-            });
+            return Object.assign({}, state, {collapsed: !state.collapsed});
         default:
             return state
     }
 };
-export default Home
+
+
+export {Home, Action}
