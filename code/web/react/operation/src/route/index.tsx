8b9eb365a27d4374cafbfbd25263a6c40bc5ca06
Bruce CHEN <805899926@qq.com>
2020-10-28 11:02:02 +0800

    #

--- code/web/react/operation/src/route/index.tsx
+++ code/web/react/operation/src/route/index.tsx
@@ -1,17 +1,7 @@
-import A from "@v/A";
-import B from "@v/B";
 import User from "@v/user"
 import {RouteConfig} from "react-router-config";
 
 const routes: RouteConfig[] = [
-    {
-        path: "/system",
-        component: A,
-    },
-    {
-        path: "/b",
-        component: B,
-    },
     {
         path: "/user",
         component: User,
--- code/web/react/operation/src/view/B.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default class B extends React.Component<any, any> {
-
-    render() {
-        return <div>B页面</div>;
-    }
-}
