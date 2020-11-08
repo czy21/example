3f8cc2516c9cfe9411f26bfc6d5d04632b549afe
Bruce CHEN <805899926@qq.com>
2020-11-08 23:33:29 +0800

    #

--- code/api/application/src/test/java/com/team/infrastructure/IncludeParserTest.java
+++ code/api/application/src/test/java/com/team/infrastructure/IncludeParserTest.java
@@ -1,11 +1,17 @@
 package com.team.infrastructure;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class IncludeParserTest {
 
@@ -28,6 +34,29 @@ public class IncludeParserTest {
     public Object getMock() throws IOException {
         InputStream stream = this.getClass().getClassLoader().getResourceAsStream("includeTest.json");
         return mapper.readValue(stream, Map.class);
+    }
+
+    @Test
+    public void test(List<String> seq) {
+//        List<String> seq = List.of("a","b");
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < seq.size(); i++) {
+            if (seq.size() == 1) {
+                result.append(seq.get(0));
+                break;
+            }
+            if (i + 1 < seq.size()) {
+                result.append(StringUtils.join("ifnull(", seq.get(i), ","));
+            }
+            if (i + 1 == seq.size()) {
+                result.append(StringUtils.join(seq.get(i), String.join("", Collections.nCopies(i, ")"))));
+            }
+
+        }
+
+        System.out.println(result);
+
     }
 
 }
