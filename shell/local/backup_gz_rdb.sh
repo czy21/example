89eade5f1dd30168da0e0f9f356aaaaeb3833c10
Bruce CHEN <805899926@qq.com>
2020-11-13 20:51:07 +0800

    #

---
+++ shell/local/backup_gz_rdb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd $(cd "$(dirname "$0")"; pwd)
+
+env="local"
+
+python3 ../../shell/run.py \
+--env ../../shell/${env}/_env.py \
+--log-file build_api.log \
+--cmd '
+from script.domain.source import mysql as mysql_source
+mysql_source.backup_mysql_gz()
+'
