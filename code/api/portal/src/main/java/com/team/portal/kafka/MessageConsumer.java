a52b3b5396ddfa49366d6871f4ea7ca1ef8784d8
Bruce CHEN <805899926@qq.com>
2021-01-09 00:00:02 +0800

    #Bruce Improve

--- code/api/portal/src/main/java/com/team/portal/kafka/MessageConsumer.java
+++ code/api/portal/src/main/java/com/team/portal/kafka/MessageConsumer.java
@@ -1,15 +1,22 @@
 package com.team.portal.kafka;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageConsumer {
 
-    @KafkaListener(topics = {"my-topic"}) //1
-//    @SendTo("confirm-topic") //2
+    @KafkaListener(topics = {"my-topic"})
+    @SendTo("confirm-topic")
     public String consume(MessageEvent event) {
         System.out.println("在consume方法中处理事件" + event);
         return "接收到了:" + event.toString();
+    }
+
+
+    @KafkaListener(topics = {"another-topic"})
+    public void consumeAnother(MessageEvent event) {
+        System.out.println("在consumeAnother方法中处理another-topic中的事件" + event);
     }
 }
