23c89e15bef3fbffbb6ea42d6b5511f2703abb62
Bruce CHEN <805899926@qq.com>
2021-01-09 22:42:37 +0800

    #

--- code/gradle-plugin/src/main/groovy/com/team/gradle/DeployPlugin.groovy
+++ code/gradle-plugin/src/main/groovy/com/team/gradle/DeployPlugin.groovy
@@ -6,6 +6,9 @@ import org.gradle.api.Project
 class DeployPlugin implements Plugin<Project> {
 
     void apply(Project p) {
+        p.bootJar {
+            enabled = true
+        }
         if (p.rootProject.hasProperty("extraConfig")) {
             print("> Loading extra config from \"${p.rootProject.extraConfig}\"")
             p.apply from: p.rootProject.extraConfig
