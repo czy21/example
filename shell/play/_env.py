b50738f9f1f2ef67f3f8ba639fb6d2e3cbb13333
Bruce CHEN <805899926@qq.com>
2021-01-11 22:44:38 +0800

    #

--- shell/play/_env.py
+++ shell/play/_env.py
@@ -4,10 +4,10 @@ from shell import env_common
 env_common.param_env_suffix = "play"
 
 # mysql
-env_common.default_common.param_main_db_mysql_port = "3306"
+env_common.param_main_db_mysql_port = "3306"
 
 # mongo
-env_common.default_common.param_main_db_mongo_port = "27017"
+env_common.param_main_db_mongo_port = "27017"
 
 # neo4j
-env_common.default_common.param_main_db_neo4j_port = "7687"
+env_common.param_main_db_neo4j_port = "7687"
