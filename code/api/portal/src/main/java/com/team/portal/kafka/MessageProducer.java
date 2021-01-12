26c729753e4a8e19317e23a51f21a715f690cb9a
Bruce CHEN <805899926@qq.com>
2021-01-13 00:00:30 +0800

    #

--- code/api/portal/src/main/java/com/team/portal/kafka/MessageProducer.java
+++ code/api/portal/src/main/java/com/team/portal/kafka/MessageProducer.java
@@ -1,22 +1,19 @@
 package com.team.portal.kafka;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
-
 @Component
 public class MessageProducer {
 
-    KafkaTemplate<Object, Object> kafkaTemplate;
-
-    public MessageProducer(KafkaTemplate<Object, Object> kafkaTemplate) { //1
-        this.kafkaTemplate = kafkaTemplate;
-    }
+    @Autowired
+    KafkaTemplate<String, String> kafkaTemplate;
 
     @Scheduled(fixedRate = 2000)
     public void send() {
-        kafkaTemplate.send("my-topic", new MessageEvent(UUID.randomUUID().toString(), "wyf"));
+        kafkaTemplate.send("my-topic", "czy");
+//        kafkaTemplate.send("my-topic", "zjy", (long) 6);
     }
 }
