d21d3ddde7ecba4b2027e76f73fedb4a1fe270f6
Bruce CHEN <805899926@qq.com>
2021-01-15 12:56:41 +0800

    #

--- shell/local/_env.py
+++ shell/local/_env.py
@@ -4,15 +4,15 @@ from shell import env_common
 # mysql
 main_db_host = "192.168.2.4"
 
-env_common.param_env_suffix = "local"
-env_common.param_main_db_mysql_host = main_db_host
-env_common.param_main_db_mysql_port = "3306"
+param_env_suffix = "local"
+param_main_db_mysql_host = main_db_host
+param_main_db_mysql_port = "3306"
 
 # mongo
-env_common.param_main_db_mongo_host = main_db_host
-env_common.param_main_db_mongo_port = "27017"
+param_main_db_mongo_host = main_db_host
+param_main_db_mongo_port = "27017"
 
 # neo4j
-env_common.param_main_db_neo4j_host = main_db_host
-env_common.param_main_db_neo4j_port = "7687"
-env_common.param_api_docker_gradle_command = ""
+param_main_db_neo4j_host = main_db_host
+param_main_db_neo4j_port = "7687"
+param_api_docker_gradle_command = ""
--- shell/run.py
+++ shell/run.py
@@ -21,6 +21,7 @@ def exec_file():
     parser.add_argument('--log-file')
     parser.add_argument('--cmd')
     args = parser.parse_args()
+    args_param_dict = dict({t.split("=")[0]: t.split("=")[1] for t in args.param})
 
     env_path = Path(args.env).resolve()
     env_stem = env_path.parent.stem
@@ -34,9 +35,14 @@ def exec_file():
     getattr(default_path_module, "re_mkdir")(rm_output=args.skip_rm_output)
 
     # injected param to global
-    env_module = importlib.import_module("".join(["shell.", env_stem, "._env"]))
-    getattr(getattr(env_module, "env_common"), "inject")(args)
+    env_pwd_mod = importlib.import_module("".join(["shell.", env_stem, "._env"]))
+
+    env_common_mod = env_pwd_mod.env_common
+    env_common_mod.__dict__.update(dict({k: v for k, v in env_pwd_mod.__dict__.items() if k.startswith("param")}).items(), **args_param_dict)
+    getattr(env_common_mod, "inject")(args)
+
     default_common_mod = importlib.import_module("script.domain.default.common")
+    default_common_mod.__dict__.update(dict({k: v for k, v in env_common_mod.__dict__.items() if k.startswith("param")}))
     common_param = getattr(default_common_mod, "get_params")()
     logger.info(basic_util.action_formatter("params", json.dumps(common_param, sort_keys=True)), default_common_mod.__name__)
     if args.cmd:
