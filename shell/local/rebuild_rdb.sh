ab609835ab0bf590ba9df6894b60e55e6b6b113b
Bruce CHEN <805899926@qq.com>
2021-02-01 10:09:43 +0800

    #

--- shell/local/rebuild_rdb.sh
+++ shell/local/rebuild_rdb.sh
@@ -9,7 +9,7 @@ python3 ../../shell/run.py \
 --log-file rebuild_rdb.log \
 --cmd '
 from script.domain.source import mysql as mysql_source
-#mysql_source.assemble()
-#mysql_source.recreate()
-#mysql_source.exec()
+mysql_source.assemble()
+mysql_source.recreate()
+mysql_source.exec()
 '
