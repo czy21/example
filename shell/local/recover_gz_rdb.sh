841cb2297f8b1a55e7d791de3a2ec49df5d5cdf3
Bruce CHEN <805899926@qq.com>
2021-02-26 19:25:10 +0800

    #Bruce

--- shell/local/recover_gz_rdb.sh
+++ shell/local/recover_gz_rdb.sh
@@ -6,7 +6,7 @@ env="local"
 
 python3 ../../shell/run.py \
 --env ../../shell/${env}/_env.py \
---log-file build_api.log --skip-rm-output \
+--log-file recover_gz_rdb.log --skip-rm-output \
 --cmd '
 from script.domain.source import mysql as mysql_source
 mysql_source.recover_mysql_gz()
