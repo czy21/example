cacfaa04debb6c937de5dd06f88fb7a4cd1c050e
Bruce CHEN <805899926@qq.com>
2021-03-17 22:05:01 +0800

    #

---
+++ shell/test/build_api.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd $(cd "$(dirname "$0")"; pwd)
+
+env="test"
+
+python3 ../../shell/run.py \
+--env ../../shell/${env}/_env.py \
+--log-file build_api.log \
+--param param_api_image=erp:test \
+--cmd '
+from script.domain.source import java as java_source
+java_source.ensure_network()
+java_source.build_api()
+java_source.build_api_compose()
+'
