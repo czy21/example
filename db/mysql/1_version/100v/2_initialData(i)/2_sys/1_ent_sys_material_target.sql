9e40d8b80dd4627a2df7e41ba1f49fa0eff6bcb2
Bruce CHEN <805899926@qq.com>
2021-03-29 01:37:51 +0800

    #

--- db/mysql/1_version/100v/2_initialData(i)/2_sys/1_ent_sys_material_target.sql
+++ db/mysql/1_version/100v/2_initialData(i)/2_sys/1_ent_sys_material_target.sql
@@ -1,2 +1,3 @@
 INSERT INTO `ent_sys_material_target`(`key`, `root_url`, `root_path`, `target_kind`)
-VALUES ('DEFAULT', '/', 'upload', 'LOCAL');
+VALUES ('DEFAULT', '/', 'upload', 'LOCAL'),
+       ('ERROR', '/', 'upload/error', 'LOCAL');
