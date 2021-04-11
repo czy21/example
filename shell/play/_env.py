2118737c638995ab1ffb372aad73200e657bb105
Bruce CHEN <805899926@qq.com>
2021-04-11 22:48:45 +0800

    #

--- shell/play/_env.py
+++ shell/play/_env.py
@@ -5,14 +5,6 @@ from script.utility import path as path_util
 
 param_env_suffix = "play"
 
-qualify_project_env = "_".join([env_common.param_project_name, param_env_suffix])
-param_main_db_name = qualify_project_env
-param_main_db_bak_name = "_".join([param_main_db_name, "bak"])
-param_web_nginx_env_file_name = qualify_project_env + ".conf"
-param_web_nginx_output_file_path = path_util.pure_path_join(default_path.output_tmp, param_web_nginx_env_file_name)
-
-param_api_network_name = "_".join([env_common.param_project_name, param_env_suffix, "default"])
-
 db_host = "192.168.2.3"
 
 # redis
