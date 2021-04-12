a44175debf133958cab4c10abcefdfe31d146412
Bruce CHEN <805899926@qq.com>
2021-04-12 18:36:14 +0800

    #

--- code/api/file_resolve/src/main/java/com/team/fileresolve/receiver/FileResolveReceiver.java
+++ code/api/file_resolve/src/main/java/com/team/fileresolve/receiver/FileResolveReceiver.java
@@ -1,8 +1,15 @@
 package com.team.fileresolve.receiver;
 
+import com.alibaba.excel.EasyExcel;
 import com.alibaba.fastjson.JSONObject;
 import com.team.application.ApplicationConfig;
 import com.team.application.model.vo.MaterialVO;
+import com.team.application.util.MaterialUtil;
+import com.team.domain.entity.MaterialEntity;
+import com.team.domain.mapper.MaterialMapper;
+import com.team.domain.mongo.repository.FileColumnMappingRepository;
+import com.team.fileresolve.listener.FileListener;
+import org.apache.catalina.mapper.MapperListener;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
@@ -10,13 +17,24 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Map;
 
 @Component
 public class FileResolveReceiver {
 
     @Autowired
     RabbitTemplate rabbitTemplate;
+    @Autowired
+    MaterialMapper materialMapper;
+    @Autowired
+    private RedisTemplate<String, Map<String, Object>> stringListRedisTemplate;
+    @Autowired
+    FileColumnMappingRepository fileColumnMappingRepository;
 
     @Bean
     public SimpleRabbitListenerContainerFactory fileContainerFactory(
@@ -31,7 +49,10 @@ public class FileResolveReceiver {
     @RabbitListener(queues = ApplicationConfig.FILE_RESOLVE_QUEUE, containerFactory = "fileContainerFactory")
     public void receive(String message) {
         MaterialVO materialVO = JSONObject.parseObject(message, MaterialVO.class);
-        System.out.println(message);
+        MaterialEntity materialEntity = materialMapper.selectById(materialVO.getUid());
+        File f = Path.of(materialEntity.getMaterialTarget().getRootUrl(), materialEntity.getMaterialTarget().getRootPath(), materialEntity.getPath()).toFile();
+        EasyExcel.read(f, new FileListener(stringListRedisTemplate, fileColumnMappingRepository)).sheet().doRead();
+
     }
 
 }
