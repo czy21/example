2e1ca51d6ed643d692dd5f4f3da7915750e4b919
Bruce CHEN <805899926@qq.com>
2021-04-12 18:36:14 +0800

    #

--- code/api/portal/src/main/java/com/team/portal/controller/SaleController.java
+++ code/api/portal/src/main/java/com/team/portal/controller/SaleController.java
@@ -11,11 +11,13 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.Map;
 
 @RequestMapping(path = "sale")
 @RestController
@@ -33,10 +35,20 @@ public class SaleController extends BaseController {
     @Autowired
     ObjectMapper objectMapper;
 
+
+    @Autowired
+    RedisTemplate<String, Map<String, Object>> stringMapRedisTemplate;
+
     @PostMapping(path = "upload")
     public MaterialVO upload(FileVO fileVO) throws IOException {
         MaterialVO materialVO = materialService.upload(fileVO, "DEFAULT");
         rabbitTemplate.convertAndSend(queue.getName(), JSONObject.toJSONString(materialVO));
         return materialVO;
     }
+
+    @PostMapping(path = "redisTest")
+    public Map<String, Object> redisTest() {
+        return Map.of();
+    }
+
 }
