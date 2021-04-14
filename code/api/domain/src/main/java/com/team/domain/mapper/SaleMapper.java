9fd7a65e0bd59fe57970bc968c0bce4023fb9b2a
Bruce CHEN <805899926@qq.com>
2021-04-14 20:17:21 +0800

    #

--- code/api/domain/src/main/java/com/team/domain/mapper/SaleMapper.java
+++ code/api/domain/src/main/java/com/team/domain/mapper/SaleMapper.java
@@ -1,6 +1,18 @@
 package com.team.domain.mapper;
 
+import com.team.domain.entity.SaleEntity;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
 public interface SaleMapper {
 
-//    List<SaleEntity> selectByPage(@Param("_skiprows") int skipRows, @Param("_pagesize") int pageSize);
+    @Select("select bf.* from ent_sfl_inspect_sale bf limit #{_skiprows},#{_pagesize}")
+    SaleEntity selectAllByPage(@Param("_skiprows") int skipRow, @Param("_pagesize") int pageSize);
+
+    @Insert("insert into ent_sfl_inspect_sale_1 (id,from_institution_code,from_institution_name,to_institution_code,to_institution_name,product_code,product_name,product_spec,product_unit) " +
+            " values " +
+            "(#{id},#{fromInstitutionCode},#{fromInstitutionName},#{toInstitutionCode},#{toInstitutionName},#{productCode},#{productName},#{productSpec},#{productUnit})")
+    void insert(SaleEntity entity);
+
 }
