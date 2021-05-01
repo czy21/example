bf8981dba0f0767215b6ff362023646a042da7ee
Bruce CHEN <805899926@qq.com>
2021-05-01 15:05:49 +0800

    #

--- shell/local/build_api.sh
+++ shell/local/build_api.sh
@@ -8,7 +8,11 @@ python3 ../../script/run.py \
 --env ../../shell/${env}/_env.py \
 --init \
 --log-file build_api.log \
---param param_project_name=erp-file-resolve param_api_image=erp-file-resolve:play param_api_module_name=file-resolve \
+--param param_api_image=erp_portal:local \
+        param_api_module_name=portal \
+        param_api_source_config_dir=erp_play_portal/config \
+        param_api_source_data_dir=erp_play_portal/data \
+        param_api_source_log_dir=erp_play_portal/log \
 --cmd '
 from script.domain.source import base as base_source,java as java_source
 java_source.build_api()
