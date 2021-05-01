e5d2dfa68eceab3c29fec3f0fff97d1dff86c8a8
Bruce CHEN <805899926@qq.com>
2021-05-01 16:41:39 +0800

    #

--- shell/local/build_api.sh
+++ shell/local/build_api.sh
@@ -3,6 +3,7 @@
 cd $(cd "$(dirname "$0")"; pwd)
 
 env="local"
+source_mount="/volume1/docker-data/volumes/erp_play_portal"
 
 python3 ../../script/run.py \
 --env ../../shell/${env}/_env.py \
@@ -10,9 +11,9 @@ python3 ../../script/run.py \
 --log-file build_api.log \
 --param param_api_image=erp_portal:local \
         param_api_module_name=portal \
-        param_api_source_config_dir=erp_play_portal/config \
-        param_api_source_data_dir=erp_play_portal/data \
-        param_api_source_log_dir=erp_play_portal/log \
+        param_api_config_path=${source_mount}/config \
+        param_api_data_path=${source_mount}/data \
+        param_api_log_path=${source_mount}/log \
 --cmd '
 from script.domain.source import base as base_source,java as java_source
 java_source.build_api()
