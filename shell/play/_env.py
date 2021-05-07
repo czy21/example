ea25a9505c05753a29fdec507253fd8d3ba90610
Bruce CHEN <805899926@qq.com>
2021-05-07 11:04:05 +0800

    #

--- shell/play/_env.py
+++ shell/play/_env.py
@@ -9,7 +9,7 @@ qualify_project_env = "_".join([env_common.param_project_name, param_env_suffix]
 param_main_db_name = qualify_project_env
 param_main_db_bak_name = "_".join([param_main_db_name, "bak"])
 
-db_host = "192.168.2.3"
+db_host = "192.168.2.23"
 
 # redis
 param_main_redis_host = db_host
