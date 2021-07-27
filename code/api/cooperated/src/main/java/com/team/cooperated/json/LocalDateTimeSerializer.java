a0445eeba6290ea91df8e543293bbe71c9c93c4d
Bruce CHEN <805899926@qq.com>
2021-07-28 00:01:56 +0800

    #

--- code/api/cooperated/src/main/java/com/team/cooperated/json/LocalDateTimeSerializer.java
+++ code/api/cooperated/src/main/java/com/team/cooperated/json/LocalDateTimeSerializer.java
@@ -3,6 +3,7 @@ package com.team.cooperated.json;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.team.infrastructure.util.DateTimeUtil;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -11,6 +12,6 @@ import java.time.ZoneOffset;
 public class LocalDateTimeSerializer extends JsonSerializer<LocalDateTime> {
     @Override
     public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-        gen.writeNumber(value.toInstant(ZoneOffset.ofHours(8)).toEpochMilli());
+        gen.writeNumber(DateTimeUtil.toTimeStamp(value));
     }
 }
--- code/api/infrastructure/src/main/java/com/team/infrastructure/util/DateTimeUtil.java
+++ code/api/infrastructure/src/main/java/com/team/infrastructure/util/DateTimeUtil.java
@@ -2,29 +2,34 @@ package com.team.infrastructure.util;
 
 import java.time.*;
 import java.time.temporal.ChronoUnit;
+import java.util.concurrent.TimeUnit;
 
 
 public class DateTimeUtil {
 
-    private static final ZoneOffset ZONE_OFF_SET = ZoneOffset.ofHours(8);
-
     public static Long toTimeStamp(LocalDateTime localDateTime) {
-        return localDateTime.toInstant(ZONE_OFF_SET).toEpochMilli();
+        return TimeUnit.SECONDS.toMicros(localDateTime.atZone(ZoneId.systemDefault()).toEpochSecond()) + TimeUnit.NANOSECONDS.toMicros(localDateTime.getNano());
     }
 
     public static LocalDateTime toLocalDateTime(Long timeStamp) {
         String tStr = timeStamp.toString();
         if (tStr.length() == 16) {
-            return LocalDateTime.ofInstant(Instant.ofEpochMilli(timeStamp / 1_000).plus(timeStamp % 1_000, ChronoUnit.MICROS), ZoneId.systemDefault());
+            return LocalDateTime.ofInstant(Instant.ofEpochSecond(
+                    TimeUnit.NANOSECONDS.toSeconds(timeStamp),
+                    (timeStamp % 1_000_000_000L)), ZoneId.systemDefault());
         }
         return LocalDateTime.ofInstant(Instant.ofEpochMilli(timeStamp), ZoneId.systemDefault());
     }
 
     public static Long toTimeStamp(LocalDate localDate) {
-        return localDate.atStartOfDay(ZONE_OFF_SET).toInstant().toEpochMilli();
+        return TimeUnit.SECONDS.toMicros(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant().getEpochSecond());
     }
 
     public static LocalDate toLocalDate(Long timeStamp) {
-        return Instant.ofEpochMilli(timeStamp).atZone(ZONE_OFF_SET).toLocalDate();
+        String tStr = timeStamp.toString();
+        if (tStr.length() == 16) {
+            return LocalDate.ofInstant(Instant.ofEpochMilli(timeStamp / 1_000).plus(timeStamp % 1_000, ChronoUnit.MICROS), ZoneId.systemDefault());
+        }
+        return LocalDate.ofInstant(Instant.ofEpochMilli(timeStamp), ZoneId.systemDefault());
     }
 }
