f585e4ee1ab0603c38609d712039edf5d15ab3aa
Bruce CHEN <805899926@qq.com>
2021-08-03 21:53:44 +0800

    #

--- code/api/application/src/main/java/com/team/application/ApplicationConfig.java
+++ code/api/application/src/main/java/com/team/application/ApplicationConfig.java
@@ -10,6 +10,7 @@ import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.SeekToCurrentErrorHandler;
 import org.springframework.kafka.support.converter.RecordMessageConverter;
 import org.springframework.kafka.support.converter.StringJsonMessageConverter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.util.backoff.FixedBackOff;
 
 //@Configuration
@@ -19,32 +20,32 @@ public class ApplicationConfig {
     public static final String SPI_FILE_TOPIC = "spiFileTopic";
     public static final String SPI_DATA_TOPIC = "spiDataTopic";
 
-    @Bean
-    NewTopic materialTopic() {
-        return TopicBuilder.name(SPI_FILE_TOPIC)
-                .partitions(1)
-                .replicas(1)
-                .build();
-    }
-
-    @Bean
-    NewTopic spiDataTopic() {
-        return TopicBuilder.name(SPI_DATA_TOPIC)
-                .partitions(1)
-                .replicas(1)
-                .build();
-    }
-
-    @Bean
-    public SeekToCurrentErrorHandler errorHandler(KafkaOperations<Object, Object> template) {
-        return new SeekToCurrentErrorHandler(
-                new DeadLetterPublishingRecoverer(template), new FixedBackOff(1000L, 2));
-    }
-
-    @Bean
-    public RecordMessageConverter converter() {
-        return new StringJsonMessageConverter();
-    }
+//    @Bean
+//    NewTopic materialTopic() {
+//        return TopicBuilder.name(SPI_FILE_TOPIC)
+//                .partitions(1)
+//                .replicas(1)
+//                .build();
+//    }
+//
+//    @Bean
+//    NewTopic spiDataTopic() {
+//        return TopicBuilder.name(SPI_DATA_TOPIC)
+//                .partitions(1)
+//                .replicas(1)
+//                .build();
+//    }
+//
+//    @Bean
+//    public SeekToCurrentErrorHandler errorHandler(KafkaOperations<Object, Object> template) {
+//        return new SeekToCurrentErrorHandler(
+//                new DeadLetterPublishingRecoverer(template), new FixedBackOff(1000L, 2));
+//    }
+//
+//    @Bean
+//    public RecordMessageConverter converter() {
+//        return new StringJsonMessageConverter();
+//    }
 
 
 }
