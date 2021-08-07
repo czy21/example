eb15a49ae118f98e26bd0aa61e7d5b4de331f0e2
Bruce CHEN <805899926@qq.com>
2021-08-07 19:13:17 +0800

    #

--- code/api/portal/src/main/java/com/team/portal/configure/PortalConfiguration.java
+++ code/api/portal/src/main/java/com/team/portal/configure/PortalConfiguration.java
@@ -1,5 +1,9 @@
 package com.team.portal.configure;
 
+import com.team.application.service.impl.HBaseService;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,5 +11,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class PortalConfiguration {
 
+
+    @Value("${hbase.zookeeper.quorum}")
+    private String zookeeper;
+
+    @Bean
+    public HBaseService config() {
+        org.apache.hadoop.conf.Configuration config = HBaseConfiguration.create();
+        config.set("hbase.zookeeper.quorum",zookeeper);
+        return new HBaseService(config);
+    }
 
 }
