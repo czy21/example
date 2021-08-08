b7803bf0ae9fa97871f932576bf3171b2e3d35d3
Bruce CHEN <805899926@qq.com>
2021-08-08 22:10:06 +0800

    #

--- shell/local/backup_cp_rdb.sh
+++ shell/local/backup_cp_rdb.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 cd $(cd "$(dirname "$0")"; pwd)
+sh_name="$(basename ${0%.*})"
 
 env="local"
 
 python3 ../../script/run.py \
 --env ../../shell/${env}/_env.py \
---log-file backup_cp_rdb.log \
+--log-file ${sh_name}.log \
 --cmd '
 from script.domain.source import mysql as mysql_source
 mysql_source.backup_mysql()
--- shell/local/build_api.sh
+++ shell/local/build_api.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
 cd $(cd "$(dirname "$0")"; pwd)
+sh_name="$(basename ${0%.*})"
 
 env="local"
-source_mount="/volume1/docker-data/volumes/erp_play_portal"
 
 python3 ../../script/run.py \
 --env ../../shell/${env}/_env.py \
 --init \
---log-file build_api.log \
---param param_api_image=erp_portal:local \
-        param_api_module_name=portal \
-        param_api_config_path=${source_mount}/config \
-        param_api_data_path=${source_mount}/data \
-        param_api_log_path=${source_mount}/log \
+--log-file ${sh_name}.log \
+--param param_api_module_name=portal \
 --cmd '
 from script.domain.source import base as base_source,java as java_source
 java_source.build_api()
-base_source.build_template_dict()
-
 '
