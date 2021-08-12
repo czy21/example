07796743ff43513a87f71fe4ba6adce8addf220a
Bruce CHEN <805899926@qq.com>
2021-08-12 18:41:26 +0800

    #

---
+++ code/api/domain/src/main/java/com/team/domain/mapper/RepositoryMapper.java
@@ -0,0 +1,14 @@
+package com.team.domain.mapper;
+
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.mapping.ResultSetType;
+
+import java.util.List;
+import java.util.Map;
+
+public interface RepositoryMapper {
+    @Select(value = "${sql}")
+    @Options(resultSetType = ResultSetType.FORWARD_ONLY, fetchSize = Integer.MIN_VALUE)
+    List<Map<String, Object>> selectDynamicSql(Map<String, Object> parameters);
+}
