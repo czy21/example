0bf7a6c1b7d97380b017b831a6d78a0cc00f95ee
Bruce CHEN <805899926@qq.com>
2021-08-22 01:39:09 +0800

    #

--- code/api/portal/src/main/java/com/team/portal/PortalApplication.java
+++ code/api/portal/src/main/java/com/team/portal/PortalApplication.java
@@ -3,10 +3,13 @@ package com.team.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy(proxyTargetClass=true)
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        ElasticsearchDataAutoConfiguration.class
+})
 public class PortalApplication {
     public static void main(String[] args) {
         SpringApplication.run(PortalApplication.class, args);
