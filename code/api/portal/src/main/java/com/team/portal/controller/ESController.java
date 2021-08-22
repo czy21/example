ab79bb9cf1ea10a6065acc443e165274ddc08cd7
Bruce CHEN <805899926@qq.com>
2021-08-22 11:18:09 +0800

    #

--- code/api/portal/src/main/java/com/team/portal/controller/ESController.java
+++ code/api/portal/src/main/java/com/team/portal/controller/ESController.java
@@ -1,16 +1,19 @@
 package com.team.portal.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.team.cooperated.controller.BaseController;
-import org.elasticsearch.action.get.GetRequest;
+import lombok.SneakyThrows;
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "es")
@@ -19,10 +22,17 @@ public class ESController extends BaseController {
     @Autowired
     RestHighLevelClient highLevelClient;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @SneakyThrows
     @PostMapping(path = "put")
-    public Map<String, Object> putObj() throws IOException {
-        GetRequest getRequest = new GetRequest("post", "customer");
-        var p = highLevelClient.get(getRequest, RequestOptions.DEFAULT);
+    public Map<String, Object> putObj() {
+        IndexRequest request = new IndexRequest("posts");
+        request.id(UUID.randomUUID().toString());
+        String jsonString = objectMapper.writeValueAsString(Map.of("name", "陈昭宇"));
+        request.source(jsonString, XContentType.JSON);
+        highLevelClient.index(request, RequestOptions.DEFAULT);
         return Map.of("status", "success");
     }
 }
