4f060f3dd20767e7806bc1ec39ffba7bd68b69d0
Bruce CHEN <805899926@qq.com>
2021-08-22 21:36:43 +0800

    #

--- shell/local/recover_gz_mysql.sh
+++ shell/local/recover_gz_mysql.sh
@@ -7,7 +7,6 @@ env="local"
 
 python3 ../../script/run.py \
 --env ../../shell/${env}/_env.py \
---init \
 --log-file ${sh_name}.log \
 --cmd '
 from script.domain.source import mysql as mysql_source
