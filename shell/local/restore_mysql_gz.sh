9f789d18efc459d90227026a47aea0458a6cc0eb
Bruce CHEN <805899926@qq.com>
2021-08-25 23:09:43 +0800

    #

--- shell/local/restore_mysql_gz.sh
+++ shell/local/restore_mysql_gz.sh
@@ -9,6 +9,6 @@ python3 ../../script/run.py \
 --env ../../shell/${env}/_env.py \
 --log-file ${sh_name}.log \
 --cmd '
-from script.domain.source import mongo as mongo_source
-mongo_source.backup_gz()
+from script.domain.source import mysql as db_source
+db_source.restore_gz()
 '
