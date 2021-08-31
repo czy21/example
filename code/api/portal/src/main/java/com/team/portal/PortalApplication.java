d29cfd84746acc43cd995c4c215719b8f8767dcf
zhaoyu.chen <805899926@qq.com>
2021-08-31 17:18:10 +0800

    #

--- code/api/portal/src/main/java/com/team/portal/PortalApplication.java
+++ code/api/portal/src/main/java/com/team/portal/PortalApplication.java
@@ -3,14 +3,10 @@ package com.team.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
-@SpringBootApplication(exclude = {
-        ElasticsearchDataAutoConfiguration.class
-})
-@EnableDiscoveryClient
+@SpringBootApplication
+//@EnableDiscoveryClient
 public class PortalApplication {
     public static void main(String[] args) {
         SpringApplication.run(PortalApplication.class, args);
