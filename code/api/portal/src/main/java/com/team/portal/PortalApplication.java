2552a83bedf76d42ae3f37c57de3959ebd17311d
zhaoyu.chen <805899926@qq.com>
2021-09-01 13:19:06 +0800

    #

--- code/api/portal/src/main/java/com/team/portal/PortalApplication.java
+++ code/api/portal/src/main/java/com/team/portal/PortalApplication.java
@@ -3,10 +3,11 @@ package com.team.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class PortalApplication {
     public static void main(String[] args) {
         SpringApplication.run(PortalApplication.class, args);
