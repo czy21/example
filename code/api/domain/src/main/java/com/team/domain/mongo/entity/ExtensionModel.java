fb5aa9785f9dc91312b3185a324c02d88c03856b
zhaoyu.chen <805899926@qq.com>
2021-09-02 18:42:51 +0800

    #

--- code/api/domain/src/main/java/com/team/domain/mongo/entity/ExtensionModel.java
+++ code/api/domain/src/main/java/com/team/domain/mongo/entity/ExtensionModel.java
@@ -2,9 +2,11 @@ package com.team.domain.mongo.entity;
 
 import lombok.Data;
 
+import javax.persistence.Transient;
 import java.util.Map;
 
 @Data
 public class ExtensionModel {
+    @Transient
     private Map<String, Object> extension;
 }
