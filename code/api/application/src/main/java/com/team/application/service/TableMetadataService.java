6d9e0a9170cc5f293bdb5b8b75579940046974cc
zhaoyu.chen <805899926@qq.com>
2021-09-02 18:37:33 +0800

    #

--- code/api/application/src/main/java/com/team/application/service/TableMetadataService.java
+++ code/api/application/src/main/java/com/team/application/service/TableMetadataService.java
@@ -1,12 +1,12 @@
 package com.team.application.service;
 
-import com.team.domain.mongo.entity.TableMetadataEntity;
+import com.team.domain.mongo.entity.HBaseTableMetadataEntity;
 
 import java.util.List;
 import java.util.Map;
 
 public interface TableMetadataService {
-    TableMetadataEntity findOne(String namespace, String tableName);
+    HBaseTableMetadataEntity findOne(String namespace, String tableName);
 
     void save(String namespace, String tableName, Map<String, List<String>> columnFamily);
 
--- code/api/domain/src/main/java/com/team/domain/mongo/entity/ExtensionModel.java
+++ code/api/domain/src/main/java/com/team/domain/mongo/entity/ExtensionModel.java
@@ -2,11 +2,9 @@ package com.team.domain.mongo.entity;
 
 import lombok.Data;
 
-import javax.persistence.Transient;
 import java.util.Map;
 
 @Data
 public class ExtensionModel {
-    @Transient
     private Map<String, Object> extension;
 }
