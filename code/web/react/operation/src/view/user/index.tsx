a8289fe7db0d88c0a2b053993a80e01957e21841
Bruce CHEN <805899926@qq.com>
2021-09-21 21:19:25 +0800

    #

--- code/web/react/operation/src/view/user/index.tsx
+++ code/web/react/operation/src/view/user/index.tsx
@@ -10,30 +10,6 @@ let load = () => {
     // api.post("portal/user/search", {}).then(data => {
     //     console.log(data)
     // })
-    let ret: any = {}
-    Object.entries(stub.ref.lodash.groupBy(records, (t: any) => t.code))
-        .forEach(([k, v]) => {
-            ret[k] = {}
-            v.forEach(t => {
-                let url = new URL(t.url)
-                let list = {
-                    "url": url.pathname,
-                    "method": "POST"
-                }
-                let meta = {
-                    "url": `/${url.pathname.substring(1).split("/")[0]}/api/v1/get/admin/properties?adminPage=${stub.ref.lodash.camelCase(stub.ref.lodash.join([k, "insurance", t.domain_code], "_"))}`,
-                    "method": "GET"
-
-                }
-                ret[k][`${t.domain_code}`] = {}
-                ret[k][`${t.domain_code}`]["list"] = list
-                ret[k][`${t.domain_code}`]["meta"] = meta
-            })
-        })
-
-    console.log(JSON.stringify(ret))
-
-
 }
 
 export default class Index extends React.Component<any, any> {
