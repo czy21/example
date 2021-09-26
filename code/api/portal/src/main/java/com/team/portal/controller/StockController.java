a8fe4dbd78967e2605cca31fbf169bb9462945c6
zhaoyu.chen <805899926@qq.com>
2021-09-26 19:23:58 +0800

    #

--- code/api/portal/src/main/java/com/team/portal/controller/StockController.java
+++ code/api/portal/src/main/java/com/team/portal/controller/StockController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -29,6 +31,15 @@ public class StockController {
     public Map<String, Object> detail(@RequestBody StockDTO input) {
         stockService.reduce(input);
         return Map.of("status", "success");
+    }
+
+    @PostMapping(path = "gcTest1")
+    public Map<String, Object> gcTest1() throws InterruptedException {
+        List<byte[]> lists = new ArrayList<>();
+        while (true) {
+            Thread.sleep(1000 * 2);
+            lists.add(new byte[10 * 1024*1024]);
+        }
     }
 
 }
