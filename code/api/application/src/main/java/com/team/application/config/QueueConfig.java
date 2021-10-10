8bd39f1099132f564d8e3012caae0b516ee8c826
Bruce Chen <805899926@qq.com>
2021-10-10 21:45:51 +0800

    #

--- code/api/application/src/main/java/com/team/application/config/QueueConfig.java
+++ code/api/application/src/main/java/com/team/application/config/QueueConfig.java
@@ -9,6 +9,7 @@ public class QueueConfig {
 
     public static final String SPI_FILE_TOPIC = "spiFileTopic";
     public static final String SPI_DATA_TOPIC = "spiDataTopic";
+    public static final String ThroughputTest1_TOPIC = "ThroughputTest1";
 
     @Bean
     public Queue spiFileTopic() {
@@ -19,4 +20,7 @@ public class QueueConfig {
     public Queue spiDataTopic() {
         return new Queue(SPI_DATA_TOPIC, true);
     }
+
+    @Bean
+    public Queue ThroughputTest1(){return new Queue(ThroughputTest1_TOPIC, true);}
 }
