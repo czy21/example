0c4cc940403bc6a2be2f596b0d76237d12d0ddb9
zhaoyu.chen <805899926@qq.com>
2021-10-13 17:24:14 +0800

    #

---
+++ code/api/portal/src/main/java/com/team/portal/configure/MQConfigure.java
@@ -0,0 +1,51 @@
+package com.team.portal.configure;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.Channel;
+import com.team.application.config.QueueConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.lucene.queryparser.flexible.core.nodes.ProximityQueryNode;
+import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Configuration
+@Slf4j
+public class MQConfigure {
+
+    @Autowired
+    ObjectMapper objectMapper;
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+
+    @Bean
+    public MessageConverter jsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @RabbitListener(queues = QueueConfig.ThroughputTest1_TOPIC, concurrency = "1", ackMode = "MANUAL")
+    public void ThroughputTest1_TOPIConsumer(Map<String, Object> data, Message message, Channel channel) throws IOException {
+//        messages.add(data);
+        log.info(objectMapper.writeValueAsString(data));
+//        log.info(""+messages.size());
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+    }
+
+}
