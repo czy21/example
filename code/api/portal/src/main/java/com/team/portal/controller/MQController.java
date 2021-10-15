648d9bc878e9d72ea56aace856557d5d749ba2e2
zhaoyu.chen <805899926@qq.com>
2021-10-15 20:05:59 +0800

    #

--- code/api/portal/src/main/java/com/team/portal/controller/MQController.java
+++ code/api/portal/src/main/java/com/team/portal/controller/MQController.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.lang.ref.Reference;
@@ -24,6 +25,8 @@ public class MQController {
     RabbitTemplate rabbitTemplate;
     @Autowired
     ObjectMapper objectMapper;
+    @Autowired
+    KafkaTemplate<String, Object> kafkaTemplate;
 
     /*
     127.0.0.1:8080/mq/rabbitTest1?start=1&end=1000
@@ -33,9 +36,16 @@ public class MQController {
         rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
         log.info(StringUtils.join(List.of("==========", start), " "));
         IntStream.rangeClosed(start, end).forEach(t -> {
-            rabbitTemplate.convertAndSend(QueueConfig.ThroughputTest1_TOPIC, Map.of("id",t));
+            rabbitTemplate.convertAndSend(QueueConfig.ThroughputTest1_TOPIC, Map.of("id", t));
         });
         log.info(StringUtils.join(List.of("==========", end), " "));
         return Map.of("start", start, "end", end);
     }
+
+    @GetMapping(path = "kafkaTest1")
+    public Map<String, Object> kafkaTest1(@RequestParam("start") Integer start, @RequestParam("end") Integer end) {
+        kafkaTemplate.send(QueueConfig.SPI_FILE_TOPIC, "hello");
+        return Map.of("start", start, "end", end);
+    }
+
 }
