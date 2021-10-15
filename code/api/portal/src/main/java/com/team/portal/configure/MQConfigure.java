398966abb9475e0835f5797c086db2ac28939ce9
zhaoyu.chen <805899926@qq.com>
2021-10-15 20:05:59 +0800

    #

--- code/api/portal/src/main/java/com/team/portal/configure/MQConfigure.java
+++ code/api/portal/src/main/java/com/team/portal/configure/MQConfigure.java
@@ -1,10 +1,14 @@
 package com.team.portal.configure;
 
 
+import com.alibaba.excel.EasyExcel;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.Channel;
 import com.team.application.config.QueueConfig;
+import com.team.application.model.vo.MaterialVO;
+import com.team.application.util.MaterialUtil;
+import com.team.domain.entity.MaterialEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.lucene.queryparser.flexible.core.nodes.ProximityQueryNode;
 import org.springframework.amqp.core.AcknowledgeMode;
@@ -19,7 +23,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -48,6 +54,11 @@ public class MQConfigure {
         log.info(objectMapper.writeValueAsString(data));
 //        log.info(""+messages.size());
         channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+    }
+
+    @KafkaListener(topics = QueueConfig.SPI_FILE_TOPIC)
+    public void materialTopicReceive(Object data) throws IOException {
+        log.info(data.toString());
     }
 
 }
