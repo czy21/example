712bdb5ceecc5a2fea499e48002bed17c185fac3
Bruce Chen <805899926@qq.com>
2021-10-17 03:01:02 +0800

    #

--- db/mysql/1_version/100v/1_structure(s)/2_sys/1_ent_task.sql
+++ db/mysql/1_version/100v/1_structure(s)/2_sys/1_ent_task.sql
@@ -4,10 +4,7 @@ CREATE TABLE `ent_task`  (
   `status` varchar(100) not null,
   `execute_count` int not null,
   `batch_id` varchar(36) null,
-`created_date` TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-                          `created_user` varchar(36) DEFAULT NULL,
-                          `modified_date` TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-                          `modified_user` varchar(36) DEFAULT NULL,
+  ${{{TrackedColumns}}},
   PRIMARY KEY (`id`)
 ) COMMENT='任务';
 
@@ -15,11 +12,6 @@ CREATE TABLE `ent_task_log`  (
   `id` varchar(36)  NOT NULL,
   `code` varchar(100) not null,
   `batch_id` varchar(36) null,
-`created_date` TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-                          `created_user` varchar(36) DEFAULT NULL,
-                          `modified_date` TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-                          `modified_user` varchar(36) DEFAULT NULL,
+  ${{{TrackedColumns}}},
   PRIMARY KEY (`id`)
 ) COMMENT='任务';
-
---   ${{{TrackedColumns}}},
