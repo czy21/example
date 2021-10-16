bee8d2622ebb5c07dc48da739c91481892b7fd3e
zhaoyu.chen <805899926@qq.com>
2021-10-16 18:43:22 +0800

    #

--- code/api/portal/src/main/java/com/team/portal/configure/MQConfigure.java
+++ code/api/portal/src/main/java/com/team/portal/configure/MQConfigure.java
@@ -1,36 +1,36 @@
 package com.team.portal.configure;
 
 
-import com.alibaba.excel.EasyExcel;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.Channel;
 import com.team.application.config.QueueConfig;
 import com.team.application.model.vo.MaterialVO;
-import com.team.application.util.MaterialUtil;
-import com.team.domain.entity.MaterialEntity;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.lucene.queryparser.flexible.core.nodes.ProximityQueryNode;
-import org.springframework.amqp.core.AcknowledgeMode;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.support.converter.RecordMessageConverter;
+import org.springframework.kafka.support.converter.StringJsonMessageConverter;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 @Configuration
 @EnableKafka
@@ -44,8 +44,13 @@ public class MQConfigure {
 
 
     @Bean
-    public MessageConverter jsonMessageConverter(){
+    public MessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RecordMessageConverter converter() {
+        return new StringJsonMessageConverter();
     }
 
     @RabbitListener(queues = QueueConfig.ThroughputTest1_TOPIC, concurrency = "1", ackMode = "MANUAL")
@@ -56,9 +61,10 @@ public class MQConfigure {
         channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
     }
 
+
     @KafkaListener(topics = QueueConfig.SPI_FILE_TOPIC)
-    public void materialTopicReceive(Object data) throws IOException {
-        log.info(data.toString());
+    public void materialTopicReceive(Map<String,Object> message) throws IOException {
+        log.info(message.toString());
     }
 
 }
