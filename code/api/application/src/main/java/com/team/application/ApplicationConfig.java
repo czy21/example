4cf85c5210c354a930c0b697a1b58713eb3304f0
Bruce Chen <805899926@qq.com>
2021-10-17 21:33:43 +0800

    #

--- code/api/application/src/main/java/com/team/application/ApplicationConfig.java
+++ code/api/application/src/main/java/com/team/application/ApplicationConfig.java
@@ -1,6 +1,9 @@
 package com.team.application;
 
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -17,4 +20,5 @@ public class ApplicationConfig {
 //        config.set("hbase.zookeeper.quorum",zookeeper);
 //        return new HBaseServiceImpl(config);
 //    }
+
 }
--- code/api/file_resolve/src/main/java/com/team/fileresolve/receiver/RabbitReceiver.java
+++ code/api/file_resolve/src/main/java/com/team/fileresolve/receiver/RabbitReceiver.java
@@ -1,8 +1,12 @@
 package com.team.fileresolve.receiver;
 
+import com.alibaba.excel.EasyExcel;
 import com.team.application.config.QueueConfig;
 import com.team.application.model.vo.MaterialVO;
+import com.team.application.util.MaterialUtil;
+import com.team.domain.entity.MaterialEntity;
 import com.team.domain.mapper.MaterialMapper;
+import com.team.fileresolve.listener.FileListener;
 import org.springframework.amqp.core.AcknowledgeMode;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
@@ -12,29 +16,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.IOException;
+
 @Component
 public class RabbitReceiver {
 
     @Autowired
     MaterialMapper materialMapper;
 
-    @Bean(name = "singleListenerContainer")
-    public SimpleRabbitListenerContainerFactory singleListenerContainer(ConnectionFactory connectionFactory) {
-        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-        factory.setMessageConverter(new Jackson2JsonMessageConverter());
-        factory.setConcurrentConsumers(1);
-        factory.setPrefetchCount(1);
-        factory.setAcknowledgeMode(AcknowledgeMode.AUTO);
-        return factory;
+    @RabbitListener(queues = QueueConfig.SPI_FILE_TOPIC)
+    public void receive(MaterialVO materialVO) throws IOException {
+        MaterialEntity materialEntity = materialMapper.selectById(materialVO.getUid());
+        File f = MaterialUtil.getFile(materialEntity.getPath(), materialEntity.getMaterialTarget().getRootPath());
+        EasyExcel.read(f, new FileListener(kafkaTemplate, fileColumnMappingRepository)).sheet().doRead();
     }
 
-    @RabbitListener(queues = QueueConfig.SPI_FILE_TOPIC,containerFactory = "singleListenerContainer")
-    public void receive(MaterialVO materialVO) {
-//        System.out.println("aaa");
-        System.out.println(materialVO);
-//        MaterialEntity materialEntity = materialMapper.selectById(materialvo.getUid());
-//        File f = MaterialUtil.getFile(materialEntity.getPath(), materialEntity.getMaterialTarget().getRootPath());
-//        EasyExcel.read(f, new FileListener(kafkaTemplate, fileColumnMappingRepository)).sheet().doRead();
-    }
+
+
 }
