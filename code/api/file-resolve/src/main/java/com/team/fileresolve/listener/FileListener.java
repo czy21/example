b526b2d97301e0866a7e2105e8e8bd1a90cc33ed
zhaoyu.chen <805899926@qq.com>
2021-10-18 20:13:52 +0800

    #

--- code/api/file-resolve/src/main/java/com/team/fileresolve/listener/FileListener.java
+++ code/api/file-resolve/src/main/java/com/team/fileresolve/listener/FileListener.java
@@ -7,38 +7,58 @@ import com.team.domain.mongo.entity.FileColumnMappingEntity;
 import com.team.domain.mongo.repository.FileColumnMappingRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.kafka.core.KafkaTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 @Slf4j
-public class FileListener extends AnalysisEventListener<Map<String, Object>> {
+public class FileListener extends AnalysisEventListener<Map<Integer, Object>> {
 
-    Map<String, Map<Integer, String>> head = new HashMap<>();
+    Map<String, Map<String, FileColumnMappingEntity.Field>> columnMetadata = new ConcurrentHashMap<>();
     RabbitTemplate rabbitTemplate;
     FileColumnMappingRepository fileColumnMappingRepository;
-    FileColumnMappingEntity fileColumnMappingEntity;
 
-    public FileListener(RabbitTemplate rabbitTemplate,FileColumnMappingRepository fileColumnMappingRepository) {
-        this.rabbitTemplate=rabbitTemplate;
+    public FileListener(RabbitTemplate rabbitTemplate, FileColumnMappingRepository fileColumnMappingRepository) {
+        this.rabbitTemplate = rabbitTemplate;
         this.fileColumnMappingRepository = fileColumnMappingRepository;
     }
 
     @Override
     public void invokeHeadMap(Map<Integer, String> headMap, AnalysisContext context) {
         String currentSheet = context.readSheetHolder().getSheetName();
-        head.put(context.readSheetHolder().getSheetName(), headMap);
-        fileColumnMappingEntity = fileColumnMappingRepository.findByBusinessTypeEquals(currentSheet);
+        var fe = fileColumnMappingRepository.findByBusinessTypeEquals(currentSheet);
+        columnMetadata.put(currentSheet, fe.getFields().stream().collect(Collectors.toMap(FileColumnMappingEntity.Field::getKey, t -> {
+            t.setBusinessType(currentSheet);
+            t.setTableName(fe.getTableName());
+            headMap.entrySet().stream().filter(p -> p.getValue().equals(t.getHeader()))
+                    .findFirst()
+                    .ifPresent(h -> t.setIndex(h.getKey()));
+            return t;
+        })));
     }
 
     @Override
-    public void invoke(Map<String, Object> data, AnalysisContext context) {
-        data.put("sqlCommand", "INSERT");
-        data.put("businessType", context.readSheetHolder().getSheetName());
+    public void invoke(Map<Integer, Object> data, AnalysisContext context) {
+        String businessType = context.readSheetHolder().getSheetName();
+        String sqlCommand = "INSERT";
 
-        head.get(context.readSheetHolder().getSheetName()).forEach((k, v) -> data.put(v, data.remove(k)));
-        rabbitTemplate.convertAndSend(QueueConfig.SPI_DATA_TOPIC, data);
+        var msg = columnMetadata.get(businessType).entrySet().stream()
+                .map(t -> {
+                    Map<String, Object> obj = new HashMap<>();
+                    obj.put("businessType", businessType);
+                    obj.put("sqlCommand", sqlCommand);
+                    obj.put("tableName", t.getValue().getTableName());
+                    Integer colIndex = Optional.ofNullable(t.getValue().getIndex()).orElse(null);
+                    if (colIndex != null) {
+                        Object cellValue = data.get(colIndex);
+                        obj.put(t.getKey(), cellValue);
+                    }
+                    return obj;
+                }).collect(HashMap::new, Map::putAll, Map::putAll);
+        rabbitTemplate.convertAndSend(QueueConfig.SPI_DATA_TOPIC, msg);
     }
 
     @Override
