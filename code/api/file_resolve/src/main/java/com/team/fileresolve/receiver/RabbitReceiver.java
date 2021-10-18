0a2f08b5695f0ab3a7e8784a663d1cb2da58d041
zhaoyu.chen <805899926@qq.com>
2021-10-18 15:44:27 +0800

    #

--- code/api/file_resolve/src/main/java/com/team/fileresolve/receiver/RabbitReceiver.java
+++ code/api/file_resolve/src/main/java/com/team/fileresolve/receiver/RabbitReceiver.java
@@ -1,25 +1,15 @@
 package com.team.fileresolve.receiver;
 
-import com.alibaba.excel.EasyExcel;
 import com.team.application.config.QueueConfig;
 import com.team.application.model.vo.MaterialVO;
-import com.team.application.util.MaterialUtil;
-import com.team.domain.entity.MaterialEntity;
 import com.team.domain.mapper.MaterialMapper;
 import com.team.domain.mongo.repository.FileColumnMappingRepository;
-import com.team.fileresolve.listener.FileListener;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.AcknowledgeMode;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
 import java.io.IOException;
 
 @Slf4j
