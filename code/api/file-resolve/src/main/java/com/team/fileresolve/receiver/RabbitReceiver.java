29291a1055a888162c877850bd57cedc77d70f17
zhaoyu.chen <805899926@qq.com>
2021-10-19 13:36:52 +0800

    #

--- code/api/file-resolve/src/main/java/com/team/fileresolve/receiver/RabbitReceiver.java
+++ code/api/file-resolve/src/main/java/com/team/fileresolve/receiver/RabbitReceiver.java
@@ -5,23 +5,42 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.Channel;
 import com.team.application.automap.RowAutoMap;
 import com.team.application.config.QueueConfig;
+import com.team.application.model.RowModel;
 import com.team.application.model.vo.MaterialVO;
 import com.team.domain.entity.MaterialEntity;
 import com.team.domain.mapper.MaterialMapper;
 import com.team.domain.mapper.RepositoryMapper;
 import com.team.domain.mongo.repository.FileColumnMappingRepository;
 import com.team.fileresolve.listener.FileListener;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.MutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.amqp.core.AcknowledgeMode;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @Slf4j
 @Component
@@ -50,55 +69,55 @@ public class RabbitReceiver {
         channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
     }
 
-//    @Bean
-//    public SimpleRabbitListenerContainerFactory batchListenerFactory(ConnectionFactory connectionFactory,
-//                                                                     @Qualifier("jsonMessageConverter") MessageConverter messageConverter) {
-//        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-//        factory.setConnectionFactory(connectionFactory);
-//        factory.setBatchListener(true);
-//        factory.setBatchSize(20000);
-//        factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
-//        factory.setMessageConverter(messageConverter);
-//        factory.setPrefetchCount(20000);
-//        factory.setConcurrentConsumers(5);
-//        return factory;
-//    }
+    @Bean
+    public SimpleRabbitListenerContainerFactory batchListenerFactory(ConnectionFactory connectionFactory,
+                                                                     @Qualifier("jsonMessageConverter") MessageConverter messageConverter) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setBatchListener(true);
+        factory.setBatchSize(5000);
+        factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        factory.setMessageConverter(messageConverter);
+        factory.setPrefetchCount(5000);
+        factory.setConcurrentConsumers(5);
+        return factory;
+    }
 
-//    @SneakyThrows
-//    @RabbitListener(queues = QueueConfig.SPI_DATA_TOPIC, containerFactory = "batchListenerFactory")
-//    public void rowData(List<RowModel> rows, Message message, Channel channel) {
-//        var gRows = rows.stream().collect(Collectors.groupingBy(RowModel::getBusinessType, Collectors.toList()));
-//        gRows.forEach((key, value) -> {
-//            var tableMeta = value.get(0);
-//            List<MutablePair<String, String>> columns =
-//                    Stream.concat(
-//                            Stream.of(MutablePair.of("id", "id")),
-//                            tableMeta.getData().values().stream().map(t -> MutablePair.of(t.getKey(), t.getColumn()))
-//                    ).collect(Collectors.toList());
-//
-//            String values = IntStream.rangeClosed(1, columns.size()).mapToObj(p -> "?").collect(Collectors.joining(","));
-//            jdbcTemplate.batchUpdate(
-//                    "insert into " + tableMeta.getTableName() + "(" + columns.stream().map(Pair::getValue).collect(Collectors.joining(",")) + ") values(" + values + ")",
-//                    new BatchPreparedStatementSetter() {
-//                        @Override
-//                        public void setValues(PreparedStatement ps, int i) throws SQLException {
-//                            var t = value.get(i).getData();
-//                            for (int j = 0; j < columns.size(); j++) {
-//                                String c = columns.get(j).getKey();
-//                                Object value = Optional.ofNullable(t.get(c)).map(RowModel.ColModel::getValue).orElse(null);
-//                                ps.setObject(j + 1, c.equals("id") ? UUID.randomUUID().toString().replace("-", "") : value);
-//                            }
-//                        }
-//
-//                        @Override
-//                        public int getBatchSize() {
-//                            return value.size();
-//                        }
-//                    }
-//            );
-//
-//        });
-//        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
-//    }
+    @SneakyThrows
+    @RabbitListener(queues = QueueConfig.SPI_DATA_TOPIC, containerFactory = "batchListenerFactory")
+    public void rowData(List<RowModel> rows, Message message, Channel channel) {
+        var gRows = rows.stream().collect(Collectors.groupingBy(RowModel::getBusinessType, Collectors.toList()));
+        gRows.forEach((key, value) -> {
+            var tableMeta = value.get(0);
+            List<MutablePair<String, String>> columns =
+                    Stream.concat(
+                            Stream.of(MutablePair.of("id", "id")),
+                            tableMeta.getData().values().stream().map(t -> MutablePair.of(t.getKey(), t.getColumn()))
+                    ).collect(Collectors.toList());
+
+            String values = IntStream.rangeClosed(1, columns.size()).mapToObj(p -> "?").collect(Collectors.joining(","));
+            jdbcTemplate.batchUpdate(
+                    "insert into " + tableMeta.getTableName() + "(" + columns.stream().map(Pair::getValue).collect(Collectors.joining(",")) + ") values(" + values + ")",
+                    new BatchPreparedStatementSetter() {
+                        @Override
+                        public void setValues(PreparedStatement ps, int i) throws SQLException {
+                            var t = value.get(i).getData();
+                            for (int j = 0; j < columns.size(); j++) {
+                                String c = columns.get(j).getKey();
+                                Object value = Optional.ofNullable(t.get(c)).map(RowModel.ColModel::getValue).orElse(null);
+                                ps.setObject(j + 1, c.equals("id") ? UUID.randomUUID().toString().replace("-", "") : value);
+                            }
+                        }
+
+                        @Override
+                        public int getBatchSize() {
+                            return value.size();
+                        }
+                    }
+            );
+
+        });
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+    }
 
 }
