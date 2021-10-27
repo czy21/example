26777a25115e854deac1e15387d7389096cd2fcc
Bruce CHEN <805899926@qq.com>
2021-10-27 22:17:20 +0800

    #

--- code/api/cooperated/src/main/java/com/team/cooperated/CooperatedConfigure.java
+++ code/api/cooperated/src/main/java/com/team/cooperated/CooperatedConfigure.java
@@ -1,7 +1,7 @@
 package com.team.cooperated;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.team.cooperated.configure.JacksonConfigure;
+import com.team.infrastructure.json.JacksonConfigure;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,7 +23,6 @@ import java.util.List;
 @EnableWebMvc
 @EnableAsync
 @Configuration
-@Import(JacksonConfigure.class)
 public class CooperatedConfigure implements WebMvcConfigurer {
 
     @Autowired
--- code/api/infrastructure/src/main/java/com/team/infrastructure/json/LocalDateTimeDeserializer.java
+++ code/api/infrastructure/src/main/java/com/team/infrastructure/json/LocalDateTimeDeserializer.java
@@ -1,4 +1,4 @@
-package com.team.cooperated.json;
+package com.team.infrastructure.json;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
