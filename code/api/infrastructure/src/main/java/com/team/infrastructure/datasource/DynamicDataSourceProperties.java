2cc4c30eb36f254542e2d7c62455f4730629400f
Bruce CHEN <805899926@qq.com>
2021-10-30 12:57:34 +0800

    #

--- code/api/infrastructure/src/main/java/com/team/infrastructure/datasource/DynamicDataSourceProperties.java
+++ code/api/infrastructure/src/main/java/com/team/infrastructure/datasource/DynamicDataSourceProperties.java
@@ -5,13 +5,11 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Properties;
 
 @Data
 @Component
 @ConfigurationProperties(prefix = "spring.datasource.dynamic")
 public class DynamicDataSourceProperties {
-    private LinkedHashMap<String, HikariConfig> datasource;
+    private Map<String, HikariConfig> datasource;
 }
