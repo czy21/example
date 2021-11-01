167637c7b49227ff87603b8853ed639d6de29796
Bruce CHEN <805899926@qq.com>
2021-11-01 18:53:48 +0800

    #

--- code/api/infrastructure/src/main/java/com/team/infrastructure/util/DateTimeUtil.java
+++ code/api/infrastructure/src/main/java/com/team/infrastructure/util/DateTimeUtil.java
@@ -1,35 +1,24 @@
 package com.team.infrastructure.util;
 
 import java.time.*;
-import java.time.temporal.ChronoUnit;
-import java.util.concurrent.TimeUnit;
 
 
 public class DateTimeUtil {
 
     public static Long toTimeStamp(LocalDateTime localDateTime) {
-        return TimeUnit.SECONDS.toMicros(localDateTime.atZone(ZoneId.systemDefault()).toEpochSecond()) + TimeUnit.NANOSECONDS.toMicros(localDateTime.getNano());
+        return localDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
     }
 
     public static LocalDateTime toLocalDateTime(Long timeStamp) {
-        String tStr = timeStamp.toString();
-        if (tStr.length() == 16) {
-            return LocalDateTime.ofInstant(Instant.ofEpochSecond(
-                    TimeUnit.NANOSECONDS.toSeconds(timeStamp),
-                    (timeStamp % 1_000_000_000L)), ZoneId.systemDefault());
-        }
         return LocalDateTime.ofInstant(Instant.ofEpochMilli(timeStamp), ZoneId.systemDefault());
     }
 
     public static Long toTimeStamp(LocalDate localDate) {
-        return TimeUnit.SECONDS.toMicros(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant().getEpochSecond());
+        return localDate.atStartOfDay(ZoneOffset.systemDefault()).toInstant().toEpochMilli();
     }
 
     public static LocalDate toLocalDate(Long timeStamp) {
-        String tStr = timeStamp.toString();
-        if (tStr.length() == 16) {
-            return LocalDate.ofInstant(Instant.ofEpochMilli(timeStamp / 1_000).plus(timeStamp % 1_000, ChronoUnit.MICROS), ZoneId.systemDefault());
-        }
         return LocalDate.ofInstant(Instant.ofEpochMilli(timeStamp), ZoneId.systemDefault());
     }
+
 }
