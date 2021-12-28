3ce4f9433f566b9224abd1306c4fe6c4a23263e9
Bruce CHEN <805899926@qq.com>
2021-12-28 14:20:39 +0800

    #

--- code/api/application/src/main/java/com/team/application/config/QueueConfig.java
+++ code/api/application/src/main/java/com/team/application/config/QueueConfig.java
@@ -1,5 +1,6 @@
 package com.team.application.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -23,7 +24,7 @@ public class QueueConfig {
     }
 
     @Bean
-    public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+    public MessageConverter jsonMessageConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
     }
 }
