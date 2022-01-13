14122f4bb95a62e5b81ba52c4a67fc425f402451
Bruce Chen <805899926@qq.com>
2022-01-13 23:27:37 +0800

    #

--- code/web/vue/operation/src/router/index.ts
+++ code/web/vue/operation/src/router/index.ts
@@ -14,6 +14,11 @@ const routes: Array<RouteConfig> = [
                 path: "user",
                 component: () => import('@v/pages/user/index.vue')
             },
+            {
+                name: "菜单管理",
+                path: "menu",
+                component: () => import('@v/pages/menu/index.vue')
+            },
         ]
     },
 ]
