963968e1600264d65fcaa694d8a936ba1b047129
Bruce CHEN <805899926@qq.com>
2022-01-14 18:36:42 +0800

    #

--- code/api/file-resolve/src/main/java/com/team/fileresolve/FileResolveApplication.java
+++ code/api/file-resolve/src/main/java/com/team/fileresolve/FileResolveApplication.java
@@ -10,5 +10,6 @@ public class FileResolveApplication {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(FileResolveApplication.class);
         app.setAllowCircularReferences(true);
+        app.run(args);
     }
 }
