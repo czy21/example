db435ccc05a21233d6f3c20bc4e4db4bb957dc9d
Bruce CHEN <805899926@qq.com>
2022-01-19 13:29:30 +0800

    #

--- code/web/react/operation/webpack.config.js
+++ code/web/react/operation/webpack.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    overrideWebpackConfig: ({ webpackConfig, cracoConfig, pluginOptions, context: { env, paths } }) => {
+    overrideWebpackConfig: ({webpackConfig, cracoConfig, pluginOptions, context: {env, paths}}) => {
         if (pluginOptions.preText) {
             console.log(pluginOptions.preText);
         }
         // console.log(JSON.stringify(webpackConfig, null, 4))
-
+        webpackConfig.output.publicPath = process.env.NODE_ENV === "production" ? "/erp/" : "/"
         // Always return the config object.
         return webpackConfig;
     }
--- code/web/vue/operation/src/router/index.ts
+++ code/web/vue/operation/src/router/index.ts
@@ -26,7 +26,7 @@ const routes: Array<RouteConfig> = [
 
 const router = new VueRouter({
     mode: 'history',
-    base: '/erp/',
+    base:"/",
     routes
 })
 
