16d79e2313d6f0750eca399305ccb0a2b8605d42
Bruce CHEN <805899926@qq.com>
2022-01-19 17:39:09 +0800

    #

--- code/web/react/operation/src/App.tsx
+++ code/web/react/operation/src/App.tsx
@@ -8,10 +8,11 @@ import Home from "@/layout/Home";
 const store = createStore(rootReducer);
 
 function App() {
-    console.log(process.env.BASE_URL)
+    console.log(process.env.REACT_APP_BASE_URL)
+    console.log("aaa")
     return (
         <Provider store={store}>
-            <BrowserRouter basename={process.env.BASE_URL??"/react/"}>
+            <BrowserRouter basename={process.env.REACT_APP_BASE_URL??"/react/"}>
                 <Home/>
             </BrowserRouter>
         </Provider>
