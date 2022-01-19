39cc00aed418148bcd02e0ff6855d3d192cab093
Bruce CHEN <805899926@qq.com>
2022-01-19 14:21:56 +0800

    #

--- code/web/react/operation/src/App.tsx
+++ code/web/react/operation/src/App.tsx
@@ -10,7 +10,7 @@ const store = createStore(rootReducer);
 function App() {
     return (
         <Provider store={store}>
-            <BrowserRouter basename={"/erp/"}>
+            <BrowserRouter basename={process.env.BASE_URL}>
                 <Home/>
             </BrowserRouter>
         </Provider>
