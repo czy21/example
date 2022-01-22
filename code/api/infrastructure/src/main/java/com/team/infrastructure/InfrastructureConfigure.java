08c1fd17f085a9da13925d125bfb18c9c1ba4498
Bruce Chen <805899926@qq.com>
2022-01-22 11:58:39 +0800

    #

--- code/api/infrastructure/src/main/java/com/team/infrastructure/InfrastructureConfigure.java
+++ code/api/infrastructure/src/main/java/com/team/infrastructure/InfrastructureConfigure.java
@@ -1,10 +1,10 @@
 package com.team.infrastructure;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.team.infrastructure.datasource.DynamicDataSourceConfigure;
 import com.team.infrastructure.json.JacksonConfigure;
 import com.team.infrastructure.metadata.EntityMetadataHandler;
 import com.team.infrastructure.oss.OSSConfigure;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -12,7 +12,6 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -24,29 +23,24 @@ import java.util.Optional;
         OSSConfigure.class
 })
 class InfrastructureConfigure {
+
     @Bean
-    @ConditionalOnMissingBean
     public EntityMetadataHandler entityMetadataHandler() {
         return new EntityMetadataHandler();
     }
 
     @Bean
-    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    public AuditorAware<LocalDateTime> JPAEntityAuditor() {
+        return () -> Optional.of(LocalDateTime.now());
+    }
+
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper) {
         RedisTemplate<Object, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
-        template.setKeySerializer(new GenericJackson2JsonRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setKeySerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
         return template;
     }
-
-
-    @Component
-    public static class EntityAuditing implements AuditorAware<LocalDateTime> {
-        @Override
-        public Optional<LocalDateTime> getCurrentAuditor() {
-            return Optional.of(LocalDateTime.now());
-        }
-    }
-
 }
 
