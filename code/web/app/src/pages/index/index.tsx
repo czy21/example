7844d9b8a083c693913f165a51644b2a48d7fa9f
Bruce CHEN <805899926@qq.com>
2022-01-22 09:51:55 +0800

    #

--- code/web/app/src/pages/index/index.tsx
+++ code/web/app/src/pages/index/index.tsx
@@ -2,28 +2,6 @@ import React from "react";
 import { View, Text } from '@tarojs/components'
 import './index.less'
 
-// export default class Index extends Component {
-//
-//   componentWillMount () { }
-//
-//   componentDidMount () {
-//     console.log("world")
-//   }
-//
-//   componentWillUnmount () { }
-//
-//   componentDidShow () { }
-//
-//   componentDidHide () { }
-//
-//   render () {
-//     return (
-//       <View className='index'>
-//         <Text>Hello world!</Text>
-//       </View>
-//     )
-//   }
-// }
 const List: React.FC<any> = () => {
 
   return(
