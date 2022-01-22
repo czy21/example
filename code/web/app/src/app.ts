99e5519bf33d7c74bbb05b4415b58b454158bd38
Bruce CHEN <805899926@qq.com>
2022-01-22 09:51:55 +0800

    #

--- code/web/app/src/app.ts
+++ code/web/app/src/app.ts
@@ -1,20 +1,8 @@
-import { Component } from 'react'
+import React from 'react'
 import './app.less'
 
-class App extends Component {
-
-  componentDidMount () {}
-
-  componentDidShow () {}
-
-  componentDidHide () {}
-
-  componentDidCatchError () {}
-
-  // this.props.children 是将要会渲染的页面
-  render () {
-    return this.props.children
-  }
+const App: React.FC<any> = (props: any) => {
+  return props.children
 }
 
 export default App
