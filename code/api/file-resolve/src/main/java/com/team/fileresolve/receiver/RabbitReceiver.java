31fcf27604557699a84d7eca574ac2c98e484852
Bruce CHEN <805899926@qq.com>
2022-01-24 18:15:10 +0800

    #

--- code/api/file-resolve/src/main/java/com/team/fileresolve/receiver/RabbitReceiver.java
+++ code/api/file-resolve/src/main/java/com/team/fileresolve/receiver/RabbitReceiver.java
@@ -113,7 +113,7 @@ public class RabbitReceiver {
         SQL sql = new SQL();
         sql.INSERT_INTO(tableMeta.getTableName());
         sql.INTO_COLUMNS(columns.stream().map(Pair::getValue).collect(Collectors.toList()).toArray(new String[]{}));
-        sql.INTO_VALUES(columns.stream().map(t -> t.getValue().equals("id") ? "replace(uuid(),'-','')" : StringUtils.join(List.of("#{", t.getKey(), "}"), "")).collect(Collectors.toList()).toArray(new String[]{}));
+        sql.INTO_VALUES(columns.stream().map(t -> t.getValue().equals("id") ? "uuid_generate_v4()" : StringUtils.join(List.of("#{", t.getKey(), "}"), "")).collect(Collectors.toList()).toArray(new String[]{}));
         String sqlStatement = sql.toString();
         var sqlSession = sqlSessionFactory.openSession(ExecutorType.BATCH, TransactionIsolationLevel.NONE);
         rows.forEach(t -> {
