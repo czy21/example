10fba7976f0e3705d966a792658f02be75741425
Bruce CHEN <805899926@qq.com>
2022-01-26 18:31:17 +0800

    #

--- code/api/cooperated/src/main/java/com/team/cooperated/CooperatedConfigure.java
+++ code/api/cooperated/src/main/java/com/team/cooperated/CooperatedConfigure.java
@@ -2,7 +2,11 @@ package com.team.cooperated;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.team.cooperated.feign.FeignConfigure;
+import io.github.mweirauch.micrometer.jvm.extras.ProcessMemoryMetrics;
+import io.github.mweirauch.micrometer.jvm.extras.ProcessThreadMetrics;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -64,6 +68,14 @@ public class CooperatedConfigure implements WebMvcConfigurer {
         AntPathMatcher matcher = new AntPathMatcher();
         matcher.setCaseSensitive(false);
         configurer.setPathMatcher(matcher);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> meterRegistryCustomizer() {
+        return (registry) -> {
+            new ProcessMemoryMetrics().bindTo(registry);
+            new ProcessThreadMetrics().bindTo(registry);
+        };
     }
 
 }
