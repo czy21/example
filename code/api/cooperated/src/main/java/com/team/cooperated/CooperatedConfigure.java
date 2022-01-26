91c561d15c3caaf579dd6bc5afeca631c272858f
Bruce CHEN <805899926@qq.com>
2022-01-26 16:55:53 +0800

    #

--- code/api/cooperated/src/main/java/com/team/cooperated/CooperatedConfigure.java
+++ code/api/cooperated/src/main/java/com/team/cooperated/CooperatedConfigure.java
@@ -2,10 +2,7 @@ package com.team.cooperated;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.team.cooperated.feign.FeignConfigure;
-import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -67,11 +64,6 @@ public class CooperatedConfigure implements WebMvcConfigurer {
         AntPathMatcher matcher = new AntPathMatcher();
         matcher.setCaseSensitive(false);
         configurer.setPathMatcher(matcher);
-    }
-
-    @Bean
-    MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
-        return (registry) -> registry.config().commonTags("application", applicationName);
     }
 
 }
