9eb633590b07e74d462b8b64bf523abe6906a8af
Bruce CHEN <805899926@qq.com>
2022-01-27 13:39:47 +0800

    #

--- code/api/gateway/src/main/java/com/team/gateway/config/GatewayConfigure.java
+++ code/api/gateway/src/main/java/com/team/gateway/config/GatewayConfigure.java
@@ -1,5 +1,9 @@
 package com.team.gateway.config;
 
+import io.github.mweirauch.micrometer.jvm.extras.ProcessMemoryMetrics;
+import io.github.mweirauch.micrometer.jvm.extras.ProcessThreadMetrics;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
