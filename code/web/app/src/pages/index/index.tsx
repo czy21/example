07597889e8839f0049896e1bd3fb05ec40c26353
Bruce CHEN <805899926@qq.com>
2022-02-10 19:30:42 +0800

    #

--- code/web/app/src/pages/index/index.tsx
+++ code/web/app/src/pages/index/index.tsx
@@ -1,21 +1,13 @@
 import * as React from 'react';
-import { View, Text, Image } from 'remax/one';
+import {View,Button,Text} from 'remax/wechat';
 import styles from './index.css';
 
 export default () => {
-  return (
-    <View className={styles.app}>
-      <View className={styles.header}>
-        <Image
-          src="https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*OGyZSI087zkAAAAAAAAAAABkARQnAQ"
-          className={styles.logo}
-          alt="logo"
-        />
-        <View className={styles.text}>
-          编辑 <Text className={styles.path}>src/pages/index/index.js</Text>{' '}
-          开始
+    return (
+        <View className={styles.app}>
+            <View className={styles.header}>
+                <Button onClick={()=>{console.log("aaa")}}>确认</Button>
+            </View>
         </View>
-      </View>
-    </View>
-  );
+    );
 };
