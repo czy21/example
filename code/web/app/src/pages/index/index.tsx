fd75c4a72f4e13eadeb658f4250638a6d8fe075c
Bruce CHEN <805899926@qq.com>
2022-02-11 10:54:13 +0800

    #

--- code/web/app/src/pages/index/index.tsx
+++ code/web/app/src/pages/index/index.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
-import {View,Button} from 'remax/one';
+import {View, Button,Text} from 'remax/one';
 import styles from './index.css';
 
 export default () => {
     return (
         <View className={styles.app}>
-            <View className={styles.header}/>
-            <Button onTap={()=>console.log("hello")}>确认</Button>
+            {/*<Button onTap={() => console.log("hello")}>确认</Button>*/}
+            <Text>你好我的世界</Text>
         </View>
     );
 };
