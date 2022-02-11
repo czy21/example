e9e56ae073de5342b6599f62c90159ef6516f488
Bruce CHEN <805899926@qq.com>
2022-02-11 16:46:29 +0800

    #

---
+++ code/web/mapp/src/app.config.ts
@@ -0,0 +1,11 @@
+export default defineAppConfig({
+  pages: [
+    'view/home'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  }
+})
