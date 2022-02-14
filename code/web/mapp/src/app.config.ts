88f0db15cba49dafaa2a1db9f42e39d8bb0bd929
Bruce CHEN <805899926@qq.com>
2022-02-14 11:54:19 +0800

    #

--- code/web/mapp/src/app.config.ts
+++ code/web/mapp/src/app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   pages: [
-    'view/home'
+    'view/home/index'
   ],
   window: {
     backgroundTextStyle: 'light',
--- code/web/mapp/src/view/home/index.tsx
+++ code/web/mapp/src/view/home/index.tsx
@@ -1,20 +1,18 @@
 import React from 'react'
-import {View, Text} from '@tarojs/components'
+import {View, Text, Swiper, SwiperItem} from '@tarojs/components'
 import '@v/home/index.scss'
 
 const Index: React.FC<any> = (props: any) => {
   return (
-    <View className='components-page'>
-      <View className='flex-wrp' style='flex-direction:column;'>
-        <View className='flex-item flex-item-V demo-text-1'>
-          <Text>A</Text>
-        </View>
-        <View className='flex-item flex-item-V demo-text-2'>
-          <Text>B</Text>
-        </View>
-        <View className='flex-item flex-item-V demo-text-3'>
-          <Text>C</Text>
-        </View>
+    <View className='flex-wrp' style='flex-direction:column;'>
+      <View className='flex-item flex-item-V'>
+        <Text>A</Text>
+      </View>
+      <View className='flex-item flex-item-V'>
+        <Text>B</Text>
+      </View>
+      <View className='flex-item flex-item-V'>
+        <Text>C</Text>
       </View>
     </View>
   )
