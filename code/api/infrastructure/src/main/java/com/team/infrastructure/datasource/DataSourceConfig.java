dfaf6dbbb350478c798c6e38f09595a41bdd3dc6
Bruce CHEN <805899926@qq.com>
2022-03-11 18:03:55 +0800

    #

---
+++ code/api/infrastructure/src/main/java/com/team/infrastructure/datasource/DataSourceConfig.java
@@ -0,0 +1,52 @@
+package com.team.infrastructure.datasource;
+
+import com.zaxxer.hikari.metrics.MetricsTrackerFactory;
+import lombok.Data;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
+
+@Data
+public class DataSourceConfig {
+    private String catalog;
+    private Long connectionTimeout;
+    private Long validationTimeout;
+    private Long idleTimeout;
+    private Long leakDetectionThreshold;
+    private Long maxLifetime;
+    private Integer maxPoolSize;
+    private Integer minIdle;
+    private String username;
+    private String password;
+
+    private Long initializationFailTimeout;
+    private String connectionInitSql;
+    private String connectionTestQuery;
+    private String dataSourceClassName;
+    private String dataSourceJndiName;
+    private String driverClassName;
+    private String exceptionOverrideClassName;
+    private String jdbcUrl;
+    private String poolName;
+    private String schema;
+    private String transactionIsolationName;
+    private Boolean isAutoCommit;
+    private Boolean isReadOnly;
+    private Boolean isIsolateInternalQueries;
+    private Boolean isRegisterMbeans;
+    private Boolean isAllowPoolSuspension;
+    private DataSource dataSource;
+    private Properties dataSourceProperties;
+    private ThreadFactory threadFactory;
+    private ScheduledExecutorService scheduledExecutor;
+    private MetricsTrackerFactory metricsTrackerFactory;
+    private Object metricRegistry;
+    private Object healthCheckRegistry;
+    private Properties healthCheckProperties;
+
+    private Long keepaliveTime;
+
+    private Boolean sealed;
+}
