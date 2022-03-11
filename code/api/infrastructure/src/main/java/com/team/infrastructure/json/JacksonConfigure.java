e9af7fb95e3fcb9036530166060c1e1a31389aa9
Bruce CHEN <805899926@qq.com>
2022-03-11 18:32:08 +0800

    #

--- code/api/infrastructure/src/main/java/com/team/infrastructure/json/JacksonConfigure.java
+++ code/api/infrastructure/src/main/java/com/team/infrastructure/json/JacksonConfigure.java
@@ -11,11 +11,13 @@ import org.springframework.context.annotation.Bean;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class JacksonConfigure {
 
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        DateTimeFormatter dateTimeFormatter= DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         return builder -> {
             builder.serializationInclusion(JsonInclude.Include.NON_NULL);
             builder.serializerByType(LocalDateTime.class, LocalDateTimeSerializer.INSTANCE);
