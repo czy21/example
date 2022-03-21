45395ad02418faaf3fde90f5f89b285285f10203
Bruce Chen <805899926@qq.com>
2022-03-22 00:12:09 +0800

    #

--- code/api/portal/src/main/java/com/team/portal/controller/UserController.java
+++ code/api/portal/src/main/java/com/team/portal/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.team.portal.controller;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.team.application.model.dto.PageDTO;
 import com.team.application.model.dto.UserDTO;
 import com.team.application.model.vo.SearchVO;
@@ -13,6 +14,10 @@ import com.team.cooperated.annotation.EnumOption;
 import com.team.cooperated.annotation.SpecialOption;
 import com.team.cooperated.controller.BaseController;
 import com.team.portal.model.UserExportDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
@@ -25,6 +30,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -84,6 +90,59 @@ public class UserController extends BaseController {
         String filename = URLEncoder.encode("模板", StandardCharsets.UTF_8.toString()) + ".xlsx";
         return CompletableFuture.supplyAsync(() -> downloadExcel(List.of(), UserExportDTO.class, filename));
     }
+
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class TestDTO {
+
+        private LocalDateTime t1;
+//        private LocalDateTime t2;
+
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime t3;
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime t4;
+
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime t5;
+
+        private Date t6;
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:s")
+        private Date t7;
+
+    }
+
+    @PostMapping(path = "testTime")
+    public TestDTO testTime(@RequestBody TestDTO param) throws Exception {
+        if (param.getT1() == null) {
+            param.t1 = LocalDateTime.now();
+        }
+//        if (param.getT2() == null) {
+//            param.t2 = LocalDateTime.now();
+//        }
+        if (param.getT3() == null) {
+            param.t3 = LocalDateTime.now();
+        }
+        if (param.getT4() == null) {
+            param.t4 = LocalDateTime.now();
+        }
+        if (param.getT5() == null) {
+            param.t5 = LocalDateTime.now();
+        }
+
+        if (param.getT6() == null) {
+            param.t6 = new Date();
+        }
+
+        if (param.getT7() == null) {
+            param.t7 = new Date();
+        }
+        return param;
+    }
+
 
 }
 
--- code/web/react/operation/src/view/user/index.tsx
+++ code/web/react/operation/src/view/user/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import stub from "@/init";
-import Table from 'frontend-framework/lib/table'
+import {Table} from '@fra'
 
 
 const records: any = []
@@ -13,10 +13,47 @@ let load = () => {
     })
 }
 
-export default class Index extends React.Component<any, any> {
-    render() {
-        // return <Table dataSource={this.state.list} columns={columns}/>;
-        // return <stub.ref.antd.Button onClick={load}>加载</stub.ref.antd.Button>
-        return Table
+const InstanceList: React.FC<any> = (props: any) => {
+
+    const [data, setData] = stub.ref.react.useState<any>({})
+    const [query, setQuery] = stub.ref.react.useState<any>()
+
+    stub.ref.react.useEffect(() => {
+        // stub.store.dispatch(stub.reducer.action.option.fetch(["dbInstanceKind", "genderKind"]))
+        handleSearch()
+    }, [])
+
+    const columns = [
+        {
+            key: 'name',
+            header: "名称",
+            render: (text: any, record: any) => {
+                return (
+                    <a onClick={() => {
+                        props.history.push(`${props.route.path}/${record.name}`)
+                    }}>{record.name}</a>
+                )
+            }
+        },
+    ];
+
+    const handleSearch = (q?: any) => {
+        setQuery(q)
+        stub.api.post("db/instance/search", stub.ref.lodash.omit(q, "total")).then((t: any) => setData(t.data))
     }
+
+    const [instanceAddVisible, setInstanceAddVisible] = stub.ref.react.useState<boolean>(false);
+
+    return (
+        <div>
+            <Table
+                columns={columns}
+                list={data.list}
+                page={data.page}
+            />
+        </div>
+    )
+
 }
+
+export default InstanceList
