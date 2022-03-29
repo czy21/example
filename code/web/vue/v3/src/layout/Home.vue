bed878e4365a970c456a5002914c3a9d949876be
Bruce Chen <805899926@qq.com>
2022-03-29 22:37:19 +0800

    # init vue3 and vite

---
+++ code/web/vue/v3/src/layout/Home.vue
@@ -0,0 +1,6 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  // 已启用类型推断
+})
+</script>
---
+++ code/web/vue/v3/src/layout/NavMenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <template v-for="t in menuTree">
+      <el-submenu v-if="t.children" :key="t.id" :data="t" :index="t.name">
+        <template slot="title">
+          <i :class="t.icon" :style="{fontSize:iconSize}"></i>
+          <span slot="title" v-if="!collapse"> {{ t.name }}</span>
+        </template>
+        <NavMenu :menuTree="t.children"/>
+      </el-submenu>
+      <el-menu-item v-if="!t.children" :key="t.id" :data="t" :index="t.path" :route="t.path">
+        <i :class="t.icon" :style="{fontSize:iconSize}"></i>
+        <span slot="title">{{ t.name }}</span>
+      </el-menu-item>
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Prop, Vue} from "vue-property-decorator";
+
+@Component
+export default class NavMenu extends Vue {
+  @Prop({default: () => []}) private menuTree?: any
+  @Prop({default: () => false}) private collapse?: boolean
+  @Prop({default: () => "16px"}) private iconSize?: string
+}
+</script>
