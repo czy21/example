699d5359cb702208bb253a1dc0047c2dafcde3b3
Bruce Chen <805899926@qq.com>
2022-03-30 11:26:19 +0800

    #

--- code/web/vue/v3/src/layout/Home.vue
+++ code/web/vue/v3/src/layout/Home.vue
@@ -1,11 +1,21 @@
 <template>
-  <div>aaa</div>
+  <el-button @click="add">hello</el-button>
 </template>
 <script lang="ts">
 import {defineComponent} from 'vue'
 
 export default defineComponent({
-  computed: {},
-  methods: {}
+  data() {
+    return {
+      userList: [{
+        name: "hello"
+      }]
+    }
+  },
+  methods: {
+    add() {
+      console.log(this.userList)
+    }
+  }
 })
 </script>
