92f458b65d948b923d8bb343f47dc4abd0675381
Bruce Chen <805899926@qq.com>
2022-04-02 17:02:43 +0800

    # add redis stream

--- code/api/file-resolve/src/main/java/com/team/fileresolve/FileResolveApplication.java
+++ code/api/file-resolve/src/main/java/com/team/fileresolve/FileResolveApplication.java
@@ -1,7 +1,6 @@
 package com.team.fileresolve;
 
 
-import com.team.fileresolve.receiver.RedisMessageConsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
