eeb12a20e28ab61012901e1ea9907d59c4745162
Bruce Chen <805899926@qq.com>
2022-04-02 17:57:34 +0800

    #

--- code/api/file-resolve/src/main/java/com/team/fileresolve/configure/FileResolveConfiguration.java
+++ code/api/file-resolve/src/main/java/com/team/fileresolve/configure/FileResolveConfiguration.java
@@ -1,13 +1,12 @@
 package com.team.fileresolve.configure;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.stream.Consumer;
-import org.springframework.data.redis.connection.stream.ObjectRecord;
-import org.springframework.data.redis.connection.stream.ReadOffset;
-import org.springframework.data.redis.connection.stream.StreamOffset;
+import org.springframework.data.redis.connection.stream.*;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.stream.StreamListener;
 import org.springframework.data.redis.stream.StreamMessageListenerContainer;
 import org.springframework.data.redis.stream.Subscription;
@@ -15,10 +14,18 @@ import org.springframework.data.redis.stream.Subscription;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
 
 @ComponentScan(value = "com.team")
 @Configuration
-public class FileResolveConfiguration {
+public class FileResolveConfiguration implements InitializingBean {
+
+    StringRedisTemplate redisTemplate;
+
+    public FileResolveConfiguration(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
 
     @Bean
     public Subscription subscription(RedisConnectionFactory redisConnectionFactory, StreamListener<String, ObjectRecord<String, Object>> streamListener) throws UnknownHostException {
@@ -38,4 +45,15 @@ public class FileResolveConfiguration {
         return subscription;
     }
 
+    // XGROUP CREATE kf:log:token kf-log-token-group 0-0 MKSTREAM
+    @Override
+    public void afterPropertiesSet() {
+        try {
+            redisTemplate.opsForStream().createGroup("kf:log:token", ReadOffset.from("0-0"), "kf-log-token-group");
+        } catch (Exception e) {
+            if (!e.getMessage().startsWith("BUSYGROUP Consumer Group name already exists")) {
+                throw e;
+            }
+        }
+    }
 }
