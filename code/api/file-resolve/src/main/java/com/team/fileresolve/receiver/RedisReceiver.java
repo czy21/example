20c366f2cf15909d90a1f15e44761f6637b1dec5
Bruce Chen <805899926@qq.com>
2022-04-03 19:21:37 +0800

    #

--- code/api/file-resolve/src/main/java/com/team/fileresolve/receiver/RedisReceiver.java
+++ code/api/file-resolve/src/main/java/com/team/fileresolve/receiver/RedisReceiver.java
@@ -1,6 +1,10 @@
 package com.team.fileresolve.receiver;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.team.domain.log.ApiLogModel;
 import com.team.infrastructure.datasource.RoutingDataSource;
+import lombok.SneakyThrows;
+import org.springframework.data.redis.connection.stream.MapRecord;
 import org.springframework.data.redis.connection.stream.ObjectRecord;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.stream.StreamListener;
@@ -12,22 +16,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Component
-public class RedisReceiver implements StreamListener<String, ObjectRecord<String, Object>> {
+public class RedisReceiver implements StreamListener<String, MapRecord<String, String, String>> {
 
     JdbcTemplate jdbcTemplate;
     StringRedisTemplate redisTemplate;
+    ObjectMapper objectMapper;
 
     public RedisReceiver(RoutingDataSource dataSource,
-                         StringRedisTemplate redisTemplate) {
+                         StringRedisTemplate redisTemplate,
+                         ObjectMapper objectMapper) {
         Map<String, DataSource> dataSourceMap = dataSource.getResolvedDataSources().entrySet().stream().collect(HashMap::new, (m, n) -> m.put(n.getKey().toString(), n.getValue()), Map::putAll);
         jdbcTemplate = new JdbcTemplate(dataSourceMap.get("master"));
         this.redisTemplate = redisTemplate;
+        this.objectMapper = objectMapper;
     }
 
+    @SneakyThrows
     @Override
-    public void onMessage(ObjectRecord<String, Object> message) {
-        System.out.println(message);
-        jdbcTemplate.update("insert into ent_api_log(args) values (?)", new Object[]{message.getValue()});
+    public void onMessage(MapRecord<String, String, String> message) {
+        jdbcTemplate.update("insert into ent_api_log(args) values (?)", new Object[]{objectMapper.writeValueAsString(message.getValue())});
         redisTemplate.opsForStream().delete(message);
     }
 }
