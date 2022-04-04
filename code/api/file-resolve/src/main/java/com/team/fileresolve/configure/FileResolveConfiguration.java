07299c3aeabe0892b52ec3a05d63ea5440660f56
Bruce CHEN <805899926@qq.com>
2022-04-04 17:23:03 +0800

    #

--- code/api/file-resolve/src/main/java/com/team/fileresolve/configure/FileResolveConfiguration.java
+++ code/api/file-resolve/src/main/java/com/team/fileresolve/configure/FileResolveConfiguration.java
@@ -1,20 +1,24 @@
 package com.team.fileresolve.configure;
 
-import org.checkerframework.checker.units.qual.K;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.stream.*;
+import org.springframework.data.redis.connection.stream.Consumer;
+import org.springframework.data.redis.connection.stream.MapRecord;
+import org.springframework.data.redis.connection.stream.ReadOffset;
+import org.springframework.data.redis.connection.stream.StreamOffset;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.stream.StreamListener;
 import org.springframework.data.redis.stream.StreamMessageListenerContainer;
 import org.springframework.data.redis.stream.Subscription;
 
-import java.net.UnknownHostException;
 import java.time.Duration;
-import java.util.Map;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 @ComponentScan(value = "com.team")
 @Configuration
@@ -27,7 +31,8 @@ public class FileResolveConfiguration implements InitializingBean {
     }
 
     @Bean
-    public Subscription subscription(RedisConnectionFactory redisConnectionFactory, StreamListener<String, MapRecord<String, String, String>> streamListener) throws UnknownHostException {
+    public Subscription subscription(RedisConnectionFactory redisConnectionFactory,
+                                     @Qualifier("redisLogReceiver") StreamListener<String, MapRecord<String, String, String>> streamListener) {
         var options = StreamMessageListenerContainer
                 .StreamMessageListenerContainerOptions
                 .builder()
