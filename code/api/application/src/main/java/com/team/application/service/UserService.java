ae53a321c19122eb8a4e7b5ebee1fc400d786291
Bruce Chen <805899926@qq.com>
2022-06-03 20:16:43 +0800

    #

--- code/api/application/src/main/java/com/team/application/service/UserService.java
+++ code/api/application/src/main/java/com/team/application/service/UserService.java
@@ -1,13 +1,10 @@
 package com.team.application.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.learning.web.model.SimpleItemModel;
 import com.team.application.model.dto.PageDTO;
 import com.team.application.model.dto.UserDTO;
 import com.team.application.model.vo.SearchVO;
 import com.team.domain.entity.UserEntity;
-
-import java.util.List;
 
 public interface UserService extends IService<UserEntity> {
 
