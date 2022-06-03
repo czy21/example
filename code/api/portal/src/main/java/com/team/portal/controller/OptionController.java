9cd25fc391832ab4962a00014a3315f01c2018c9
Bruce Chen <805899926@qq.com>
2022-06-03 19:22:48 +0800

    #

--- code/api/portal/src/main/java/com/team/portal/controller/OptionController.java
+++ code/api/portal/src/main/java/com/team/portal/controller/OptionController.java
@@ -3,11 +3,9 @@ package com.team.portal.controller;
 import com.learning.web.model.SimpleItemModel;
 import com.learning.web.service.OptionService;
 import com.team.application.model.query.OptionQuery;
+import com.team.application.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -19,6 +17,8 @@ public class OptionController {
     @Autowired
     OptionService optionService;
 
+    @Autowired
+    UserService userService;
     @PostMapping(path = "query")
     public Map<String, List<? extends SimpleItemModel<?>>> query(@RequestBody OptionQuery query) {
         return optionService.findByKeys(query.getKeys());
