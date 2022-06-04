8c7c44587c2a2aadf7f5b4f97df16d81021e4e88
Bruce Chen <805899926@qq.com>
2022-06-04 16:56:26 +0800

    #

--- code/api/portal/src/main/java/com/team/portal/controller/TestController.java
+++ code/api/portal/src/main/java/com/team/portal/controller/TestController.java
@@ -1,10 +1,22 @@
 package com.team.portal.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.czy.learning.pulsar.core.PulsarTemplate;
+import org.apache.pulsar.client.api.PulsarClientException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RequestMapping(path = "test")
 @RestController
 public class TestController {
 
+    @Autowired
+    PulsarTemplate pulsarTemplate;
+
+    @PostMapping(path = "pulsar/put")
+    public Map<String, Object> pulsarPut(@RequestParam String topic, @RequestBody Map<String, Object> param) throws PulsarClientException {
+        pulsarTemplate.send(topic, param);
+        return Map.of();
+    }
 }
--- code/api/portal/src/main/java/com/team/portal/listener/PulsarConsumer.java
+++ code/api/portal/src/main/java/com/team/portal/listener/PulsarConsumer.java
@@ -1,11 +1,18 @@
 package com.team.portal.listener;
 
 import com.czy.learning.pulsar.annotation.PulsarListener;
+import com.czy.learning.pulsar.core.ProducerBuilderWrapper;
 import com.czy.learning.pulsar.core.PulsarTemplate;
+import com.team.application.config.QueueConfig;
+import com.team.application.model.RowModel;
 import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.client.api.Schema;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class PulsarConsumer {
@@ -15,13 +22,13 @@ public class PulsarConsumer {
     @Autowired
     PulsarClient pulsarClient;
 
-    @PulsarListener(topic = "topic-a",subscriptionName = "hello")
-    public void topicA() {
-        System.out.println("a");
+    @PulsarListener(topics = QueueConfig.TOPIC_A, subscriptionName = "hello", clazz = Map.class)
+    public void topicA(Map<String, Object> record) {
+        System.out.println(record);
     }
 
-    @PulsarListener(topic = "topic-b",subscriptionName = "hello")
-    public void topicB() {
-        System.out.println("a");
+    @PulsarListener(topics = QueueConfig.TOPIC_B, subscriptionName = "hello", clazz = Map.class)
+    public void topicB(Map<String, Object> record) {
+        System.out.println(record);
     }
 }
