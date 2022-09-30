3c7057d27dd8fc382ecbfb42b798e7d837922c31
Bruce CHEN <805899926@qq.com>
2022-09-30 15:35:14 +0800

    #

--- db/mysql/1_version/100v/1_structure(s)/4_book/1_ent_book.sql
+++ db/mysql/1_version/100v/1_structure(s)/4_book/1_ent_book.sql
@@ -4,3 +4,16 @@ CREATE TABLE `ent_book`  (
   {{ TrackColumn }},
   PRIMARY KEY (`id`)
 ) COMMENT='书表';
+
+
+
+CREATE TABLE `kafka_topic` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `value` int DEFAULT NULL COMMENT '指',
+  create_time TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间',
+	create_user varchar(36)  NULL COMMENT '创建人',
+	update_time TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '更新时间',
+	update_user varchar(36)  NULL COMMENT '更新人',
+	deleted bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除',
+  PRIMARY KEY (`id`)
+) COMMENT='kafka-topic';
