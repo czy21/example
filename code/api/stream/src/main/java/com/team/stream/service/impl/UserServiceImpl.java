46cb5fbb52ae533a826b97c1539ed0437d200229
Bruce CHEN <805899926@qq.com>
2022-10-12 18:27:47 +0800

    #

--- code/api/stream/src/main/java/com/team/stream/service/impl/UserServiceImpl.java
+++ code/api/stream/src/main/java/com/team/stream/service/impl/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package com.team.stream.service.impl;
 
-import com.czy.learning.db.annotation.DS;
+import com.czy.learning.db.datasource.DynamicDataSourceContext;
 import com.team.stream.mapper.UserMapper;
 import com.team.stream.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 @Service
@@ -14,9 +17,14 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserMapper userMapper;
 
-    @DS("ds2")
+    ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(10,
+            50, 60, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+
     @Override
     public void batchAdd(String tableName, Integer size) {
-        IntStream.range(0, size).forEach(t -> userMapper.insert(tableName, Integer.toString(t)));
+        IntStream.range(0, size).forEach(t -> threadPoolExecutor.execute(() -> {
+            DynamicDataSourceContext.put("ds2");
+            userMapper.insert(tableName, Integer.toString(t));
+        }));
     }
 }
