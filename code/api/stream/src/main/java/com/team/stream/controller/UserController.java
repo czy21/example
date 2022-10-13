688974697914cd5239b7806ebaa5240f03a946ce
Bruce CHEN <805899926@qq.com>
2022-10-13 10:40:54 +0800

    #

--- code/api/stream/src/main/java/com/team/stream/controller/UserController.java
+++ code/api/stream/src/main/java/com/team/stream/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping(path = "user")
 public class UserController {
@@ -15,9 +17,20 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping(path = "batchAdd")
-    public Integer batchAdd(@RequestParam("ds") String dataSource, @RequestParam("size") Integer size) {
-        userService.batchAdd(dataSource, "user_1", size);
+    @GetMapping(path = "syncInsert")
+    public Integer syncInsert(@RequestParam("ds") String dataSource,
+                              @RequestParam(value = "tableName", required = false) String tableName,
+                              @RequestParam("size") Integer size) {
+        userService.syncInsert(dataSource, Optional.ofNullable(tableName).orElse("user_1"), size);
+        return 1;
+    }
+
+    @GetMapping(path = "parallelInsert")
+    public Integer parallelInsert(@RequestParam("ds") String ds,
+                                  @RequestParam("size") Integer size,
+                                  @RequestParam(value = "tableName", required = false) String tableName,
+                                  @RequestParam(value = "batchSize", required = false) Integer batchSize) {
+        userService.parallelInsert(ds, Optional.ofNullable(tableName).orElse("user_1"), size, Optional.ofNullable(batchSize).orElse(500));
         return 1;
     }
 }
--- code/api/stream/src/main/java/com/team/stream/service/impl/UserServiceImpl.java
+++ code/api/stream/src/main/java/com/team/stream/service/impl/UserServiceImpl.java
@@ -3,13 +3,20 @@ package com.team.stream.service.impl;
 import com.czy.learning.db.datasource.DynamicDataSourceContext;
 import com.team.stream.mapper.UserMapper;
 import com.team.stream.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.ListUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.*;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
 
     @Autowired
@@ -17,11 +24,26 @@ public class UserServiceImpl implements UserService {
     ExecutorService executor = Executors.newFixedThreadPool(500);
 
     @Override
-    public void batchAdd(String dataSource, String tableName, Integer size) {
-        IntStream.range(0, size).forEach(t ->
-                executor.execute(() -> {
-                    DynamicDataSourceContext.put(dataSource);
-                    userMapper.insert(tableName, Integer.toString(t));
-                }));
+    public void syncInsert(String dataSource, String tableName, Integer size) {
+        DynamicDataSourceContext.put(dataSource);
+        IntStream.rangeClosed(1, size).forEach(t -> userMapper.insert(tableName, Integer.toString(t)));
+    }
+
+    @Override
+    public void parallelInsert(String dataSource, String tableName, Integer size, Integer batchSize) {
+        List<List<Integer>> sizeGroup = ListUtils.partition(IntStream.rangeClosed(1, size).boxed().collect(Collectors.toList()), batchSize)
+                .stream()
+                .map(t -> IntStream.rangeClosed(1, t.size()).boxed().collect(Collectors.toList())).collect(Collectors.toList());
+        for (int i = 1; i <= sizeGroup.size(); i++) {
+            List<Integer> t = sizeGroup.get(i - 1);
+            List<CompletableFuture<Void>> futures = t.stream()
+                    .map(s -> CompletableFuture.runAsync(
+                            () -> {
+                        DynamicDataSourceContext.put(dataSource);
+                        userMapper.insert(tableName, Integer.toString(s));
+                            }, executor)).collect(Collectors.toList());
+            CompletableFuture.allOf(futures.toArray(new CompletableFuture[]{}));
+            log.info("parallelInsert: {} {} {} {} finished", dataSource, size, t.size(), i);
+        }
     }
 }
