e84af521fc2a6ea32dc0b2c01fd112c1f87c95d2
Bruce CHEN <805899926@qq.com>
2022-10-20 16:48:05 +0800

    #

--- shell/env_common.py
+++ shell/env_common.py
@@ -3,15 +3,15 @@
 from domain.default import path as default_path
 from utility import path as path_util
 
-shell_path = path_util.pure_path_join(default_path.root_path, "shell")
+shell_path = path_util.join_path(default_path.root_path, "shell")
 
 param_project_name = "demo"
 
 # api
-param_api_root_project_path = path_util.pure_path_join(default_path.project_code_api)
+param_api_root_project_path = path_util.join_path(default_path.project_code_api)
 
 # web
-param_web_root_project_path = path_util.pure_path_join(default_path.project_code_web, "react", "operation")
+param_web_root_project_path = path_util.join_path(default_path.project_code_web, "react", "operation")
 
 param_main_db_mysql_user = \
     param_main_db_mongo_user = "admin"
@@ -29,10 +29,10 @@ param_main_db_neo4j_user = "neo4j"
 param_main_db_pgsql_user = "postgres"
 param_main_db_cksql_user = "default"
 
-param_main_db_mysql_file_path = path_util.pure_path_join(default_path.project_db, "mysql", "1_version")
-# param_main_db_mysql_file_path = path_util.pure_path_join("C:/Users/zhaoyu.chen/Desktop/developer/mx-project/v2.7.0/")
-param_main_db_pgsql_file_path = path_util.pure_path_join(default_path.project_db, "pgsql", "1_version")
-param_main_db_mssql_file_path = path_util.pure_path_join(default_path.project_db, "mssql", "1_version")
-param_main_db_cksql_file_path = path_util.pure_path_join(default_path.project_db, "cksql", "1_version")
-param_main_db_mongo_file_path = path_util.pure_path_join(default_path.project_db, "mongo", "1_version")
-param_main_db_neo4j_file_path = path_util.pure_path_join(default_path.project_db, "neo4j", "1_version")
+param_main_db_mysql_file_path = path_util.join_path(default_path.project_db, "mysql", "1_version")
+# param_main_db_mysql_file_path = path_util.join_path("C:/Users/zhaoyu.chen/Desktop/developer/mx-project/v2.7.0/")
+param_main_db_pgsql_file_path = path_util.join_path(default_path.project_db, "pgsql", "1_version")
+param_main_db_mssql_file_path = path_util.join_path(default_path.project_db, "mssql", "1_version")
+param_main_db_cksql_file_path = path_util.join_path(default_path.project_db, "cksql", "1_version")
+param_main_db_mongo_file_path = path_util.join_path(default_path.project_db, "mongo", "1_version")
+param_main_db_neo4j_file_path = path_util.join_path(default_path.project_db, "neo4j", "1_version")
