38461c6ac09875ac390cb4e9acc7546f54621cbf
Bruce CHEN <805899926@qq.com>
2022-10-28 17:05:54 +0800

    #

--- code/api/mybatis/src/main/java/com/team/mybatis/controller/UserController.java
+++ code/api/mybatis/src/main/java/com/team/mybatis/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.team.mybatis.controller;
 
 
+import com.team.mybatis.entity.UserPO;
 import com.team.mybatis.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "user")
@@ -16,7 +19,7 @@ public class UserController {
     UserService userService;
 
     @GetMapping(path = "search")
-    public Integer syncInsert() {
-        return 1;
+    public List<UserPO> syncInsert() {
+        return userService.findAll();
     }
 }
--- code/api/mybatis/src/main/java/com/team/mybatis/service/impl/UserServiceImpl.java
+++ code/api/mybatis/src/main/java/com/team/mybatis/service/impl/UserServiceImpl.java
@@ -1,11 +1,26 @@
 package com.team.mybatis.service.impl;
 
+import com.team.mybatis.entity.UserPO;
+import com.team.mybatis.mapper.UserMapper;
 import com.team.mybatis.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    UserMapper userMapper;
+
+    @Override
+    public List<UserPO> findAll() {
+        UserPO a = userMapper.selectOneBy(new UserPO());
+        UserPO b = userMapper.selectOneById(9999L);
+        int count = userMapper.count();
+        return userMapper.selectListBy(new UserPO());
+    }
 }
