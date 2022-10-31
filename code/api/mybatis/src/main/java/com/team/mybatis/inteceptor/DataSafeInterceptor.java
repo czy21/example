1c3b73c8bdf9ffecd73fbe31f94d6392031dbacb
Bruce CHEN <805899926@qq.com>
2022-10-31 14:55:34 +0800

    #

--- code/api/mybatis/src/main/java/com/team/mybatis/inteceptor/DataSafeInterceptor.java
+++ code/api/mybatis/src/main/java/com/team/mybatis/inteceptor/DataSafeInterceptor.java
@@ -1,89 +1,114 @@
 package com.team.mybatis.inteceptor;
 
-import com.team.mybatis.annotation.SafeField;
+import com.team.mybatis.annotation.SensitiveAction;
+import com.team.mybatis.annotation.SensitiveEnum;
+import com.team.mybatis.annotation.SensitiveField;
 import org.apache.commons.beanutils.BeanUtils;
-import org.apache.ibatis.executor.parameter.ParameterHandler;
-import org.apache.ibatis.executor.resultset.ResultSetHandler;
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 @Component
 @Intercepts({
-//        @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class}),
-//        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class})
-        @Signature(type = ResultSetHandler.class, method = "handleResultSets", args = {Statement.class}),
-        @Signature(type = ParameterHandler.class, method = "setParameters", args = {PreparedStatement.class})
+        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
+        @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class})
 })
 public class DataSafeInterceptor implements Interceptor {
-    private Properties properties = new Properties();
 
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
         Object target = invocation.getTarget();
-        Method method = invocation.getMethod();
         Object[] args = invocation.getArgs();
-        Object result = invocation.proceed();
-        if (target instanceof ResultSetHandler) {
-            Collection<?> resultList = ((Collection<?>) result);
-            Class<?> targetClass = resultList.stream().findFirst().map(Object::getClass).orElse(null);
-            if (targetClass == null) {
-                return result;
+        Object result = null;
+        if (target instanceof Executor) {
+            MappedStatement ms = (MappedStatement) args[0];
+            String mapperFullName = ms.getId().substring(0, ms.getId().lastIndexOf("."));
+            String mapperMethodName = ms.getId().substring(ms.getId().lastIndexOf(".") + 1);
+            Object mapperMethodParam = args[1];
+            SensitiveEnum selectParamAction = (SensitiveEnum) SensitiveAction.class.getMethod("selectParam").getDefaultValue();
+            SensitiveEnum selectResultAction = (SensitiveEnum) SensitiveAction.class.getMethod("selectResult").getDefaultValue();
+            SensitiveEnum updateParamAction = (SensitiveEnum) SensitiveAction.class.getMethod("updateParam").getDefaultValue();
+            SensitiveAction methodSensitiveAction = Arrays.stream(Class.forName(mapperFullName).getMethods())
+                    .filter(t -> t.getName().equals(mapperMethodName)).findFirst()
+                    .map(t -> t.getAnnotation(SensitiveAction.class)).orElse(null);
+            if (methodSensitiveAction != null) {
+                selectParamAction = methodSensitiveAction.selectParam();
+                selectResultAction = methodSensitiveAction.selectResult();
+                updateParamAction = methodSensitiveAction.updateParam();
             }
-            Map<String, SafeField> safeMap = new HashMap<>(1);
-            Field[] fields = targetClass.getDeclaredFields();
-            for (Field field : fields) {
-                SafeField safeFieldAnnotation = field.getAnnotation(SafeField.class);
-                if (safeFieldAnnotation != null) {
-                    safeMap.put(field.getName(), safeFieldAnnotation);
+            // handle param
+            Map<String, SensitiveField> paramSafeMap = getSafeFieldMap(mapperMethodParam.getClass());
+            // query
+            if (args.length == 4) {
+                for (Map.Entry<String, SensitiveField> e : paramSafeMap.entrySet()) {
+                    setSensitiveField(selectParamAction, mapperMethodParam, e.getKey());
+                }
+                result = invocation.proceed();
+                List<?> resultList = (List<?>) result;
+                Class<?> targetClass = resultList.stream().findFirst().map(Object::getClass).orElse(null);
+                if (targetClass == null) {
+                    return result;
+                }
+                for (Object t : resultList) {
+                    for (Map.Entry<String, SensitiveField> e : paramSafeMap.entrySet()) {
+                        setSensitiveField(selectResultAction, t, e.getKey());
+                    }
                 }
             }
-            for (Object t : resultList) {
-                for (Map.Entry<String, SafeField> s : safeMap.entrySet()) {
-                    Object value = BeanUtils.getProperty(t, s.getKey());
-                    Object decryptedValue = "decrypted value1"; // invoke SafeService
-                    BeanUtils.setProperty(t, s.getKey(), decryptedValue);
+            // update
+            if (args.length == 2) {
+                for (Map.Entry<String, SensitiveField> e : paramSafeMap.entrySet()) {
+                    setSensitiveField(updateParamAction, mapperMethodParam, e.getKey());
                 }
+                result = invocation.proceed();
             }
         }
         return result;
     }
 
-//    protected void decrypt(Object object, String propertyName) {
-//
-//    }
+    protected void setSensitiveField(SensitiveEnum action, Object obj, String fieldName) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        Object originValue = BeanUtils.getProperty(obj, fieldName);
+        Object targetValue = originValue;
+        switch (action) {
+            case ENCRYPT:
+                targetValue = "haha";
+                break;
+            case DECRYPT:
+                targetValue = "hoho";
+                break;
+            case DECRYPT_MASK:
+                targetValue = "bushiz";
+                break;
+            case NONE:
+                break;
+            default:
+                break;
+        }
+        BeanUtils.setProperty(obj, fieldName, targetValue);
+    }
 
-    private void decrypt(Object object) throws InvocationTargetException, IllegalAccessException {
-//        Field[] fields = object.getClass().getDeclaredFields();
-//        for (Field field : fields) {
-//            SafeField confidential = field.getAnnotation(SafeField.class);
-//            if (confidential == null) {
-//                continue;
-//            }
-//            PropertyDescriptor ps = BeanUtils.getPropertyDescriptor(object.getClass(), field.getName());
-//            if (ps.getReadMethod() == null || ps.getWriteMethod() == null) {
-//                continue;
-//            }
-//            Object value = ps.getReadMethod().invoke(object);
-//            if (value != null) {
-//                ps.getWriteMethod().invoke(object, "***");
-//            }
-//        }
+    private Map<String, SensitiveField> getSafeFieldMap(Class<?> targetClass) {
+        Map<String, SensitiveField> safeMap = new HashMap<>(1);
+        Field[] fields = targetClass.getDeclaredFields();
+        for (Field field : fields) {
+            SensitiveField safeFieldAnnotation = field.getAnnotation(SensitiveField.class);
+            if (safeFieldAnnotation != null) {
+                safeMap.put(field.getName(), safeFieldAnnotation);
+            }
+        }
+        return safeMap;
     }
 
     public void setProperties(Properties properties) {
-        this.properties = properties;
     }
 }
--- code/api/mybatis/src/main/java/com/team/mybatis/mapper/UserMapper.java
+++ code/api/mybatis/src/main/java/com/team/mybatis/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import com.team.mybatis.entity.UserPO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -15,7 +16,7 @@ public interface UserMapper {
     @Select(value = "select * from ent_sys_safe_user")
     List<UserPO> selectListBy(UserPO query);
 
-    @Select(value = "select * from ent_sys_safe_user limit 1")
+    @Select(value = "select * from ent_sys_safe_user where phone_no = #{phoneNo} limit 1")
     UserPO selectOneBy(UserPO query);
 
     @Select(value = "select count(0) from ent_sys_safe_user")
@@ -23,4 +24,10 @@ public interface UserMapper {
 
     @Select(value = "select * from ent_sys_safe_user where id = #{id}")
     UserPO selectOneById(@Param("id") Long id);
+
+    @Update(value = "update ent_sys_safe_user set phone_no = #{phoneNo} where id = #{id}")
+    int updateBy(UserPO entity);
+
+    @Insert(value = "insert into ent_sys_safe_user(name,phone_no,id_num)values (#{name},#{phoneNo},#{idNum})")
+    int insertOne(UserPO entity);
 }
