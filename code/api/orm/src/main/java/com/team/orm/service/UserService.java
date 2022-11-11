5046352393b83006e919ec7eb9adeab99dc8285b
Bruce CHEN <805899926@qq.com>
2022-11-11 18:37:57 +0800

    #

--- code/api/orm/src/main/java/com/team/orm/service/UserService.java
+++ code/api/orm/src/main/java/com/team/orm/service/UserService.java
@@ -2,6 +2,7 @@ package com.team.orm.service;
 
 import com.team.orm.entity.UserPO;
 import com.team.orm.repository.UserRepository;
+import org.apache.commons.collections4.map.ReferenceMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +11,18 @@ import java.util.List;
 @Service
 public class UserService {
 
+    private ReferenceMap<String, Object> cache = new ReferenceMap<>();
+//    private Map<String, SoftReference<Object>> cache = new HashMap<>();
+
     @Autowired
     UserRepository userRepository;
 
     public List<UserPO> findAll() {
         return userRepository.findAll();
+    }
+
+    public void put(String key, Object value) {
+        cache.computeIfAbsent(key, t -> value);
+        System.out.println(cache.size());
     }
 }
