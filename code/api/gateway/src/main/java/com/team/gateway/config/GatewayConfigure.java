7c058efd846c5c3c8790e978875b30928daf147a
Bruce CHEN <805899926@qq.com>
2023-02-22 22:02:04 +0800

    #

--- code/api/gateway/src/main/java/com/team/gateway/config/GatewayConfigure.java
+++ code/api/gateway/src/main/java/com/team/gateway/config/GatewayConfigure.java
@@ -17,18 +17,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Configuration
-public class GatewayConfigure implements InitializingBean {
-    SpringDocConfigProperties springDocConfigProperties;
-    SwaggerUiConfigParameters swaggerUiConfigParameters;
-    RouteDefinitionLocator routeDefinitionLocator;
-
-    public GatewayConfigure(SpringDocConfigProperties springDocConfigProperties,
-                            SwaggerUiConfigParameters swaggerUiConfigParameters,
-                            RouteDefinitionLocator routeDefinitionLocator) {
-        this.springDocConfigProperties = springDocConfigProperties;
-        this.swaggerUiConfigParameters = swaggerUiConfigParameters;
-        this.routeDefinitionLocator = routeDefinitionLocator;
-    }
+public class GatewayConfigure {
 
     @Bean
     MeterRegistryCustomizer<MeterRegistry> meterRegistryCustomizer() {
@@ -36,21 +25,5 @@ public class GatewayConfigure implements InitializingBean {
             new ProcessMemoryMetrics().bindTo(registry);
             new ProcessThreadMetrics().bindTo(registry);
         };
-    }
-
-    List<String> ignoreRouteIds = List.of("openapi");
-
-    @Override
-    public void afterPropertiesSet() {
-        Optional.ofNullable(routeDefinitionLocator.getRouteDefinitions().collectList().block()).orElse(new ArrayList<>())
-                .stream()
-                .filter(t -> !ignoreRouteIds.contains(t.getId()))
-                .forEach(t -> {
-                    AbstractSwaggerUiConfigProperties.SwaggerUrl swaggerUrl = new AbstractSwaggerUiConfigProperties.SwaggerUrl(
-                            t.getId(),
-                            springDocConfigProperties.getApiDocs().getPath() + "/" + t.getId(), ""
-                    );
-                    swaggerUiConfigParameters.getUrls().add(swaggerUrl);
-                });
     }
 }
