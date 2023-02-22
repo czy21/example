81f2d7dc78db2e18a21f914dacf90bba8ba1ace8
Bruce CHEN <805899926@qq.com>
2023-02-22 19:45:50 +0800

    #

--- code/api/gateway/src/main/java/com/team/gateway/config/GatewayConfigure.java
+++ code/api/gateway/src/main/java/com/team/gateway/config/GatewayConfigure.java
@@ -3,12 +3,32 @@ package com.team.gateway.config;
 import io.github.mweirauch.micrometer.jvm.extras.ProcessMemoryMetrics;
 import io.github.mweirauch.micrometer.jvm.extras.ProcessThreadMetrics;
 import io.micrometer.core.instrument.MeterRegistry;
+import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties;
+import org.springdoc.core.properties.SpringDocConfigProperties;
+import org.springdoc.core.properties.SwaggerUiConfigParameters;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Configuration
-public class GatewayConfigure {
+public class GatewayConfigure implements InitializingBean {
+    SpringDocConfigProperties springDocConfigProperties;
+    SwaggerUiConfigParameters swaggerUiConfigParameters;
+    RouteDefinitionLocator routeDefinitionLocator;
+
+    public GatewayConfigure(SpringDocConfigProperties springDocConfigProperties,
+                            SwaggerUiConfigParameters swaggerUiConfigParameters,
+                            RouteDefinitionLocator routeDefinitionLocator) {
+        this.springDocConfigProperties = springDocConfigProperties;
+        this.swaggerUiConfigParameters = swaggerUiConfigParameters;
+        this.routeDefinitionLocator = routeDefinitionLocator;
+    }
 
     @Bean
     MeterRegistryCustomizer<MeterRegistry> meterRegistryCustomizer() {
@@ -16,5 +36,21 @@ public class GatewayConfigure {
             new ProcessMemoryMetrics().bindTo(registry);
             new ProcessThreadMetrics().bindTo(registry);
         };
+    }
+
+    List<String> ignoreRouteIds = List.of("openapi");
+
+    @Override
+    public void afterPropertiesSet() {
+        Optional.ofNullable(routeDefinitionLocator.getRouteDefinitions().collectList().block()).orElse(new ArrayList<>())
+                .stream()
+                .filter(t -> !ignoreRouteIds.contains(t.getId()))
+                .forEach(t -> {
+                    AbstractSwaggerUiConfigProperties.SwaggerUrl swaggerUrl = new AbstractSwaggerUiConfigProperties.SwaggerUrl(
+                            t.getId(),
+                            springDocConfigProperties.getApiDocs().getPath() + "/" + t.getId(), ""
+                    );
+                    swaggerUiConfigParameters.getUrls().add(swaggerUrl);
+                });
     }
 }
