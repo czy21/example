9604d2c8d56f89d4d47d880b0bf26868ae0d47e7
Bruce CHEN <805899926@qq.com>
2023-02-24 18:22:07 +0800

    #

--- code/api/gateway/src/main/java/com/team/gateway/config/GatewayConfigure.java
+++ code/api/gateway/src/main/java/com/team/gateway/config/GatewayConfigure.java
@@ -3,18 +3,9 @@ package com.team.gateway.config;
 import io.github.mweirauch.micrometer.jvm.extras.ProcessMemoryMetrics;
 import io.github.mweirauch.micrometer.jvm.extras.ProcessThreadMetrics;
 import io.micrometer.core.instrument.MeterRegistry;
-import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties;
-import org.springdoc.core.properties.SpringDocConfigProperties;
-import org.springdoc.core.properties.SwaggerUiConfigParameters;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
-import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @Configuration
 public class GatewayConfigure {
