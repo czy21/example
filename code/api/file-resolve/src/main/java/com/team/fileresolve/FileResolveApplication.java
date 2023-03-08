97c0acc412d2701cfe8a7827a658b01761e6d7f4
Bruce CHEN <805899926@qq.com>
2023-03-08 16:00:52 +0800

    #

--- code/api/file-resolve/src/main/java/com/team/fileresolve/FileResolveApplication.java
+++ code/api/file-resolve/src/main/java/com/team/fileresolve/FileResolveApplication.java
@@ -3,8 +3,9 @@ package com.team.fileresolve;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class FileResolveApplication {
     public static void main(String[] args) {
