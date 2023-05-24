557fbd3914d416820400a4bb45e432e06697ff5f
Bruce CHEN <805899926@qq.com>
2023-05-24 14:38:24 +0800

    #

--- db/mysql/1_version/100v/1_structure(s)/1_sys/10_dict.sql
+++ db/mysql/1_version/100v/1_structure(s)/1_sys/10_dict.sql
@@ -4,7 +4,7 @@ CREATE TABLE `sys_dict` (
     `value`    varchar(100) NOT NULL COMMENT 'value',
     `type`     varchar(100) NOT NULL COMMENT 'value类型;0:String,1:Int',
     `sort`     int          NOT NULL DEFAULT 0 COMMENT '排序',
-    `category` tinyint(1) DEFAULT 0 COMMENT '类别;0:系统字典,1:业务字典'
+    `category` tinyint(1)   NOT NULL DEFAULT 0 COMMENT '类别;0:系统字典,1:业务字典'
     `parent_id` varchar (36) NULL COMMENT '上级',
     {{ TrackColumn }},
     PRIMARY KEY (`id`)
