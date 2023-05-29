9641ab27776c73a9a5b3c45902c725765135a623
Bruce CHEN <805899926@qq.com>
2023-05-29 08:44:08 +0800

    # add edge module

--- code/api/edge/src/main/java/com/team/edge/SocketPushHandler.java
+++ code/api/edge/src/main/java/com/team/edge/SocketPushHandler.java
@@ -1,4 +1,4 @@
-package com.team.portal.configure;
+package com.team.edge;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
--- code/api/edge/src/main/java/com/team/edge/WebSocketConfiguration.java
+++ code/api/edge/src/main/java/com/team/edge/WebSocketConfiguration.java
@@ -1,7 +1,5 @@
-package com.team.portal.configure;
+package com.team.edge;
 
-import org.apache.poi.ss.formula.functions.T;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -9,8 +7,6 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-import java.io.IOException;
 
 @Configuration
 @EnableWebSocket
