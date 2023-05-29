9e9026944e3aa40d4ba8513a4ed732025253de34
Bruce CHEN <805899926@qq.com>
2023-05-29 14:54:48 +0800

    #

--- code/api/edge/src/main/java/com/team/edge/WebSocketConfiguration.java
+++ code/api/edge/src/main/java/com/team/edge/WebSocketConfiguration.java
@@ -1,27 +1,20 @@
 package com.team.edge;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfiguration implements WebSocketConfigurer {
-    Integer i = 0;
+
+    @Autowired
+    AlertSocketHandler alertSocketHandler;
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new TextWebSocketHandler() {
-            @Override
-            public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-            }
-            @Override
-            protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-
-            }
-        }, "/socket1");
+        registry.addHandler(alertSocketHandler, "/socket1");
     }
 }
