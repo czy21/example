9937c8725418a971977ad9b67c16c09c11b91933
Bruce CHEN <805899926@qq.com>
2023-05-29 14:19:43 +0800

    # 添加rabbitmq fanout 模式

---
+++ code/api/edge/src/main/java/com/team/edge/controller/EdgeController.java
@@ -0,0 +1,29 @@
+package com.team.edge.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.team.edge.config.EdgeQueueConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.stream.IntStream;
+
+@RequestMapping(path = "edge")
+@RestController
+public class EdgeController {
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @PostMapping(path = "publish")
+    public void publish(@RequestBody Map<String, Object> param) {
+        IntStream.rangeClosed(1, (Integer) param.get("size")).forEach(t -> rabbitTemplate.convertAndSend(EdgeQueueConfig.GH_DEVICE_ALERT_EXCHANGE, "", Map.of("i", t)));
+    }
+}
