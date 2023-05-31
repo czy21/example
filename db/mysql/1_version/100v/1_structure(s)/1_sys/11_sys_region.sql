a275a54b69724a7affac358313cab608a45394b6
Bruce CHEN <805899926@qq.com>
2023-05-31 17:00:02 +0800

    #

--- db/mysql/1_version/100v/1_structure(s)/1_sys/11_sys_region.sql
+++ db/mysql/1_version/100v/1_structure(s)/1_sys/11_sys_region.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `sys_area` (
+CREATE TABLE `sys_region` (
   `id` varchar(20) COLLATE utf8mb4_general_ci NOT NULL COMMENT '主键',
   `name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL COMMENT '名称',
   `parent_id` varchar(20) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '父id',
