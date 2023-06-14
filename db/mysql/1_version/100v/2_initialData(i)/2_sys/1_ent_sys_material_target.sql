d4b545df9d7cadd28447f49077a00b46bfa52438
Bruce CHEN <805899926@qq.com>
2023-06-14 12:12:43 +0800

    #

--- db/mysql/1_version/100v/2_initialData(i)/2_sys/1_ent_sys_material_target.sql
+++ db/mysql/1_version/100v/2_initialData(i)/2_sys/1_ent_sys_material_target.sql
@@ -1,4 +1,4 @@
 INSERT INTO `ent_sys_material_target`(`key`, `root_url`, `root_path`, `target_kind`)
 VALUES ('DEFAULT', 'data', 'upload', 'LOCAL'),
        ('ERROR', 'data', 'upload/error', 'LOCAL'),
-       ('OSS', 'http://minio.cluster.com/', 'public', 'OSS');
+       ('OSS', 'http://minio.czy21.com/', 'public', 'OSS');
