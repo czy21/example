25c15f9ba76dbe647f3f06c375587468922932e1
Bruce Chen <805899926@qq.com>
2025-08-24 22:10:46 +0800

    adjust shell

--- shell/env_common.sh
+++ shell/env_common.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+
 shopt -s expand_aliases
 
 if [ -f  "~/.bash_aliases" ];then
   source ~/.bash_aliases
 fi
+
+python_exec="$HOME/.python3/Scripts/python3"
+
+if [ -f "$HOME/.python3/bin/python3" ];then
+  python_exec="$HOME/.python3/bin/python3"
+fi
+
+if [ -f "${python_exec}" ];then
+  alias python3="${python_exec}"
+fi
+
+type python3
 
 sh_file="${0}"
 sh_name="$(basename ${0})"
